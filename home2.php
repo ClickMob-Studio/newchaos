265d55c135274145bd9cf67cdff557998f16529e
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-03-24 10:17:56 +0000

    images and css

--- home2.php
+++ home2.php
@@ -1,98 +1,128 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-session_start();
-include "classes.php";
-include "database/pdo_class.php";
-?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-		<link rel="stylesheet" href="css/login.css" media="screen"/>
-        <title>YobCity Login</title>
-    </head>
-    <body>
-		<div id="logo-text"></div>
-		<div id="logo"></div>
-		<div id="content">
-			<div class="flexcont">
-				<div class="flexele">
-					<span id="errors" style="color:red;font-size:16px;font-weight:bold;"><?php echo ($_SESSION['failmessage']) ? $_SESSION['failmessage'] : "" ; ?></span>
-					<form name="login" action="login.php" method="post" accept-charset="utf-8">
-						<label for="username">Username</label><br />
-						<input class="textbox" type="text" name="username" id="username" required> <br /><br />
-						<label for="userpass">Password</label><br />
-						<input class="textbox" type="password" name="password" id="password" required> <br /><br />
-						<input id="button-login" type="submit" value=""/><br /><br />
-						<a href="register.php">Join Yob City</a>&nbsp;&nbsp;<a href="http://facebook.com/yobcity">Can't log in?</a>
-					</form>
-				</div>
-				<div class="flexele">
-					<?php
-					$db->query("SELECT id, lastactive FROM grpgusers ORDER BY lastactive DESC LIMIT 15");
-					$rows = $db->fetch_row();
-					$i = 1;
-					echo'<table style="margin:auto;text-align:left;">';
-						echo'<tr>';
-							echo'<th colspan="3">Last Active Players</th>';
-						echo'</tr>';
-					foreach($rows as $row){
-						echo'<tr><td>' . $i++ . '.</td><td>' . formatName($row['id']) . '</td><td style="text-align:center;">'.howLongAgo($row['lastactive']).'</td></tr>';
-					}
-					echo'</table>';
-					?>
-				</div>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="lang" content="english">
+	<meta name="robots" content="All">
+	<title>{$set['game_name']} - Free Online Web RPG</title>
+	<link href="assets/css/login.css" type="text/css" rel="stylesheet" />
+	<link href="assets/css/game.css" type="text/css" rel="stylesheet" />
+	<link rel="icon" type="image/png" href="favicon.ico">
+	<script>
+		var end = new Date('11/11/2016 10:00 AM');
+    		var _second = 1000;
+    		var _minute = _second * 60;
+    		var _hour = _minute * 60;
+    		var _day = _hour * 24;
+    		var timer;
+
+    		function showRemaining() {
+        		var now = new Date();
+        		var distance = end - now;
+        		if (distance < 0) {
+            			clearInterval(timer);
+            			document.getElementById('countdown').innerHTML = 'EXPIRED!';
+            			return;
+        		}
+        		var days = Math.floor(distance / _day);
+        		var hours = Math.floor((distance % _day) / _hour);
+        		var minutes = Math.floor((distance % _hour) / _minute);
+        		var seconds = Math.floor((distance % _minute) / _second);
+
+        		document.getElementById('countdown').innerHTML = days + 'days ';
+        		document.getElementById('countdown').innerHTML += hours + 'hrs ';
+        		document.getElementById('countdown').innerHTML += minutes + 'mins ';
+        		document.getElementById('countdown').innerHTML += seconds + 'secs';
+    		}
+    		timer = setInterval(showRemaining, 1000);
+	</script>
+</head>
+<body>
+	<div id="outer" class="wrap">
+		<div id="top_bar" class="wrap">
+			<div style="float:left;color:#910503;margin-left:5px;">
+				<strong>Users Online: {$count}</strong> | 
+				<strong>Users Online in last 24 hours: {$daycount}</strong>
 			</div>
-			<div class="flexcont" style="text-align:center;">
-				<div class="flexele">
-					<?php
-					$db->query("SELECT id FROM grpgusers WHERE admin <> 1 ORDER BY total DESC LIMIT 5");
-					$rows = $db->fetch_row();
-					$i = 1;
-					echo'<table style="margin:auto;text-align:left;">';
-						echo'<tr>';
-							echo'<th colspan="2">Top 5 Strongest Players</th>';
-						echo'</tr>';
-					foreach($rows as $row){
-						echo'<tr><td>' . $i++ . '.</td><td>' . formatName($row['id']) . '</td></tr>';
-					}
-					echo'</table>';
-					?>
-				</div>
-				<div class="flexele">
-					<?php
-					$db->query("SELECT id, level FROM grpgusers WHERE admin <> 1 ORDER BY level DESC LIMIT 5");
-					$rows = $db->fetch_row();
-					$i = 1;
-					echo'<table style="margin:auto;text-align:left;">';
-						echo'<tr>';
-							echo'<th colspan="3">Top 5 Highest Leveled Players</th>';
-						echo'</tr>';
-					foreach($rows as $row){
-						echo'<tr><td>' . $i++ . '.</td><td>' . formatName($row['id']) . '</td><td style="text-align:center;">'.$row['level'].'</td></tr>';
-					}
-					echo'</table>';
-					?>
-				</div>
-			</div>
-			<div class="flexcont" style="text-align:center;">
-				<div class="flexele">
-					<img src="images/newlogin/SS-1.png" style="width:100%;height:100%;"/>
-				</div>
-				<div class="flexele">
-					<img src="images/newlogin/SS-2.png" style="width:100%;height:100%;"/>
-				</div>
-				<div class="flexele">
-					<img src="images/newlogin/SS-3.png" style="width:100%;height:100%;"/>
-				</div>
-			</div>
+			<a href="login.php">Homepage</a> <a href="register.php">Create an account</a> <a href="tos.php">Terms of Service</a>	
 		</div>
-        <footer>
-            <p>&copy; 2016 YobCity | Privacy Policy | Terms of Service</p>
-        <footer>
-    </body>
+		<div id="main" class="row">
+			<div id="header" class="row">
+				
+			</div>
+			<div id="content" class="row">
+				<div id="left_side">
+					<h2>Welcome to Chaos City!</h2>
+					Chaos City is a Massively Multiplayer Online Role Playing Game, MMORPG for short.<br /><br />
+					In this game, you play the role of a mobster who is leading a life of crime and deception. 
+					Join gangs and team up with your friends to kick ass, or go your own way. 
+					The choices are endless.<br /><br />
+					<strong>Contact us at <a style='color:black;text-decoration:none;' href="mailto:support@chaoscityrpg.com">support@chaoscityrpg.com</a></strong><br /><br />
+					<span style='color:red;font-weight:bold;'>
+						Game is closed for now due to a major server crash.<br />
+						We have lost all data due to this and are having to start again
+						which is sad news but we will have a team working to get the updates back on 
+						and try get it back to playabale.<br /><hr width='98%' align='center' />
+						We are sorry for anyone who lost there account we will try get that sorted out for you
+						once we are back and live.<br /><hr width='98%' align='center' />
+						<span style='font-size:19px;'>Our support emial is back live 01/11/2016</span>
+					</span>
+					<!-- <strong style='color:red;'>Countdown To beta Launch (<small>11/11/2016</small>)</strong><br /> -->
+					<!-- <strong><span id="countdown" style="font-size:25px;"></span></strong><br /><br />-->
+EOF;
+				/*if($set['GameStage'] == 'Alpha' || $set['GameStage'] == 'Alpha/Beta') {
+					echo "
+					<form method='post' action='betakey.php'>
+						<table width='95%' cellspacing='1' style='text-align:center;'>
+							<tr><th>Get a Beta Key</th></tr>
+							<tr><td>Email: <input type='email' name='betakey' /></td></tr>
+							<tr><td><button>Ask For Beta Key</button></td></tr>
+						</table>
+					</form>";
+				}*/
+echo <<<EOF
+				</div>
+EOF;
+ 
+$IP = str_replace(array('/', '\\', '\0'), '', $_SERVER['REMOTE_ADDR']);
+if(file_exists('ipbans/' . $IP))
+{
+    die(
+    	"<center><img src = 'images/banned.gif ' /><div style='height: 50px'></div>
+    	<span style='color:#FFF;font-size:11px;'>Copyright &copy; {$set['game_owner']}<br />All Rights Reserved.</span></center></body></html>
+    ");
+}
+
+$year = date('Y');
+ 
+echo<<<EOF
+
+			<div id="right_side">
+			<div id="error_area"></div>
+				<div id="login_panel">
+					<h3>:: Login Panel ::</h3>
+					 
+					<form method="POST"  action="authenticate.php">
+						<input type="text" class="login" value="" name="username" placeholder="Username" />
+						<input type="password" class="login" value=""  name="password" placeholder="Password" />
+						<input type='hidden' name="verf" value='{$login_csrf}' />
+						<input type="submit" id="submit" name="submit" class="log_btn" value="Login" />
+					</form>
+					<a href="resend.php">Forgot Password?</a>
+				</div>
+			</div>
+			<div class="spacer"></div>
+		</div>
+EOF;
+echo<<<EOF
+
+		<div id="bottom_content" class="row"></div>
+	</div>
+	<div id="footer" class="row">
+		<span>Chaos City</span><br />
+		&copy; 2015+ . All Rights Reserved.<br />
+		<a href="">Privacy Policy.</a> <a href="">Terms of Services.</a> <a href="">Help Tutorial.</a> <a href="">Staff	</a>
+	</div>	
+</div>
+</body>
 </html>
-<?php
-$_SESSION['failmessage'] = "";
-?>
