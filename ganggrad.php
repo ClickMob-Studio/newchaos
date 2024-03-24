43bc26cca343131649d33a60a96f414dadae887d
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-03-24 20:43:50 +0000

    Update ganggrad.php

--- ganggrad.php
+++ ganggrad.php
@@ -1,5 +1,10 @@
 <?php
 include 'header.php';
+?>
+<div class='box_top'>Gang Gradient</div>
+						<div class='box_middle'>
+							<div class='pad'>
+                                <?php
 if ($user_class->gang == 0)
     diefun("You are not in a gang.");
 $gang_class = New Gang($user_class->gang);
