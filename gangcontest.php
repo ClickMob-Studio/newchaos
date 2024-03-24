4f6e837edb5038cd7a67f0115367bf35287a5ba0
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-03-24 20:24:17 +0000

    Update gangcontest.php

--- gangcontest.php
+++ gangcontest.php
@@ -42,7 +42,7 @@ foreach($members as $member){
 		));
     }
 }
-echo'<a href="?reset" style="color:yellow;font-size:1.5em;">[Reset]</a> - ';
+echo'<a href="?reset" style="color:orange;font-size:1.5em;">[Reset]</a> - ';
 echo'<a href="?" style="color:yellow;font-size:1.5em;">[Regular]</a> - ';
 echo'<a href="?totals" style="color:yellow;font-size:1.5em;">[Totals]</a> - ';
 echo'<a href="?presnap" style="color:yellow;font-size:1.5em;">[Snapshots]</a><br />';
