cec4912bdf8bb1734bd50391593fa46eb04bf3d7
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-03-24 19:57:08 +0000

    Update gangdetails.php

--- gangdetails.php
+++ gangdetails.php
@@ -1,5 +1,11 @@
 <?php
 include 'header.php';
+?>
+
+<div class='box_top'>Gang</div>
+						<div class='box_middle'>
+							<div class='pad'>
+<?php
 if ($user_class->gang != 0) {
     $gang_class = new Gang($user_class->gang);
     if (!empty($gang_class->banner))
