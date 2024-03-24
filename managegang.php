f71f3a25654e7c5dc0d9c8c1c5aca203a00d349f
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-03-24 20:33:02 +0000

    Update managegang.php

--- managegang.php
+++ managegang.php
@@ -1,5 +1,10 @@
 <?php
 include 'header.php';
+?>
+<div class='box_top'>Manage Gang</div>
+						<div class='box_middle'>
+							<div class='pad'>
+                                <?php
 $gang_class = new Gang($user_class->gang);
 if ($user_class->gang == 0)
     error("You aren't in a gang.");
