33cf21569d3ab1923a874335d559b27f443bac09
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-03-24 20:39:44 +0000

    Update gangupgrade.php

--- gangupgrade.php
+++ gangupgrade.php
@@ -1,5 +1,10 @@
 <?php
 include 'header.php';
+?>
+<div class='box_top'>Gang Upgrade</div>
+						<div class='box_middle'>
+							<div class='pad'>
+<?php
 if ($user_class->gang != 0) {
     include("gangheaders.php");
     $user_rank = new GangRank($user_class->grank);
