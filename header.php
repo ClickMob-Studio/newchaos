cd5da0d8040c937b86bff01f52a86e661d003759
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-03-25 20:04:21 +0000

    Update header.php

--- header.php
+++ header.php
@@ -408,7 +408,7 @@ $activeRaidsCount = $activeRaidsData['activeRaidsCount'];
 <title><?php
 		echo ucwords(str_replace($headFind, $headRepl, str_replace('.php', '', $self))); ?> - <?php
 		echo $set['game_name']; ?></title>
-<link href="assets/css/games.css?v3" type="text/css" rel="stylesheet" />
+<link href="assets/css/games.css?v4" type="text/css" rel="stylesheet" />
 <link type="text/css" rel="stylesheet" href="assets/css/template.css?v10"  />
 <script src="js/java.js?12" type="text/javascript"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
