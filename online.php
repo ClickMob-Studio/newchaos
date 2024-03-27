8a1d945c85172d430f9d839e76402db54e97fcc9
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-03-27 01:33:50 +0000

    Update online.php

--- online.php
+++ online.php
@@ -1,11 +1,15 @@
 <?
 include 'header.php';
-echo '<tr><td class="contenthead">Users Online In The Last 5 Minutes</td></tr>';
+?>
+<div class='box_top'>Users Online</div>
+						<div class='box_middle'>
+							<div class='pad'>
+                                <?php
 $result = mysql_query("SELECT * FROM `grpgusers` ORDER BY `lastactive` DESC");
 echo '<table><tr><td class="contentcontent">';
 	while($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
 		$secondsago = time()-$line['lastactive'];
-		if ($secondsago<=300) {
+		if ($secondsago<=360) {
 			$user_online = new User($line['id']);
             echo "<tr>
             <td><img src='{$user['avatar']}' height='50' width='50'></td>
