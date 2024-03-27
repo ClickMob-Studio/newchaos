b92c3852df360fb193f7e7b9e5e45deba82ab3fc
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-03-27 01:32:32 +0000

    Update online.php

--- online.php
+++ online.php
@@ -1,13 +1,23 @@
-<?php
+<?
 include 'header.php';
-echo '<tr><td class="contenthead">Users Online In The Last 60 Minutes</td></tr>';
+echo '<tr><td class="contenthead">Users Online In The Last 5 Minutes</td></tr>';
 $result = mysql_query("SELECT * FROM `grpgusers` ORDER BY `lastactive` DESC");
 echo '<tr><td class="contentcontent">';
 	while($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
 		$secondsago = time()-$line['lastactive'];
-		if ($secondsago<=3600) {
+		if ($secondsago<=300) {
 			$user_online = new User($line['id']);
-			echo "<div>".$user_online->formattedname . " ". howlongago($user_online->lastactive)."</div>";
+            echo "<tr>
+            <td><img src='{$user['avatar']}' height='50' width='50'></td>
+            <td><b><i>{$user_online->id}</i></b></td>
+            <td>{$user_online->formattedname}</td>
+            <td>{$user_online->type}</td>
+            <td>{$user_online->formattedgang}</td>
+            <td>{$user_online->level}</td>
+            <td>{$user_online->cityname}</td>
+            <td>{$user->lastactive}</td>
+        </tr>";
+			
 		}
 	}
 echo '</td></tr>';
