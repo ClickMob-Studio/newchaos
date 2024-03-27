055511caa01af7a96f5a3fcbcf09d00662a183f1
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-03-27 00:43:38 +0000

    Revert "Merge branch 'main' of https://github.com/codemonkey2704/newchaos"
    This reverts commit 3c336451155f0ac21f6764e90b89cba667279c9d, reversing
    changes made to 90c05e886f1f2d519a20654bd3b7647f71aa8c23.

--- online.php
+++ online.php
@@ -82,7 +82,14 @@ if (!$m->get('24hour')) {
     }
     $m->set('24hour', $store, 10);
 }
-echo '<div class="contenthead floaty">';
+
+?>
+
+<div class="box_top">Online</div>
+<div class="box_middle">
+    <div class="pad">
+        <?php
+echo '<div>';
 echo '    <span style="margin: 0; line-height: 27px; text-transform: uppercase; font-size: 20px; text-align: left; text-indent: 25px;"><h4>Players Online</h4></span>';
 
 echo '<table id="newtables" style="width:100%;">
@@ -121,5 +128,10 @@ $city = $user['cityname'];
 }
 
 echo '</table></td></tr></div>';
+?>
+
+    </div>
+</div>
+<?php
 include 'footer.php';
 ?>
