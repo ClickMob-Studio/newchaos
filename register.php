3f5ad86792058741f3dab4ee195806271984e567
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-03-28 06:11:48 +0000

    up

--- register.php
+++ register.php
@@ -31,7 +31,7 @@ $metadesc = 'BRAND NEW MMORPG';
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<title>{$set['game_name']} - Register</title>
+	<title>Chaos City - Register</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 	<script type="text/javascript" src="{$set['jquery_location']}"></script>
 	<script type="text/javascript" src="assets/js/register.js"></script>
