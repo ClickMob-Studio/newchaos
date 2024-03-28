3bf9ca402d8340f52ca549b5461288d6c950372a
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-03-28 17:21:17 +0000

    Update numbergame.php

--- numbergame.php
+++ numbergame.php
@@ -1,54 +1,70 @@
 <?php
 include "header.php";
-?>
-	
-	<div class='box_top'>Numbers Game</div>
-						<div class='box_middle'>
-							<div class='pad'>
-								<?php
-$iir = mysql_query("SELECT * FROM `grpgusers` WHERE `id` = '{$_SESSION['id']}' LIMIT 1") or mysql_error();
-$ir = mysql_fetch_array($iir);
-$_GET['ID'] = abs((int) $_GET['ID']);
-$userid = $_SESSION['id'];
-if ($ir['jailtime'] or $ir['hospital']) {
-    die("<br />This page is unavailable while in hospital or jail.<br /><br /><hr width=80%> <a href='index.php'>Home</a><hr width=80%>");
-}
-$nogame2 = mysql_query("SELECT * FROM numbergame WHERE userid=$user_class->id");
-$no2 = mysql_num_rows($nogame2);
 
-$_GET['take'] = abs((int) $_GET['take']);
-$getid = $_GET['take'];
-if (!$getid) {
-    print "<b>Mini Competition! Current Prize: 1,000 Points<br />Guess a number from 1 to 25<br />You will recieve a event if you win.</b><br /><br /><table width='30%' cellspacing=1 class='myTable'><tr><td class='contenthead' align='center'>Number</td><td class='contenthead' align='center'>Player</td></tr>";
-    $q = mysql_query("SELECT * FROM numbergame ORDER BY number ASC") or mysql_error();
-    while ($r = mysql_fetch_array($q)) {
-        $usersid = ($r['userid']);
-        $user = mysql_query("SELECT * FROM grpgusers WHERE id={$r['userid']}");
-#$user=mysql_query("SELECT * FROM grpgusers WHERE id=$user_class->id");
-        $u = mysql_fetch_array($user);
-        if ($r['userid'] == 0) {
-            if ($no2 > 0) {
-                $u['username'] = "-";
-            } else {
-                $u['username'] = "[<a href='numbergame.php?take={$r['number']}'>select</a>]";
+?>
+
+<div class='box_top'>Numbers Game</div>
+<div class='box_middle'>
+    <div class='pad'>
+        <?php
+        $stmt = $db->prepare("SELECT * FROM `grpgusers` WHERE `id` = :id LIMIT 1");
+        $stmt->bindParam(':id', $_SESSION['id']);
+        $stmt->execute();
+        $ir = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $_GET['ID'] = abs((int)$_GET['ID']);
+        $userid = $_SESSION['id'];
+        if ($ir['jailtime'] or $ir['hospital']) {
+            die("<br />This page is unavailable while in hospital or jail.<br /><br /><hr width=80%> <a href='index.php'>Home</a><hr width=80%>");
+        }
+
+        $stmt = $db->prepare("SELECT * FROM numbergame WHERE userid=:userid");
+        $stmt->bindParam(':userid', $user_class->id);
+        $stmt->execute();
+        $no2 = $stmt->rowCount();
+
+        $_GET['take'] = abs((int)$_GET['take']);
+        $getid = $_GET['take'];
+        if (!$getid) {
+            print "<b>Mini Competition! Current Prize: 1,000 Points<br />Guess a number from 1 to 25<br />You will receive an event if you win.</b><br /><br /><table width='30%' cellspacing=1 class='myTable'><tr><td class='contenthead' align='center'>Number</td><td class='contenthead' align='center'>Player</td></tr>";
+            $stmt = $db->query("SELECT * FROM numbergame ORDER BY number ASC");
+            while ($r = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $stmt_user = $db->prepare("SELECT * FROM grpgusers WHERE id=:id");
+                $stmt_user->bindParam(':id', $r['userid']);
+                $stmt_user->execute();
+                $u = $stmt_user->fetch(PDO::FETCH_ASSOC);
+                if ($r['userid'] == 0) {
+                    if ($no2 > 0) {
+                        $u['username'] = "-";
+                    } else {
+                        $u['username'] = "[<a href='numbergame.php?take={$r['number']}'>select</a>]";
+                    }
+                } else {
+                    $u['username'] = "<a href='profiles.php?id={$u['id']}'>{$u['username']}</a>";
+                }
+                print "\n<tr align=center><td class=contentcontent>{$r['number']}</td><td class=contentcontent>{$u['username']}</td></tr>";
+                $current_row = 1 - $current_row;
             }
         } else {
-            $u['username'] = "<a href='profiles.php?id={$u['id']}'>{$u['username']}</a>";
+            $stmt = $db->prepare("SELECT * FROM numbergame WHERE number=:number");
+            $stmt->bindParam(':number', $getid);
+            $stmt->execute();
+            $no = $stmt->fetch(PDO::FETCH_ASSOC);
+            if ($no['userid'] != 0) {
+                die("<br />This number is already taken.<br /><br /><hr width=80%> <a href='numbergame.php'>Number Game</a><hr width=80%>");
+            }
+            if ($no2 > 0) {
+                die("<br />You have already chosen a number.<br /><br /><hr width=80%> <a href='numbergame.php'>Number Game</a><hr width=80%>");
+            }
+            $stmt = $db->prepare("UPDATE numbergame SET userid=:userid WHERE number=:number");
+            $stmt->bindParam(':userid', $user_class->id);
+            $stmt->bindParam(':number', $getid);
+            $stmt->execute();
+            print "<br />You have taken the number $getid<br /><br /><hr width=80%> <a href='numbergame.php'>Number Game</a><hr width=80%>";
         }
-        print "\n<tr align=center><td class=contentcontent>{$r['number']}</td><td class=contentcontent>{$u['username']}</td></tr>";
-        $current_row = 1 - $current_row;
-    }
-} else {
-    $nogame = mysql_query("SELECT * FROM numbergame WHERE number=$getid");
-    $no = mysql_fetch_row($nogame);
-    if ($no['userid'] != 0) {
-        die("<br />This number is already taken.<br /><br /><hr width=80%> <a href='numbergame.php'>Number Game</a><hr width=80%>");
-    }
-    if ($no2 > 0) {
-        die("<br />You have already chosen a number.<br /><br /><hr width=80%> <a href='numbergame.php'>Number Game</a><hr width=80%>");
-    }
-    mysql_query("UPDATE numbergame SET userid=$user_class->id WHERE number=$getid");
-    print "<br />You have taken the number $getid<br /><br /><hr width=80%> <a href='numbergame.php'>Number Game</a><hr width=80%>";
-}
+
+?>
+</table>
+<?php
 include 'footer.php';
 ?>
