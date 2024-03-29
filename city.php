21dc23c5dc4dd34ab2b4c16309a5087e954f6a17
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-03-29 09:31:45 +0000

    fix king

--- city.php
+++ city.php
@@ -77,7 +77,12 @@ $admin_ids = array_map(function($a) {
             <h4>VACANT</h4>
             <p>King of <!_-cityname-_!></p>
         <?php endif; ?>
-<a href="/attack.php?attack=<?php echo $king_result['id']; ?>" class="challenge-btn">Challenge</a>
+        <?php 
+        $csrf = md5(uniqid(rand(), true));
+        $_SESSION['csrf'] = $csrf;
+        ?>
+        
+<a href="/attack.php?attack=<?php echo $king_result['id']; ?> &csrf=<?php echo $csrf;?>" class="challenge-btn">Challenge</a>
 
 </div>
     <!-- Queen of the City -->
