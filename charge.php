536392bc8fc497e4568ba030dcf7d9d575bdd756
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-03-29 13:25:06 +0000

    Update charge.php

--- charge.php
+++ charge.php
@@ -6,7 +6,7 @@ require_once('stripe/init.php');
 \Stripe\Stripe::setApiKey('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');
 
 $token = $_POST['stripeToken'];
-
+var_dump($token);
 $charge = \Stripe\Charge::create([
     'amount' => 1000, // Amount in cents
     'currency' => 'usd',
