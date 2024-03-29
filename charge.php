01d273b69dcc51ee92207e448933c5e22c05aec5
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-03-29 13:21:46 +0000

    Update charge.php

--- charge.php
+++ charge.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require "header.php";
 require_once('stripe/init.php');
 
 \Stripe\Stripe::setApiKey('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');
