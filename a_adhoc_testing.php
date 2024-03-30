2d297b14d685893b663a307b06ebe0518c95c82f
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-03-30 16:00:00 +0000

    up

--- a_adhoc_testing.php
+++ a_adhoc_testing.php
@@ -6,11 +6,13 @@ if (isset($_GET['key']) && $_GET['key'] === 'wetesters') {
     $king_result = mysql_query("SELECT `id`, `city` FROM `grpgusers` WHERE `king` > 0");
     while ($line = mysql_fetch_array($king_result)) {
         $cityId = $king_result['city'];
+        echo $cityId . '-';
 
         $city_query = mysql_query("SELECT owned_points FROM cities WHERE id = '" . mysql_real_escape_string($king_result['city']) . "' LIMIT 1");
         $city_result = mysql_fetch_assoc($city_query);
 
         if ($city_result['owned_points'] > 0) {
+            echo $city_result['owned_points'];
             mysql_query("UPDATE `grpgusers` SET `points` = `points` + " . $city_result['owned_points'] . " WHERE `id` = " . $king_result['id']);
             Send_event($king_result['id'], "You have won " . number_format($city_result['owned_points'], 0) . " points for being the King!");
         }
