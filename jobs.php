0a905f1d9402c20f929b54e125c46943759da029
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-03-30 09:23:20 +0000

    Update jobs.php

--- jobs.php
+++ jobs.php
@@ -1,5 +1,12 @@
 <?php
-include 'header.php';
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+if($_SESSION['id'] == 1){
+include 'headernew.php';
+}else{
+	include 'header.php';
+}
 ?>
 	
 	<div class='box_top'>Jobs</div>
