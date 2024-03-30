bebf692afd3f79c0f97da7bb22fb2546e63436bd
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-03-30 15:17:37 +0000

    up

--- city.php
+++ city.php
@@ -84,7 +84,7 @@ $admin_ids = array_map(function($a) {
             <img src="<?php echo htmlspecialchars($king_result['avatar']); ?>" style="width: 100px; height: 100px;" alt="King's Avatar" class="user-avatar">
             <h4>King of <!_-cityname-_!></h4>
             <p><strong><?php echo formatName($king_result['id']); ?></strong></p>
-            <a href="/attack.php?attack=<?php echo $king_result['id']; ?>&csrf=<?php echo $csrf;?>" class="challenge-btn">Challenge</a>
+            <a href="/attack.php?attack=<?php echo $king_result['id']; ?>&csrf=<?php echo $csrf;?>" class="challenge-btn" style="text-decoration: underline;">Challenge</a>
 
         <?php else: ?>
             <img src="mlordsimages/vacant.png" style="width: 100px; height: 100px;" alt="No King" class="vacant-throne">
@@ -94,7 +94,7 @@ $admin_ids = array_map(function($a) {
         <?php endif; ?>
         <br />
 
-        <p style="font-weight: bold;">By being King of this City you will earn <?php echo number_format($city_query['owned_points'], 0) ?> points on rollover.</p>
+        <p style="font-weight: bold; margin-top: 5px;">By being King of this City you will earn <?php echo number_format($city_query['owned_points'], 0) ?> points an hour.</p>
        
 
 
@@ -105,7 +105,7 @@ $admin_ids = array_map(function($a) {
             <img src="<?php echo htmlspecialchars($queen_result['avatar']); ?>" style="width: 100px; height: 100px;" alt="Queen's Avatar" class="user-avatar">
             <h4>Queen of <!_-cityname-_!></h4>
             <p><strong><?php echo formatName($queen_result['id']); ?></strong></p>
-            <a href="/attack.php?attack=<?php echo $queen_result['id']; ?>&csrf=<?php echo $csrf;?>"  class="challenge-btn">Challenge</a>
+            <a href="/attack.php?attack=<?php echo $queen_result['id']; ?>&csrf=<?php echo $csrf;?>"  class="challenge-btn" style="text-decoration: underline;">Challenge</a>
         
             <?php else: ?>
             <img src="mlordsimages/vacant.png" style="width: 100px; height: 100px;" alt="No Queen" class="vacant-throne">
@@ -114,7 +114,7 @@ $admin_ids = array_map(function($a) {
             <?php endif; ?>
         <br />
 
-        <p style="font-weight: bold;">By being Queens of this City you will earn <?php echo number_format($city_query['owned_points'], 0) ?> points on rollover.</p>
+        <p style="font-weight: bold; margin-top: 5px">By being Queens of this City you will earn <?php echo number_format($city_query['owned_points'], 0) ?> points an hour.</p>
           </div>
 </div>
 
