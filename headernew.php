a3ecc676f2f138f2069128b9c2a4d4c00ed6182f
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-03-30 08:32:55 +0000

    Update headernew.php

--- headernew.php
+++ headernew.php
@@ -409,7 +409,11 @@ $activeRaidsCount = $activeRaidsData['activeRaidsCount'];
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"></script>
 	<script src="asset/js/app.<?php echo CACHEBURST?>.js"></script>
-
+<style>
+	a{
+		text-decoration: none;
+	}
+	</style>
 </head>
 <body>
 	<header class="mainHeader">
