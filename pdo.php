bb4c665e44aefb0238ebbeb9630d28ecd0f3ba1c
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-03-31 12:25:40 +0100

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -9,6 +9,4 @@ $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected successfully";
 }
