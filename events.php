273d76ff1c9c402448524ea3cbf9aa9bb7f7f9dc
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-03-31 12:00:41 +0100

    Update events.php

--- events.php
+++ events.php
@@ -1,62 +1,84 @@
-<?php
+ <?php
 include 'header.php';
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
+$result2 = mysql_query("UPDATE `events` SET `viewed` = '2' WHERE `to`=$user_class->id");
+if (isset($_GET['deleteall']) && $_GET['deleteall'] == 1) {
+    $result = mysql_query("DELETE FROM `events` WHERE `to` = $user_class->id");
+    echo Message("All your events have been deleted.");
+}
+if (isset($_GET['delete']) && $_GET['delete'] != "") {
+    $result = mysql_query("DELETE FROM `events` WHERE `id`='{$_GET['delete']}' AND `to` = $user_class->id");
+    echo Message("You have deleted that event.");
+}
 ?>
-<div class='box_top'>Events</div>
-<div class='box_middle'>
-    <div class='pad'>
-        <?php
-        // Assuming $conn is your PDO connection object
-        $userId = $user_class->id;
-        $conn->prepare("UPDATE `events` SET `viewed` = '2' WHERE `to` = :userId")->execute([':userId' => $userId]);
+ 
+ <form method="GET" class="d-inline float-right">
+     <input type="text" id="filterInput" placeholder="Search for an Event" name="search" <?php if (isset($_GET['search'])){?>value="<?php echo $_GET['search'] ?>"<?php } ?>>
+     <input type="submit" value="Search">
+ </form>
 
-        if (isset($_GET['deleteall']) && $_GET['deleteall'] == 1) {
-            $conn->prepare("DELETE FROM `events` WHERE `to` = :userId")->execute([':userId' => $userId]);
-            echo Message("All your events have been deleted.");
-        }
+<hr>
+<div class="contenthead floaty">
+<span style="margin: 0; line-height: 27px; text-transform: uppercase; font-size: 20px; text-align: left; text-indent: 25px;">
+<h4>Your Events</h4></span><table id="newtables" style="width:100%; text-align: left;">
+<tr>
+<th><b>Description</b></th>
+<th><b>Recieved</b></th>
+</tr>
+<style type="text/css">
+ background-color: #333; /* Dark background */
+    color: white; /* Light text */
+    padding: 20px;
+    border-radius: 10px; /* Rounded corners */
+    box-shadow: 0 -4px 8px rgba(0, 0, 0, 0.2); /* Subtle shadow at the top */
+    padding: 15px 5px 10px;
+    text-align: center;
 
-        if (isset($_GET['delete']) && $_GET['delete'] != "") {
-            $deleteId = $_GET['delete'];
-            $conn->prepare("DELETE FROM `events` WHERE `id` = :deleteId AND `to` = :userId")->execute([':deleteId' => $deleteId, ':userId' => $userId]);
-            echo Message("You have deleted that event.");
-        }
-        ?>
+}
+</style>
+<?php
+$result      = mysql_query("SELECT COUNT(*) FROM `events` WHERE `to` = $user_class->id");
+$r           = mysql_fetch_row($result);
+$numrows     = $r[0];
+$rowsperpage = 30;
+$totalpages  = ceil($numrows / $rowsperpage);
+$searchString = null;
+if ($totalpages <= 0)
+    $totalpages = 1;
+else
+    $totalpages = ceil($numrows / $rowsperpage);
+if (isset($_GET['page']) && is_numeric($_GET['page']))
+    $currentpage = (int) $_GET['page'];
+else
+    $currentpage = 1;
+if ($currentpage > $totalpages)
+    $currentpage = $totalpages;
+if ($currentpage < 1)
+    $currentpage = 1;
+if (isset($_GET['search']) && $_GET['search'] != ''){
+    $searchString = '%'.$_GET['search'].'%';
+}
+$offset = ($currentpage - 1) * $rowsperpage;
+if ($searchString == null) {
+    $res = mysql_query("SELECT * from `events` WHERE `to` = $user_class->id ORDER BY `timesent` DESC LIMIT $offset, $rowsperpage");
 
-        <form method="GET" class="d-inline float-right">
-            <input type="text" id="filterInput" placeholder="Search for an Event" name="search" value="<?= isset($_GET['search']) ? htmlspecialchars($_GET['search']) : '' ?>">
-            <input type="submit" value="Search">
-        </form>
+    // if (!$res = $m->get('events.' . $id . '.' . $offset . '.' . $rowsperpage)) {
+    //     $res = mysql_query("SELECT * from `events` WHERE `to` = $user_class->id ORDER BY `timesent` DESC LIMIT $offset, $rowsperpage");
+    //     $m->set('events.' . $id . '.' . $offset . '.' . $rowsperpage, $res, false, 120);
+    // }
 
-        <hr>
-        <table id="newtables" style="width:100%;">
-            <tr>
-                <th><b>Description</b></th>
-                <th><b>Received</b></th>
-            </tr>
-            <?php
-            $searchString = isset($_GET['search']) ? '%' . $_GET['search'] . '%' : null;
-            $stmt = $conn->prepare("SELECT * FROM `events` WHERE `to` = :userId " . ($searchString ? "AND `text` LIKE :searchString " : "") . "ORDER BY `timesent` DESC LIMIT :offset, :rowsperpage");
-            $stmt->bindValue(':userId', $userId, PDO::PARAM_INT);
-            if ($searchString) {
-                $stmt->bindValue(':searchString', $searchString, PDO::PARAM_STR);
-            }
-            $stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
-            $stmt->bindValue(':rowsperpage', $rowsperpage, PDO::PARAM_INT);
-            $stmt->execute();
-            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-            foreach ($results as $row) {
-                $text = str_replace('[-_USERID_-]', formatName($row['extra']), $row['text']);
-                // Assuming formatName() is defined elsewhere
-                echo "<tr><td width='67%'>" . htmlspecialchars($text) . "</td><td width='31%'>" . date("d F Y, g:ia", $row['timesent']) . "</td><td width='2%'><a href='events.php?delete=" . htmlspecialchars($row['id']) . "'><span class='delete'>&nbsp;X&nbsp;</span></a></td></tr>";
-            }
-            ?>
-        </table>
-        <span style="display: none" id="dataHolder" data-offset="<?= htmlspecialchars($offset) ?>" data-rowsPerPage="30" data-userId ="<?= htmlspecialchars($userId) ?>"></span>
-        <br /><br />
+} else {
+    $res = mysql_query("SELECT * from `events` WHERE `to` = $user_class->id AND `text` like '$searchString' ORDER BY `timesent` DESC LIMIT $offset, $rowsperpage");
+}
+while ($row = mysql_fetch_array($res)) {
+    $text       = str_replace('[-_USERID_-]', formatName($row['extra']), $row['text']);
+    //$text       = str_replace('[-_GANGID_-]', gangName($row['extra']), $text);
+    echo "<tr><td width='67%'>" . $text . "</td><td width='31%'>" . date("d F Y, g:ia", $row['timesent']) . "</td><td width='2%'><a href='events.php?delete={$row['id']}'><span class='delete'>&nbsp;X&nbsp;</span></a></td></tr>";
+}
+?>
+</table>
+<span style="display: none" id="dataHolder" data-offset="<?php echo $offset ?>" data-rowsPerPage="30" data-userId ="<?php echo $user_class->id ?>"></span>
+<br />
 <table width="100%">
 <tr>
 <td align="left">
@@ -78,7 +100,6 @@ if ($currentpage < $totalpages)
 </tr>
 </table>
 </td></tr>
-</div>
 <?php
 include 'footer.php';
 function gangName($id){
