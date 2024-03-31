be8c46eff6a80c4ace4facdc45efe29b1e756ea3
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-03-31 12:00:20 +0100

    Update events.php

--- events.php
+++ events.php
@@ -1,5 +1,9 @@
 <?php
 include 'header.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 ?>
 <div class='box_top'>Events</div>
 <div class='box_middle'>
