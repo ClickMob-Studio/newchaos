395c105994775a3ebd87a93424611dfdf8d99ee1
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-03-31 12:39:25 +0100

    Update ajax_crimes2.php

--- ajax_crimes2.php
+++ ajax_crimes2.php
@@ -2,7 +2,9 @@
 
 //header('Content-type: application/json');
 session_start();
-error_reporting(0);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include "classes.php";
 include "database/pdo_class.php";
 
