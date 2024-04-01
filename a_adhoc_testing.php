a6db8a1616c9d997ff643ff85c638940a60bff39
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-04-01 14:32:33 +0100

    u

--- a_adhoc_testing.php
+++ a_adhoc_testing.php
@@ -5,6 +5,7 @@ include 'header.php';
 if (isset($_GET['key']) && $_GET['key'] === 'wetesters') {
     $result = mysql_query("SELECT `id` FROM `grpgusers` WHERE `is_jail_bot` = 1");
     while ($line = mysql_fetch_array($result)) {
+        echo  $line['id'];
         mysql_query("UPDATE `grpgusers` SET `jail` = 300 WHERE `id` = " . $line['id']);
 
         Send_Event(2, "Jail Bots Ran");
