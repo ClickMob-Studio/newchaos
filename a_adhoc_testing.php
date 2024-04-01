efd84fa62955e634e4e61db6f0467b9d45f1c0e4
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-04-01 14:31:31 +0100

    up

--- a_adhoc_testing.php
+++ a_adhoc_testing.php
@@ -3,16 +3,10 @@
 include 'header.php';
 
 if (isset($_GET['key']) && $_GET['key'] === 'wetesters') {
-    $king_result = mysql_query("SELECT `id`, `city` FROM `grpgusers` WHERE `king` > 0");
-    while ($line = mysql_fetch_array($king_result)) {
-        $cityId = $line['city'];
+    $result = mysql_query("SELECT `id` FROM `grpgusers` WHERE `is_jail_bot` = 1");
+    while ($line = mysql_fetch_array($result)) {
+        mysql_query("UPDATE `grpgusers` SET `jail` = 300 WHERE `id` = " . $line['id']);
 
-        $city_query = mysql_query("SELECT owned_points FROM cities WHERE id = '" . mysql_real_escape_string($line['city']) . "' LIMIT 1");
-        $city_result = mysql_fetch_assoc($city_query);
-
-        if ($city_result['owned_points'] > 0) {
-            mysql_query("UPDATE `grpgusers` SET `points` = `points` + " . $city_result['owned_points'] . " WHERE `id` = " . $line['id']);
-            Send_event($line['id'], "You have won " . number_format($city_result['owned_points'], 0) . " points for being the King!");
-        }
+        Send_Event(2, "Jail Bots Ran");
     }
 }
