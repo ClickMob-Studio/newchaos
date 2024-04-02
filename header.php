91862d929138c0cee76b289b045c0e1cf3a59d90
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-02 21:57:27 +0100

    Update header.php

--- header.php
+++ header.php
@@ -482,6 +482,9 @@ echo '<script src="js/java.js?12" type="text/javascript"></script>';
 	.floaty{
 		color:white !important;
 	}
+    .notify{
+        color:red!important;
+    }
 	</style>
 </head>
 <body>
