83c93543927c172617b9d07174d8b158ab1ea990
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-02 17:05:12 +0100

    Update ajax_crimes21.php

--- ajax_crimes21.php
+++ ajax_crimes21.php
@@ -7,9 +7,6 @@ $ignoreForAjax = true;
 include "classes.php";
 include "database/pdo_class.php";
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 $m = new Memcache();
 $m->addServer('127.0.0.1', 11211, 33);
 
