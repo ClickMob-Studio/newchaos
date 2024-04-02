37b5bc599b4d9be93cc385b8c52a718ccb606cbc
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-02 17:09:18 +0100

    Update newcrimes.php

--- newcrimes.php
+++ newcrimes.php
@@ -1,5 +1,12 @@
 <?php
-error_reporting(0);
+if($user_class->id == 1){
+    ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+}else{
+    error_reporting(0);
+}   
 include 'header.php';
 ?>
 
