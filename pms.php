fc246982a6c5254508d11e81c278f4aa04e449aa
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-04-02 23:28:24 +0100

    u

--- pms.php
+++ pms.php
@@ -226,8 +226,8 @@ if (isset($_GET['view']) && $_GET['view'] == "inbox") {
                 $bold = "";
                 $bold2 = "&nbsp;[mail bomb]";
             } elseif ($row['viewed'] == 1) {
-                $bold = "<b>";
-                $bold2 = "</b>&nbsp;<style='color: red;'>[new]</style>";
+                $bold = "<style='color: red;'><b>";
+                $bold2 = "</b>&nbsp;[new]</style>";
             } elseif ($row['reported'] == 1) {
                 $bold = "";
                 $bold2 = "&nbsp;[reported]";
