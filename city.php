1ba2e28231fcc1a70b7ef88d84f689347e93bc23
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-02 20:40:42 +0100

    Update city.php

--- city.php
+++ city.php
@@ -2,11 +2,8 @@
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
-if($_SESSION['id'] == 1){
-include 'headernew.php';
-}else{
+
 	include 'header.php';
-}
 include_once 'databse/pdo_class.php';
 ?>
 <style>
