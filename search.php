5891ca019680d0d5063c01d0cd7841ec2f47cc5c
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-02 20:21:18 +0100

    Update search.php

--- search.php
+++ search.php
@@ -201,7 +201,7 @@ if (isset($_POST['id'])) {
 	if (!empty($search['money']))
         $sql .= " AND money > '{$search['money']}' ";
     if ($search['attack'] == 1)
-        $sql .= " AND hospital = 0 AND jail = 0 AND (gang <> $user_class->gang || gang = 0) AND signuptime < unix_timestamp() - (86400 * 1) AND admin <> 1 AND hp > (50*level)/4 AND id <> $user_class->id ";
+        $sql .= " AND hospital = 0 AND jail = 0 AND (gang <> $user_class->gang || gang = 0)  AND admin < 1 AND hp > (50*level)/4 AND id <> $user_class->id ";
     else if ($search['attack'] == 2)
         $sql .= " AND hospital > 0 ";
     $time = time() - 900;
