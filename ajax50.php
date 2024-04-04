af483df3fa22f3bc28090da0632025731ba855ba
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-04 22:54:23 +0100

    Update ajax50.php

--- ajax50.php
+++ ajax50.php
@@ -146,7 +146,7 @@ if(isset($_POST['update'])){
                     $new[$row['currency']] .= '<button onclick="takeaway(' . $row['id'] . ');">Remove Bet</button>';
                 else
                     $new[$row['currency']] .= '<button onclick="take(' . $row['id'] . ');">Take Bet</button>';
-            $new[$row['currency']] .= '</div>';
+            $new[$row['currency']] .= '</tr>';
         }
     }
     foreach($idsarr as $idssub){
