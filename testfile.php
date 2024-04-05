51b51e00b2a77926053d06c0e9e39cce00f9c1c1
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-05 09:09:18 +0100

    Create testfile.php

---
+++ testfile.php
@@ -0,0 +1,38 @@
+<?php
+chaoscit_user
+// Database info
+$dbHost     = 'localhost';
+$dbUsername = 'chaoscit_user';
+$dbPassword = '3lrKBlrfMGl2ic14';
+$dbName     = 'chaoscit_game';
+
+// Backup directory
+$backupDir = '/home/chaoscit/backup';
+// Number of days to keep backup files
+$fileRetentionDays = 7;
+
+// Set the timezone
+date_default_timezone_set('Your/Timezone');
+
+// Generate a filename for the backup file based on the current date and time
+$backupFile = $backupDir . '/db-backup-' . date("Y-m-d-H-i-s") . '.sql';
+
+// Command to perform backup
+$command = "mysqldump --opt -h $dbHost -u $dbUsername -p$dbPassword $dbName > $backupFile";
+
+// Execute the command
+system($command, $output);
+
+// Delete files older than $fileRetentionDays
+if ($handle = opendir($backupDir)) {
+    while (false !== ($file = readdir($handle))) {
+        $fileLastModified = filemtime($backupDir . '/' . $file);
+        // If file older than retention period and is a SQL file, delete it
+        if ((time() - $fileLastModified > $fileRetentionDays * 24 * 60 * 60) && preg_match('/\.sql$/', $file)) {
+            unlink($backupDir . '/' . $file);
+        }
+    }
+    closedir($handle);
+}
+
+?>
