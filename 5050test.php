9238d177471f987c5c40caab17677226e0c67b43
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-05 14:24:42 +0100

    Update 5050test.php

--- 5050test.php
+++ 5050test.php
@@ -8,21 +8,21 @@ require "header.php";
 <p>Here you can bet against each other and try and win the other person things!</p>
 
 <div class="row">
-    <div class="col-4 col-sm-12 ">
+    <div class="col-sm-12 col-md-4 col-lg-4 ">
     <h1>CasH Bets</h1>
     <form action="5050.php" method="post">
         <input type="number" name="amnt" size="5" maxlength="20" min="10000" />
         <input type="submit" name="bcash" value="Bet" />
     </form>
 </div>
-    <div class="col-4 col-sm-12 ">
+    <div class="col-4 col-md-4 col-lg-4 ">
     <h1>Points Bets</h1>
     <form action="5050.php" method="post">
         <input type="number" name="amnt" size="5" maxlength="20" min="10000" />
         <input type="submit" name="bcash" value="Bet" />
     </form>
     </div>
-    <div class="col-4 col-sm-12 ">
+    <div class="col-4 col-md-4 col-lg-4 ">
     <h1>Credit Bets</h1>
     <form action="5050.php" method="post">
         <input type="number" name="amnt" size="5" maxlength="20" min="10000" />
