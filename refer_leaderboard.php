8c79c751a15d17a8053c9178d9dbe28df83f64ba
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-04-06 13:00:49 +0100

    u

---
+++ refer_leaderboard.php
@@ -0,0 +1,30 @@
+<?php
+include 'header.php';
+?>
+<div class='box_top'>Referral Competition</div>
+<div class='box_middle'>
+    <div class='pad'>
+        <center>
+            <p>Mobsters, it's time to run a little competition to help us grow Chaos City, so welcome to our Referral Competition.</p>
+
+            <p><strong>Rules:</strong></p>
+            <ul>
+                <li>The competition will run until 15th April.</li>
+                <li>Referrals must be validated by an Admin before they are counted.</li>
+                <li>Only referrals after 06/04/2024 12:00 will count.</li>
+            </ul>
+
+            <p><strong>Prizes:</strong></p>
+            <ul>
+                <li><strong>1st: 1,000 gold</strong></li>
+                <li><strong>2nd: 400 gold</strong></li>
+                <li><strong>3rd: 100 gold</strong></li>
+            </ul>
+
+        </center>
+    </div>
+</div>
+
+<?php
+include 'footer.php';
+?>
