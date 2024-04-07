1343d24f6caa260a1a19de3d7f839d5efa54428e
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-04-07 03:19:32 +0100

    u

--- ajax_crimes2.php
+++ ajax_crimes2.php
@@ -142,7 +142,7 @@ if (isset($_POST['id']) || isset($input['id'])) {
     $star_bonus_exp = $exp * $star_level * $bonus_exp_per_star_level;
     $exp += $star_bonus_exp;
 
-    $crimeexpbonus = 0;444
+    $crimeexpbonus = 0;
     if ($user_class->crimeexpboost > 1) {
         $crimeexpbonus += 0.2;
         $crimeexpbonus += ($user_class->crimeexpboost - 1) * 0.0333;
@@ -213,7 +213,6 @@ if (isset($_POST['id']) || isset($input['id'])) {
         if ($cost < 10) {
             $cost = 10;
         }
-
         if ($cost > $user_class->points) {
             return 0;
         } else if ($user_class->points < 10) {
@@ -223,6 +222,7 @@ if (isset($_POST['id']) || isset($input['id'])) {
         $debug['cost'] = $cost;
 
         $user_class->nerve = $user_class->maxnerve;
+
 
 //        if ($user_class->id === 2) {
 //            Send_Event(2, $nerveneeded . ' - ' . $user_class->maxnerve . ' - ' . $cost, 2);
