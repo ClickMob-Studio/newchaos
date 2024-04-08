c9850c56b34cf59f856d536be7ce9c144117b3e8
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-08 22:51:36 +0100

    Update manageranks.php

--- manageranks.php
+++ manageranks.php
@@ -25,7 +25,9 @@ $gang_class = new Gang($user_class->gang);
 if ($user_class->gang == 0)
     diefun("You aren't in a gang.");
 $user_rank = new GangRank($user_class->grank);
-
+if($user_class->id -- 1){
+    var_dump($user_rank);
+}
 if ($user_class->gangleader != $user_class->id || $user_rank->ranks < 1)
     diefun("You don't have permission to be here!");
 if (isset($_POST['deleterank'])) {
