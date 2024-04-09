9644293ec141cd7ff05134f1aa9e48bc56cf16ff
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-04-09 10:41:26 +0100

    u

--- includes/functions.php
+++ includes/functions.php
@@ -1840,5 +1840,6 @@ function addItemTempUse($user_class, $field, $qty = 1)
 {
     $itemTempUse = getItemTempUse($user_class);
 
+    echo "UPDATE item_temp_use {$field} = {$field} + {$qty} WHERE id = " . $itemTempUse['id']; exit;
     mysql_query("UPDATE item_temp_use {$field} = {$field} + {$qty} WHERE id = " . $itemTempUse['id']);
 }
