158e57e9fa1e97c057eebb7280872827171938b2
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-10 09:42:25 +0100

    Update cron_hour1111221.php

--- cron_hour1111221.php
+++ cron_hour1111221.php
@@ -234,4 +234,86 @@ while ($line = mysql_fetch_array($queen_result)) {
 }
 
 mysql_query("UPDATE `grpgusers` SET `jail_bot_credits` = `jail_bot_credits` + 50 WHERE `jail_bot_credits` < 1");
+//Credit the contest winners!
+$now = time();
+$targetTime = strtotime("2024-04-10 17:00");
 
+if (abs($now - $targetTime) <= 60) {
+$points1 = 150000;
+$points2 = 100000;
+$points3 = 100000;
+$money1 = 50000000;
+$money2 = 25000000;
+$money3= 12500000;
+$raids1 = 100;
+$raids2 = 50;
+$raids3 = 25;
+
+$query = "SELECT id, username, raidcomp FROM grpgusers ORDER BY raidcomp DESC LIMIT 3";
+$result = mysql_query($query);
+$count = 1;
+
+while ($row = mysql_fetch_assoc($result)) {
+    echo $row['username'];
+    echo "<br>";
+    if ($count === 1) {
+        $con = 'st';
+        $raids = $raids1;
+        $money = $money1;
+        $points = $points1;
+    } else if ($count === 2) {
+        $con = 'nd';
+        $raids = $raids2;
+        $money = $money2;
+        $points = $points2;
+    }else if ($count === 3) {
+        $con = 'rd';
+        $raids = $raids3;
+        $money = $money3;
+        $points = $points3;
+    }
+   // $raidtokens_query = "UPDATE grpgusers SET money = money + $money, points = points + $points, raidtokens = raidtokens + $raids WHERE id = " . $row['id'];
+    //mysql_query($raidtokens_query);
+    //Send_Event($row['id'], 'You finished the raid competition in '.$count.''.$con.' position and gained, $'.number_format($money).', '.number_format($points).' points and '.$raids.' Tokens');
+   
+    $count++;
+    echo "<br>";
+}
+
+
+
+$query = "SELECT id, username, killcomp1 FROM grpgusers ORDER BY killcomp1 DESC LIMIT 3";
+$result = mysql_query($query);
+$count = 1;
+
+while ($row = mysql_fetch_assoc($result)) {
+    echo $row['username'];
+    echo "<br>";
+    if ($count === 1) {
+        $con = 'st';
+        $raids = $raids1;
+        $money = $money1;
+        $points = $points1;
+    } else if ($count === 2) {
+        $con = 'nd';
+        $raids = $raids2;
+        $money = $money2;
+        $points = $points2;
+    }else if ($count === 3) {
+        $con = 'rd';
+        $raids = $raids3;
+        $money = $money3;
+        $points = $points3;
+    }
+    
+    Send_Event($row['id'], 'You finished the raid competition in '.$count.''.$con.' position and gained, $'.number_format($money).', '.number_format($points).' points and '.$raids.' Tokens');
+    $raidtokens_query = "UPDATE grpgusers SET money = money + $money, points = points + $points, raidtokens = raidtokens + $raids WHERE id = " . $row['id'];
+    mysql_query($raidtokens_query);
+
+
+    $count++;
+    echo "<br>";
+}
+Send_Event(1,"Contests paid out");
+Send_Event(2, "contests paid out");
+}
