07f777f20feafd5317764928a9d979ee64fe0bb7
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-10 10:52:58 +0100

    Update test.php

--- test.php
+++ test.php
@@ -5,90 +5,33 @@ require "header.php";
 if($user_class->admin < 1){
     exit();
 }
-$now = time();
 
-// Convert the specific target time to a timestamp
-// Date format: Year-Month-Day Hour:Minute
-$targetTime = strtotime("2024-04-10 17:00");
-
-// Check if the current time is the target time
-if (abs($now - $targetTime) <= 60) {
-$points1 = 150000;
-$points2 = 100000;
-$points3 = 100000;
-$money1 = 50000000;
-$money2 = 25000000;
-$money3= 12500000;
-$raids1 = 100;
-$raids2 = 50;
-$raids3 = 25;
-
-$query = "SELECT id, username, raidcomp FROM grpgusers ORDER BY raidcomp DESC LIMIT 3";
-$result = mysql_query($query);
-$count = 1;
-
-while ($row = mysql_fetch_assoc($result)) {
-    echo $row['username'];
-    echo "<br>";
-    if ($count === 1) {
-        $con = 'st';
-        $raids = $raids1;
-        $money = $money1;
-        $points = $points1;
-    } else if ($count === 2) {
-        $con = 'nd';
-        $raids = $raids2;
-        $money = $money2;
-        $points = $points2;
-    }else if ($count === 3) {
-        $con = 'rd';
-        $raids = $raids3;
-        $money = $money3;
-        $points = $points3;
-    }
-   // $raidtokens_query = "UPDATE grpgusers SET money = money + $money, points = points + $points, raidtokens = raidtokens + $raids WHERE id = " . $row['id'];
-    //mysql_query($raidtokens_query);
-    //Send_Event($row['id'], 'You finished the raid competition in '.$count.''.$con.' position and gained, $'.number_format($money).', '.number_format($points).' points and '.$raids.' Tokens');
-   
-    $count++;
-    echo "<br>";
+function newitemPop($id){
+?>
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      Launch demo modal
+    </button>
+    
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php
 }
 
-
-
-$query = "SELECT id, username, killcomp1 FROM grpgusers ORDER BY killcomp1 DESC LIMIT 3";
-$result = mysql_query($query);
-$count = 1;
-
-while ($row = mysql_fetch_assoc($result)) {
-    echo $row['username'];
-    echo "<br>";
-    if ($count === 1) {
-        $con = 'st';
-        $raids = $raids1;
-        $money = $money1;
-        $points = $points1;
-    } else if ($count === 2) {
-        $con = 'nd';
-        $raids = $raids2;
-        $money = $money2;
-        $points = $points2;
-    }else if ($count === 3) {
-        $con = 'rd';
-        $raids = $raids3;
-        $money = $money3;
-        $points = $points3;
-    }
-
-    //Send_Event($row['id'], 'You finished the raid competition in '.$count.''.$con.' position and gained, $'.number_format($money).', '.number_format($points).' points and '.$raids.' Tokens');
-    //$raidtokens_query = "UPDATE grpgusers SET money = money + $money, points = points + $points, raidtokens = raidtokens + $raids WHERE id = " . $row['id'];
-    //mysql_query($raidtokens_query);
-
-
-    $count++;
-    echo "<br>";
-}
-}else{
-    echo "not yet";
-}
-
+echo newitemPop(1);
