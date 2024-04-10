b8f6a8377b7c0b4a2fcf70c9f7ae7aef540d3817
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-10 10:56:44 +0100

    Update test.php

--- test.php
+++ test.php
@@ -15,7 +15,7 @@ function newitemPop($id){
     
     <!-- Modal -->
     <div class="modal fade" data-bs-theme="dark" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
+      <div class="modal-dialog modal-dialog-centered ">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
