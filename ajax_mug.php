8088792ec4394ad8c31efe3618adf915cca48276
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-04-10 15:15:19 +0100

    u

--- ajax_mug.php
+++ ajax_mug.php
@@ -24,9 +24,6 @@ function success($msg)
 include "classes.php";
 include "database/pdo_class.php";
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 $m = new Memcache();
 $m->addServer('127.0.0.1', 11211, 33);
 
