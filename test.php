bd41ce8b53141b46a029f7bc26f3f88c9d3a259c
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-10 09:24:36 +0100

    Update test.php

--- test.php
+++ test.php
@@ -5,9 +5,15 @@ require "header.php";
 if($user_class->admin < 1){
     exit();
 }
-$prize1 = 1000;
-$prize2 = 1000;
-
+$points1 = 150000;
+$points2 = 100000;
+$points3 = 100000;
+$money1 = 50000000;
+$money2 = 25000000;
+$money3= 12500000;
+$raids1 = 100;
+$raids2 = 50;
+$raids3 = 25;
 
 $query = "SELECT id, username, raidcomp FROM grpgusers ORDER BY raidcomp DESC LIMIT 3";
 
@@ -20,14 +26,20 @@ $count = 1;
 while ($row = mysql_fetch_assoc($result)) {
     echo $row['username'];
     echo "<br>";
-    
-
     if ($count === 1) {
-        echo "Prize: $" . $prize1;
+        $raids = $raids1;
+        $money = $money1;
+        $points = $points1;
     } else if ($count === 2) {
-        echo "Prize: $" . $prize2;
+        $raids = $raids2;
+        $money = $money2;
+        $points = $points2;
+    }else if ($count === 3) {
+        $raids = $raids3;
+        $money = $money3;
+        $points = $points3;
     }
-    
+    Send_Event(1, 'You finished the raid competition in position and gained, $'.$money.', '.$points.' points and '.$raids.' Tokens');
    
     $count++;
     echo "<br>";
