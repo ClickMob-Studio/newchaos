8f3526d749e0f6c7210d4b528a5c91b7f1e282d6
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-10 09:14:56 +0100

    Update test.php

--- test.php
+++ test.php
@@ -4,10 +4,31 @@ require "header.php";
 
 if($user_class->admin < 1){
     exit();
-}
+}<?php
+$prize1 = 1000;
+$prize2 = 1000;
 
-$check = mysql_query("SELECT *,  COUNT(id) as user_count FROM grpgusers GROUP BY ip HAVING COUNT(*) > 1)");    
-var_dump($check);
-while($row = mysql_fetch_array($check)) {
-    echo "IP: " . $row["ip"]. " - Users: " . $row["user_count"]. "<br>";
+
+$query = "SELECT id, username, raidcomp FROM grpgusers ORDER BY raidcomp DESC LIMIT 3";
+
+
+$result = mysql_query($query);
+
+
+$count = 1;
+
+while ($row = mysql_fetch_assoc($result)) {
+    echo $row['username'];
+    echo "<br>";
+    
+
+    if ($count === 1) {
+        echo "Prize: $" . $prize1;
+    } else if ($count === 2) {
+        echo "Prize: $" . $prize2;
+    }
+    
+   
+    $count++;
+    echo "<br>";
 }
