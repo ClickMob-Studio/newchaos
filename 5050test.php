003889233f0a40b016dff0ebbc4cfa81bfef5bf8
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-11 17:56:45 +0100

    Update 5050test.php

--- 5050test.php
+++ 5050test.php
@@ -1,56 +1,18 @@
 <?php
 require "header.php";
 ?>
-<h1>50/50 Game</h1>
-<p>Here you can bet against each other and try and win the other person things!</p>
 
-<div class="row" style="justify-content: center;">
-    <!-- Cash Bets -->
-    <div class="col-sm-12 col-md-4 col-lg-4">
-        <h1>Cash Bets</h1>
-        <form id="cashBetForm">
-            <input type="number" name="amnt" size="5" maxlength="20" min="10000" />
-            <input type="button" value="Bet" class="betButton" data-currency="cash" />
-        </form>
-    </div>
-    <!-- Points Bets -->
-    <div class="col-4 col-md-4 col-lg-4">
-        <h1>Points Bets</h1>
-        <form id="pointsBetForm">
-            <input type="number" name="amnt" size="5" maxlength="20" min="100" />
-            <input type="button" value="Bet" class="betButton" data-currency="points" />
-        </form>
-    </div>
-    <!-- Credit Bets -->
-    <div class="col-4 col-md-4 col-lg-4">
-        <h1>Credit Bets</h1>
-        <form id="creditBetForm">
-            <input type="number" name="amnt" size="5" maxlength="20" min="10" />
-            <input type="button" value="Bet" class="betButton" data-currency="credits" />
-        </form>
-    </div>
+<h1>50/50</h1>
+<div class="row">
+<div class="col-4 col-sm-12">
+    testing cols
 </div>
 
-<script>
-document.addEventListener('DOMContentLoaded', function() {
-    // Select all bet buttons
-    const betButtons = document.querySelectorAll('.betButton');
+<div class="col-4 col-sm-12">
+    testing cols
+</div>
 
-    // Add click event listener to each button
-    betButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            const form = this.closest('form');
-            const formData = new FormData(form); 
-            formData.append('currency', this.getAttribute('data-currency'));
-                method: 'POST',
-                body: formData,
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data); // Handle response data
-            })
-            .catch(error => console.error('Error:', error));
-        });
-    });
-});
-</script>
+<div class="col-4 col-sm-12">
+    testing cols
+</div>
+</div>
