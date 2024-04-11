5b00f78aa12cf3fcfc17262beb9daa9a2f139066
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-11 18:05:03 +0100

    Update 5050test.php

--- 5050test.php
+++ 5050test.php
@@ -1,20 +1,29 @@
 <?php
 require "header.php";
+$db->query("SELECT * FROM fiftyfifty WHERE currency = cash");
+$db->execute();
+$cash = $db->fetch_row();
 ?>
 <h1>50/50</h1>
 <div class="container">
     <div class="row">
-        <!-- Column 1 -->
+        <div class="col-md-4 col-12">
+            <h1>Cash Bets</h1>
+            <table>
+            <?php foreach ($cash as $cas): ?>
+            <tr>
+                <td><?= formatName($cas['userid'])?></td>
+                <td>$<?= prettynum($cas['amnt'], 1)?></td>
+                <td>LINK</td>
+            </tr>
+            <?php endif; ?>
+            </table>
+        </div>
+
         <div class="col-md-4 col-12">
             testing cols
         </div>
 
-        <!-- Column 2 -->
-        <div class="col-md-4 col-12">
-            testing cols
-        </div>
-
-        <!-- Column 3 -->
         <div class="col-md-4 col-12">
             testing cols
         </div>
