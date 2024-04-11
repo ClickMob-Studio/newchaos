f775c12c8a078ab0a6599e4509091d53f1bccdea
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-11 18:11:52 +0100

    Update 5050test.php

--- 5050test.php
+++ 5050test.php
@@ -3,12 +3,23 @@ require "header.php";
 $db->query("SELECT * FROM fiftyfifty WHERE currency = 'cash'");
 $db->execute();
 $cash = $db->fetch_row();
+$db->query("SELECT * FROM fiftyfifty WHERE currency = 'points'");
+$db->execute();
+$points = $db->fetch_row();
+$db->query("SELECT * FROM fiftyfifty WHERE currency = 'credits'");
+$db->execute();
+$credits = $db->fetch_row();
 ?>
 <h1>50/50</h1>
 <div class="container">
     <div class="row">
         <div class="col-md-4 col-12">
             <h1>Cash Bets</h1>
+            <thead>
+                    <th>Name</th>
+                    <th>Amount</th>
+                    <th>Action</th>
+                </thead>
             <table>
                 <tbody>
             <?php foreach ($cash as $cas): ?>
@@ -23,7 +34,7 @@ $cash = $db->fetch_row();
         </div>
 
         <div class="col-md-4 col-12">
-        <h1>Cash Bets</h1>
+        <h1>Point Bets</h1>
             <table>
                 <thead>
                     <th>Name</th>
@@ -31,10 +42,10 @@ $cash = $db->fetch_row();
                     <th>Action</th>
                 </thead>
                 <tbody>
-            <?php foreach ($cash as $cas): ?>
+            <?php foreach ($points as $poin): ?>
             <tr>
-                <td><?= formatName($cas['userid'])?></td>
-                <td><?= prettynum($cas['amnt'], 1)?></td>
+                <td><?= formatName($poin['userid'])?></td>
+                <td><?= prettynum($poin['amnt'])?></td>
                 <td>LINK</td>
             </tr>
             <?php endforeach; ?>
@@ -43,13 +54,18 @@ $cash = $db->fetch_row();
         </div>
 
         <div class="col-md-4 col-12">
-        <h1>Cash Bets</h1>
+        <h1>Credit Bets</h1>
             <table>
+            <thead>
+                    <th>Name</th>
+                    <th>Amount</th>
+                    <th>Action</th>
+                </thead>
                 <tbody>
-            <?php foreach ($cash as $cas): ?>
+            <?php foreach ($credits as $cre): ?>
             <tr>
-                <td><?= formatName($cas['userid'])?></td>
-                <td><?= prettynum($cas['amnt'], 1)?></td>
+                <td><?= formatName($cre['userid'])?></td>
+                <td><?= prettynum($cre['amnt'])?></td>
                 <td>LINK</td>
             </tr>
             <?php endforeach; ?>
