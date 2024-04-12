0ed1b82861139ac1d93c8f7a0c9ff972aa1ca14b
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-12 18:06:29 +0100

    a

---
+++ js/5050.js
@@ -0,0 +1,187 @@
+$(document).ready(function(){
+
+    function updateTables() {
+        $.ajax({
+            url: '/ajax_50.php?action=update', 
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                var cashRows = '';
+                data.cash.forEach(function(bet) {
+                    cashRows += `<tr><td>${bet.formatted_userid}</td><td>$${bet.amnt}</td><td><button class="takeCashButton" value="${bet.id}">Take</button></td></tr>`;
+                });
+                $('#cashbettable tbody').html(cashRows);
+                var pointsRows = '';
+                data.points.forEach(function(bet) {
+                    pointsRows += `<tr><td>${bet.formatted_userid}</td><td>${bet.amnt} points</td><td><button class="takePointsButton" value="${bet.id}">Take</button></td></tr>`;
+                });
+                $('#pointbettable tbody').html(pointsRows);
+
+                var creditsRows = '';
+                data.credits.forEach(function(bet) {
+                    creditsRows += `<tr><td>${bet.formatted_userid}</td><td>${bet.amnt} credits</td><td><button class="takeCreditsButton" value="${bet.id}">Take</button></td></tr>`;
+                });
+                $('#creditbettable tbody').html(creditsRows);
+            },
+            error: function() {
+                console.error("Failed to fetch bet data");
+            }
+        });
+    }
+    setInterval(updateTables, 2000);
+});
+$(document).ready(function(){
+    $("#betButton").click(function(){
+        var amount = $("#betAmount").val();
+        $.ajax({
+            url: '/ajax_50.php',
+            type: 'GET',
+            data: {action: 'pointbet', amount: amount},
+            success: function(response) {
+                $(".col-12.alert.alert-info").html(response).show();
+            },
+            error: function() {
+                alert("An error occurred");
+            }
+        });
+    });
+});
+$(document).ready(function(){
+    $("#betCashButton").click(function(){
+        var amount = $("#betAmount").val(); 
+        $.ajax({
+            url: '/ajax_50.php', 
+            type: 'GET',
+            data: {action: 'cashbet', amount: amount},
+            success: function(response) {
+                $(".col-12.alert.alert-info").html(response).show();
+                var newRow = `<tr><<td><?= $user_class->formattedname; ?></td><td>$${amount}</td> <td></td></tr>`;
+                $("#cashbettable tbody").append(newRow);
+            },
+            error: function() {
+                alert("An error occurred");
+            }
+        });
+    });
+});
+$(document).ready(function(){
+    $("#betPointsButton").click(function(){
+        var amount = $("#betPAmount").val(); 
+        $.ajax({
+            url: '/ajax_50.php', 
+            type: 'GET',
+            data: {action: 'pointbet', amount: amount},
+            success: function(response) {
+                $(".col-12.alert.alert-info").html(response).show();
+                var newRow = `<tr><<td><?= $user_class->formattedname; ?></td><td>${amount} points</td> <td></td></tr>`;
+                $("#pointbettable tbody").append(newRow);
+            },
+            error: function() {
+
+                alert("An error occurred");
+            }
+        });
+    });
+});
+$(document).ready(function(){
+    $("#betCreditsButton").click(function(){
+        var amount = $("#betCAmount").val(); 
+        $.ajax({
+            url: '/ajax_50.php', 
+            type: 'GET',
+            data: {action: 'creditbet', amount: amount},
+            success: function(response) {
+                $(".col-12.alert.alert-info").html(response).show();
+                var newRow = `<tr><<td><?= $user_class->formattedname; ?></td><td>${amount} credits</td> <td></td></tr>`;
+                $("#creditbettable tbody").append(newRow);
+            },
+            error: function() {
+                alert("An error occurred");
+            }
+        });
+    });
+});
+$(document).ready(function(){
+    $(document).on('click', '.takePointsButton', function(){
+        var amount = $(this).val();
+        var $button = $(this);
+       
+        $.ajax({
+            url: '/ajax_50.php', 
+            type: 'GET',
+            data: {action: 'takepointbet', id: amount},
+            success: function(response) {
+                $(".col-12.alert.alert-info").html(response).show();
+                $button.closest('tr').fadeOut(400, function() { 
+                    $(this).remove();
+                });
+             },
+            error: function() {
+                alert("An error occurred");
+            }
+        });
+    });
+});
+$(document).ready(function(){
+    $(document).on('click', '.takeCreditsButton', function(){
+        var amount = $(this).val();
+        var $button = $(this);
+       
+        $.ajax({
+            url: '/ajax_50.php', 
+            type: 'GET',
+            data: {action: 'takepointbet', id: amount},
+            success: function(response) {
+                $(".col-12.alert.alert-info").html(response).show();
+                $button.closest('tr').fadeOut(400, function() { 
+                    $(this).remove();
+                });
+             },
+            error: function() {
+                alert("An error occurred");
+            }
+        });
+    });
+});
+$(document).ready(function(){
+    $(document).on('click', '.takeCashButton', function(){
+        var amount = $(this).val();
+        var $button = $(this);
+       
+        $.ajax({
+            url: '/ajax_50.php', 
+            type: 'GET',
+            data: {action: 'takecashbet', id: amount},
+            success: function(response) {
+                $(".col-12.alert.alert-info").html(response).show();
+                $button.closest('tr').fadeOut(400, function() { 
+                    $(this).remove();
+                });
+             },
+            error: function() {
+                alert("An error occurred");
+            }
+        });
+    });
+});
+$(document).ready(function(){
+    $(document).on('click', '.removeCashButton', function(){
+        var amount = $(this).val();
+        var $button = $(this);
+       
+        $.ajax({
+            url: '/ajax_50.php', 
+            type: 'GET',
+            data: {action: 'removecashbet', id: amount},
+            success: function(response) {
+                $(".col-12.alert.alert-info").html(response).show();
+                $button.closest('tr').fadeOut(400, function() { 
+                    $(this).remove();
+                });
+             },
+            error: function() {
+                alert("An error occurred");
+            }
+        });
+    });
+});
