a730587e79d83c282e85cd3083e0083561403143
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-04-13 16:14:08 +0100

    u

--- a_adhoc_testing.php
+++ a_adhoc_testing.php
@@ -5,27 +5,25 @@ $db->query("SELECT id FROM grpgusers");
 $db->execute();
 $rows = $db->fetch_row();
 foreach ($rows as $row) {
-    echo $row['id'] . '<br />';
-}
+    $db->query("SELECT * FROM inventory WHERE userid = " . $row['id']);
+    $db->execute();
+    $rows = $db->fetch_row();
 
-//$db->query("SELECT * FROM inventory WHERE userid = 61");
-//$db->execute();
-//$rows = $db->fetch_row();
-//
-//$itemIds = array();
-//foreach ($rows as $row) {
-//    if (!isset($itemIds[$row['itemid']])) {
-//        $itemIds[$row['itemid']] = $row['id'];
-//    } else {
-//        $rowId = $itemIds[$row['itemid']];
-//
-//        $db->query("UPDATE inventory SET quantity = quantity + " . $row['quantity'] . "  WHERE id = " . $rowId . " ");
-//        $db->execute();
-//
-//        $db->query("DELETE FROM inventory  WHERE id = " . $row['id'] . " ");
-//        $db->execute();
-//    }
-//}
+    $itemIds = array();
+    foreach ($rows as $row) {
+        if (!isset($itemIds[$row['itemid']])) {
+            $itemIds[$row['itemid']] = $row['id'];
+        } else {
+            $rowId = $itemIds[$row['itemid']];
+
+            $db->query("UPDATE inventory SET quantity = quantity + " . $row['quantity'] . "  WHERE id = " . $rowId . " ");
+            $db->execute();
+
+            $db->query("DELETE FROM inventory  WHERE id = " . $row['id'] . " ");
+            $db->execute();
+        }
+    }
+}
 
 include 'footer.php';
 ?>
