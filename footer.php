33516db2200954e948c608e0c8d0d57ffe5d66e8
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-04-13 14:38:13 +0100

    u

--- footer.php
+++ footer.php
@@ -64,7 +64,7 @@ function calcEXP(){
     });
 </script>
 
-<?php if ($user_class->box_hunt_count < 20): ?>
+<?php if (false): ?>
     <?php $boxHuntChance = mt_rand(1,100); ?>
     <?php if ($boxHuntChance <= 15): ?>
         <script type="text/javascript">
