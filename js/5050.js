093f4092200334197f17a66e2fcb4542b5aad4d7
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-14 17:13:12 +0100

    Update 5050.js

--- js/5050.js
+++ js/5050.js
@@ -184,6 +184,7 @@ $(document).ready(function(){
             type: 'POST',
             data: {action: 'removecashbet', id: amount},
             success: function(response) {
+                $(".col-12.alert.alert-info").html('').show();
                 $(".col-12.alert.alert-info").html(response.text).show();
                 $(".points").html(response.points);
                 $(".credits").html(response.credits);
