21dbe68eb68f983c2ca93dd7860ef261398d4ff4
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-04-15 20:26:15 +0100

    u

--- a_adhoc_testing.php
+++ a_adhoc_testing.php
@@ -1,7 +1,7 @@
 <?php
 include 'header.php';
 
-$db->query("SELECT * FROM `gang_comp_leaderboard` ORDER BY `daily_missions_complete` DESC LIMIT 3");
+$db->query("SELECT * FROM `gang_comp_leaderboard` ORDER BY `daily_missions_complete` DESC LIMIT 2");
 $db->execute();
 $dailyRows = $db->fetch_row();
 
@@ -9,13 +9,16 @@ $i = 1;
 foreach ($dailyRows as $row) {
     Send_Event(2, $i . ' ' . $row['gang_id'], 2);
 
-//    $db->query("SELECT * FROM `grpgusers` WHERE `gang` = " . $dailyRows['gang_id']);
-//    $db->execute();
-//    $userRows = $db->fetch_row();
-//
-//    foreach ($userRows as $uRow) {
-//
-//    }
+    if ($i == 1) {
+        $db->query("UPDATE `grpgusers` SET `points` = `points` + 25000 WHERE `id` = 2");
+        $db->execute();
+
+        Give_Item(163, 2, 1);
+        Give_Item(42, 2, 1);
+    } else {
+        Give_Item(42, 2, 1);
+    }
+
 
     $i++;
 }
