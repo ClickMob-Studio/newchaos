e60c8eb3d1b00e8e4ff77d69100a87595514a64d
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-04-16 05:36:44 +0100

    u

--- a_adhoc_testing.php
+++ a_adhoc_testing.php
@@ -1,8 +1,6 @@
 <?php
 include 'header.php';
 
-exit;
-
 $db->query("SELECT * FROM `gang_comp_leaderboard` ORDER BY `daily_missions_complete` DESC LIMIT 2");
 $db->execute();
 $dailyRows = $db->fetch_row();
