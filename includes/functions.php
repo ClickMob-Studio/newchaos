00ebdcf351f68462041fcafd860111ada7d90a5a
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-04-16 06:23:48 +0100

    u

--- includes/functions.php
+++ includes/functions.php
@@ -1890,6 +1890,7 @@ function getUserBaStats($user_class)
     $db->query("SELECT * FROM user_ba_stats WHERE user_id = " . $user_class->id . " LIMIT 1");
     $db->execute();
     $r = $db->fetch_single();
+    exit;
 
     if (isset($r['id'])) {
         $r['maxexp'] = 10000 * $r['level'];
