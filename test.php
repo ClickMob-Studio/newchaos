189929ceeaefbb8e169247326d590545074128eb
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-18 09:57:08 +0100

    Update test.php

--- test.php
+++ test.php
@@ -7,10 +7,12 @@ if($user_class->admin < 1){
 }
 if($user_class->level >= 10){
   $line = mysql_fetch_array(mysql_query("SELECT * FROM referrals WHERE referred = ".$user_class->id." AND credited = '0'"));
+  if(mysql_num_rows($line)){
   bloodbath('referrals', $line['referrer']);
   mysql_query("UPDATE grpgusers SET credits = credits + 50, points = points + 100, referrals = referrals + 1, refcomp = refcomp + 1, refcount = refcount + 1 WHERE referred = ".$user_class->id);
   mysql_query("UPDATE referrals SET credited = 1 WHERE referred =".$user_class->id);
   mysql_query("UPDATE referrals SET viewed = 1 WHERE id = ".$user_class->id);
   Send_Event($line['referrer'], "You have been credited 50 Credits & 100 Points for referring [-_USERID_-]. Keep up the good work!", $line['referred']);
   }
+}
   
