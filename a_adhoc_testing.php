4695b7123957504527f6e70dfbcdf7251a61866a
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-04-19 07:02:26 +0100

    up

--- a_adhoc_testing.php
+++ a_adhoc_testing.php
@@ -1,6 +1,6 @@
 <?php
 include 'header.php';
-exit;
+
 //$db->query("SELECT * FROM missions WHERE timestamp > 1713409200 AND completed = 'successful'");
 //$db->execute();
 //$rows = $db->fetch_row();
@@ -14,50 +14,44 @@ exit;
 //echo 'done';
 
 
-$db->query("SELECT * FROM `grpgusers` WHERE `gang` = 3");
-$db->execute();
-$userRows = $db->fetch_row();
-
-foreach ($userRows as $uRow) {
-    $db->query("UPDATE `grpgusers` SET `points` = `points` + 50000 WHERE `id` = " . $uRow['id']);
-    $db->execute();
-
-    Give_Item(163, $uRow['id'], 5);
-    Give_Item(42, $uRow['id'], 5);
-    Give_Item(250, $uRow['id'], 1);
-
-    Send_Event($uRow['id'], "Your gang won 1st place in the contest, you have been awarded 50,000 points, 5 x Police Badges, 5 x Mystery Boxes & 1 x Advanced Booster.");
-}
-
-
-$db->query("SELECT * FROM `grpgusers` WHERE `gang` = 6");
-$db->execute();
-$userRows = $db->fetch_row();
-
-foreach ($userRows as $uRow) {
-    $db->query("UPDATE `grpgusers` SET `points` = `points` + 25000 WHERE `id` = " . $uRow['id']);
-    $db->execute();
-
-    Give_Item(163, $uRow['id'], 1);
-    Give_Item(42, $uRow['id'], 1);
-    Give_Item(231, $uRow['id'], 1);
-
-    Send_Event($uRow['id'], "Your gang won 2nd place in the contest, you have been awarded 25,000 points, 1 x Police Badges, 1 x Mystery Boxes & 1 x Heroic Booster.");
-}
+//$db->query("SELECT * FROM `grpgusers` WHERE `gang` = 3");
+//$db->execute();
+//$userRows = $db->fetch_row();
+//
+//foreach ($userRows as $uRow) {
+//    $db->query("UPDATE `grpgusers` SET `points` = `points` + 50000 WHERE `id` = " . $uRow['id']);
+//    $db->execute();
+//
+//    Give_Item(163, $uRow['id'], 5);
+//    Give_Item(42, $uRow['id'], 5);
+//    Give_Item(250, $uRow['id'], 1);
+//
+//    Send_Event($uRow['id'], "Your gang won 1st place in the contest, you have been awarded 50,000 points, 5 x Police Badges, 5 x Mystery Boxes & 1 x Advanced Booster.");
+//}
+//
+//
+//$db->query("SELECT * FROM `grpgusers` WHERE `gang` = 6");
+//$db->execute();
+//$userRows = $db->fetch_row();
+//
+//foreach ($userRows as $uRow) {
+//    $db->query("UPDATE `grpgusers` SET `points` = `points` + 25000 WHERE `id` = " . $uRow['id']);
+//    $db->execute();
+//
+//    Give_Item(163, $uRow['id'], 1);
+//    Give_Item(42, $uRow['id'], 1);
+//    Give_Item(231, $uRow['id'], 1);
+//
+//    Send_Event($uRow['id'], "Your gang won 2nd place in the contest, you have been awarded 25,000 points, 1 x Police Badges, 1 x Mystery Boxes & 1 x Heroic Booster.");
+//}
 
 $db->query("SELECT * FROM `grpgusers` WHERE `gang` = 16");
 $db->execute();
 $userRows = $db->fetch_row();
 
 foreach ($userRows as $uRow) {
-    $db->query("UPDATE `grpgusers` SET `points` = `points` + 10000 WHERE `id` = " . $uRow['id']);
-    $db->execute();
-
-    Give_Item(163, $uRow['id'], 1);
-    Give_Item(230, $uRow['id'], 1);
-
-    Send_Event($uRow['id'], "Your gang won 3rd place in the contest, you have been awarded 10,000 points, 1 x Mystery Boxes & 1 x Exotic Booster.");
-}
+    Give_Item(42, $uRow['id'], 1);
+ }
 
 
 //$db->query("SELECT * FROM `gang_comp_leaderboard` ORDER BY `daily_missions_complete` DESC LIMIT 2");
