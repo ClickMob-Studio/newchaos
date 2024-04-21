4c34561118014e0e578470148295335ca4da689e
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-04-21 15:31:57 +0100

    u

--- admin_panel.php
+++ admin_panel.php
@@ -1,11 +1,16 @@
 <?php
 include 'header.php';
+if ($user_class->admin < 1) {
+    echo 'You should not be here';
+    exit;
+}
 ?>
 
 <h1>Admin Panel</h1>
 
 <ul>
-
+    <li><a href="adamexp.php">Donations</a></li>
+    <li><a href="sendlogs.php">Send Logs</a></li>
 </ul>
 
 <?php
