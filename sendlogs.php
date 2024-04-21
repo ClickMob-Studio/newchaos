1f03401f75bc9fe6f8ad2bc8c77ffd2cf71534e9
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-04-21 15:55:49 +0100

    up

--- sendlogs.php
+++ sendlogs.php
@@ -4,7 +4,46 @@ if ($user_class->admin < 1) {
     echo 'You should not be here';
     exit;
 }
+
+$db->query("SELECT * FROM transferlog ORDER BY timestamp DESC LIMIT 100")
+$db->exectue();
+$results = $db->fetch_row();
 ?>
+
+<div class="table-container">
+    <table class="new_table" id="newtables" style="width:100%;">
+        <thead>
+            <tr>
+                <th>From</th>
+                <th>To</th>
+                <th>Date</th>
+                <th>What</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($results as $res): ?>
+                <tr>
+                    <td>
+                        <?php echo formatName($res['from']) ?><br />
+                        <?php echo $res['fromip'] ?>
+                    </td>
+                    <td>
+                        <?php echo formatName($res['to']) ?><br />
+                        <?php echo $res['toip'] ?>
+                    </td>
+                    <td>
+                        <?php echo date($res['timestamp'], 'd M Y h:i:s') ?>
+                    </td>
+                    <td>
+                        
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+
+
+        </tbody>
+    </table>
+</div>
 
 <?php
 include 'footer.php';
