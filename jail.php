ee7e3c411286959916d8070a20e29b9f2f16d132
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-04-22 05:55:13 +0100

    shuffle

--- jail.php
+++ jail.php
@@ -268,6 +268,41 @@ if($user_class->jail > 0){
         </td></tr>
 
     <script type="text/javascript">
+        function sortTable() {
+            //get the parent table for convenience
+            let table = document.getElementById("jail-table");
+
+            //1. get all rows
+            let rowsCollection = table.querySelectorAll("tr");
+
+            //2. convert to array
+            let rows = Array.from(rowsCollection)
+                .slice(1); //skip the header row
+
+            //3. shuffle
+            shuffleArray(rows);
+
+            //4. add back to the DOM
+            for (const row of rows) {
+                table.appendChild(row);
+            }
+        }
+
+
+        /**
+         * Randomize array element order in-place.
+         * Using Durstenfeld shuffle algorithm.
+         * from: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array/12646864#12646864
+         */
+        function shuffleArray(array) {
+            for (var i = array.length - 1; i > 0; i--) {
+                var j = Math.floor(Math.random() * (i + 1));
+                var temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
         let jailBreakClicks = 0;
         let jailRefreshes = 0;
 
@@ -319,6 +354,8 @@ if($user_class->jail > 0){
                         );
 
                     })
+
+                    sortTable();
                 }
 
                 $('.jail-break-link').click(function(e) {
@@ -353,6 +390,8 @@ if($user_class->jail > 0){
                 });
             }, "json")
         }, 4000);
+
+
     </script>
 <?
 include 'footer.php';
