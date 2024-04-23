49841ba294f45e39a27bf3883b3016cff56a0809
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-04-23 06:39:00 +0100

    u

--- backalley_new.php
+++ backalley_new.php
@@ -50,7 +50,7 @@ $medPackTotalCount = $medPackOneCount + $medPackTwoCount;
                     <p style="color: #000;">YOU CURRENTLY HAVE <span class="gold-rush-credits-text"><?php echo $userBaStats['gold_rush_credits'] ?></span> GOLD RUSH CREDITS REMAINING!</p>
                 </div>
 
-                <div id="ba-response-message" style="min-height: 135px;"></div>
+                <div id="ba-response-message" style="min-height: 145px;"></div>
 
                 <br />
                 <hr />
