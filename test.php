11354cf931d5832b4f5156bc35e34c6c2101ad85
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-23 13:16:13 +0100

    Update test.php

--- test.php
+++ test.php
@@ -15,9 +15,10 @@ while ($line = mysql_fetch_array($queen_result)) {
     $city_result = mysql_fetch_assoc($city_query);
 
     if ($city_result['owned_points'] > 0) {
+        echo $city_query['owned_points']. "<br>";
         $owned_points = $city_query['owned_points'];
         $twenty_percent = $owned_points - ($owned_points * 0.20);
-      echo $twenty_percent ."<br>";
+     // echo $twenty_percent ."<br>";
        // mysql_query("UPDATE `grpgusers` SET `points` = `points` + " . $twenty_percent . " WHERE `id` = " . $line['id']);
         //Send_event($line['id'], "You earned " . $twenty_percent . " points for being the *Under Boss!");
     }
