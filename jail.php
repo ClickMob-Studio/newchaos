f35e851531cab6dc3c185806ccf6d2434b4bdff8
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-04-23 05:48:23 +0100

    up

--- jail.php
+++ jail.php
@@ -396,14 +396,14 @@ if($user_class->jail > 0){
             document.body.addEventListener('click', function(evt) {
                 // Check for an actual mouse click (1, 2 & 3)
                 if (evt.which > 3) {
-                    // var request = $.ajax({
-                    //     url: 'ajax_autoclick_detection.php?page=jail&reason=invalid_click',
-                    //     method: "GET",
-                    //     dataType: "json"
-                    // });
-                    // request.done(function (res) {
-                    //    console.log(res);
-                    // });
+                    var request = $.ajax({
+                        url: 'ajax_autoclick_detection.php?page=jail&reason=invalid_click',
+                        method: "GET",
+                        dataType: "json"
+                    });
+                    request.done(function (res) {
+                       console.log(res);
+                    });
                 }
 
                 if (evt.isTrusted) {
