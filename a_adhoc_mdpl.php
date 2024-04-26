f6dc5fbda734e96d8ce332ce31da907b9cb97b0c
Aaron Newton <aaron.12.newton@gmail.com>
2024-04-26 11:06:32 +0100

    UP

--- a_adhoc_mdpl.php
+++ a_adhoc_mdpl.php
@@ -6,6 +6,14 @@ if ($_GET['wekey'] === 'herewego') {
     $startDate = new \DateTime();
     $startDate->setDate(2024, 04, 01);
     $startDate->setTime(00, 00,00);
-    echo $startDate->getTimestamp(); exit;
-    var_dump($date); exit;
+
+    $endDate = new \DateTime();
+    $endDate->setDate(2024, 04, 01);
+    $endDate->setTime(23, 59,59);
+
+    $db->query("SELECT * FROM `missions` WHERE `timestamp` BETWEEN " . $startDate->getTimestamp() . " AND " . $endDate->getTimestamp() . " AND `completed` = 'successful'");
+    $db->execute();
+    $rows = $db->fetch_row();
+    echo count($rows); exit;
+
 }
