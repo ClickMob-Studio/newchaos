2ea4fdfc1ee6f7b4638cbd91916e10c1e4a53334
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-04-26 22:18:40 +0100

    Up

--- gamerules.php
+++ gamerules.php
@@ -18,7 +18,7 @@ include 'header.php';
 - Using a middle man player to exchange (middle man player will also be banned).<br><br>
 <font color=red><b>5.</b></font> No flooding/spamming anywhere (it means continually filling the screen with repetitive text, whether it be advertising or plainly abusing three or more lines of chat causing a disruptive flow of unnecessary material). Massive use of caps can also be considered as spamming.
 <br><br>
-<font color=red><b>6.</b></font> Asking for personal or contact information of any kind is strictly prohibited. Please do not post personal contact information on any part of TheMafiaLife. This includes important bank information and/or contact details.
+<font color=red><b>6.</b></font> Asking for personal or contact information of any kind is strictly prohibited. Please do not post personal contact information on any part of Chaos City. This includes important bank information and/or contact details.
 <br><br>
 <font color=red><b>7.</b></font> Using game-enhancing programs such as any form of Macros plugins, any form of auto-clicker programs, and any form of reloader plugins. The use of these programs set the abusing players at an unfair advantage over those who are playing manually, and the infraction will not be tolerated. Players caught or suspected of the use of one or more of these banned programs, plugins, or techniques are subject to banning, or account deletion at the discretion of the game admin.
 <br><br>
