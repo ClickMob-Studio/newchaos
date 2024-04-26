5b7ab811e4d5066971b6303f97a410d0dbdcc3b8
Aaron Newton <aaron.12.newton@gmail.com>
2024-04-26 10:59:32 +0100

    U

--- a_adhoc_mdpl.php
+++ a_adhoc_mdpl.php
@@ -3,6 +3,9 @@
 include 'header.php';
 
 if ($_GET['wekey'] === 'herewego') {
-    $date = new \DateTime();
+    $startDate = new \DateTime();
+    $startDate->setDate(2024, 04, 01);
+    $startDate->setTime(00, 00,00);
+    echo $startDate->getTimestamp(); exit;
     var_dump($date); exit;
 }
