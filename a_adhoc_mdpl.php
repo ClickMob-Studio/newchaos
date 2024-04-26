1fddedf9d38600ec92841977b4d666045f161368
Aaron Newton <aaron.12.newton@gmail.com>
2024-04-26 11:38:47 +0100

    up

--- a_adhoc_mdpl.php
+++ a_adhoc_mdpl.php
@@ -5,75 +5,81 @@ include 'header.php';
 if ($_GET['wekey'] === 'herewego') {
 
     // Define Dates
-    $startDate = new \DateTime();
-    $startDate->setDate(2024, 04, 10);
-    $startDate->setTime(00, 00,00);
+    $day = 11;
+    while ($day < 26){
+        $startDate = new \DateTime();
+        $startDate->setDate(2024, 04, 10);
+        $startDate->setTime(00, 00,00);
 
-    $endDate = new \DateTime();
-    $endDate->setDate(2024, 04, 10);
-    $endDate->setTime(23, 59,59);
-
-
-    // Retrieve the mission
-    $db->query("SELECT * FROM `mission`");
-    $db->execute();
-    $rows = $db->fetch_row();
-
-    // Build an array that holds the payouts for the mission, and profit, indexed on the mission ID for use later
-    $missionPayoutsIndexedOnId = array();
-    foreach ($rows as $row) {
-        $pointsPayout = $row['payCrimes'] + $row['payKills'] + $row['payMugs'] + $row['payBusts'];
-        $pointsCost = 0;
-        if ($row['crimes'] > 0) {
-            $pointsCost = $pointsCost + ($row['crimes'] / 10);
-        }
-        if ($row['mugs'] > 0) {
-            $pointsCost = $pointsCost + $row['mugs'];
-        }
-        if ($row['kills'] > 0) {
-            $pointsCost = $pointsCost + ($row['kills'] * 2);
-        }
-
-        $pointsProfit = $pointsPayout - $pointsCost;
+        $endDate = new \DateTime();
+        $endDate->setDate(2024, 04, 10);
+        $endDate->setTime(23, 59,59);
 
 
-        $missionPayoutsIndexedOnId[$row['id']] = array(
-            'points_payout' => $pointsPayout,
-            'points_cost' => $pointsCost,
-            'points_profit' => $pointsProfit
-        );
-    }
+        // Retrieve the mission
+        $db->query("SELECT * FROM `mission`");
+        $db->execute();
+        $rows = $db->fetch_row();
+
+        // Build an array that holds the payouts for the mission, and profit, indexed on the mission ID for use later
+        $missionPayoutsIndexedOnId = array();
+        foreach ($rows as $row) {
+            $pointsPayout = $row['payCrimes'] + $row['payKills'] + $row['payMugs'] + $row['payBusts'];
+            $pointsCost = 0;
+            if ($row['crimes'] > 0) {
+                $pointsCost = $pointsCost + ($row['crimes'] / 10);
+            }
+            if ($row['mugs'] > 0) {
+                $pointsCost = $pointsCost + $row['mugs'];
+            }
+            if ($row['kills'] > 0) {
+                $pointsCost = $pointsCost + ($row['kills'] * 2);
+            }
+
+            $pointsProfit = $pointsPayout - $pointsCost;
 
 
-    $db->query("SELECT * FROM `missions` WHERE `timestamp` BETWEEN " . $startDate->getTimestamp() . " AND " . $endDate->getTimestamp() . " AND `completed` = 'successful'");
-    $db->execute();
-    $rows = $db->fetch_row();
-
-
-    $valuesIndexedByUserId = array();
-    foreach ($rows as $row) {
-        if (!isset($valuesIndexedByUserId[$row['userid']])) {
-            $valuesIndexedByUserId[$row['userid']] = array();
-            $valuesIndexedByUserId[$row['userid']]['missions_complete'] = 0;
-            $valuesIndexedByUserId[$row['userid']]['total_points_earned'] = 0;
-            $valuesIndexedByUserId[$row['userid']]['total_profit_earned'] = 0;
+            $missionPayoutsIndexedOnId[$row['id']] = array(
+                'points_payout' => $pointsPayout,
+                'points_cost' => $pointsCost,
+                'points_profit' => $pointsProfit
+            );
         }
 
-        $missionPayouts = $missionPayoutsIndexedOnId[$row['mid']];
 
-        $valuesIndexedByUserId[$row['userid']]['missions_complete'] = $valuesIndexedByUserId[$row['userid']]['missions_complete'] + 1;
-        $valuesIndexedByUserId[$row['userid']]['total_points_earned'] = $valuesIndexedByUserId[$row['userid']]['total_points_earned'] + $missionPayouts['points_payout'];
-        $valuesIndexedByUserId[$row['userid']]['total_profit_earned'] = $valuesIndexedByUserId[$row['userid']]['total_profit_earned'] + $missionPayouts['points_profit'];
-    }
+        $db->query("SELECT * FROM `missions` WHERE `timestamp` BETWEEN " . $startDate->getTimestamp() . " AND " . $endDate->getTimestamp() . " AND `completed` = 'successful'");
+        $db->execute();
+        $rows = $db->fetch_row();
 
-    foreach ($valuesIndexedByUserId as  $userId => $values) {
-        $db->query("
+
+        $valuesIndexedByUserId = array();
+        foreach ($rows as $row) {
+            if (!isset($valuesIndexedByUserId[$row['userid']])) {
+                $valuesIndexedByUserId[$row['userid']] = array();
+                $valuesIndexedByUserId[$row['userid']]['missions_complete'] = 0;
+                $valuesIndexedByUserId[$row['userid']]['total_points_earned'] = 0;
+                $valuesIndexedByUserId[$row['userid']]['total_profit_earned'] = 0;
+            }
+
+            $missionPayouts = $missionPayoutsIndexedOnId[$row['mid']];
+
+            $valuesIndexedByUserId[$row['userid']]['missions_complete'] = $valuesIndexedByUserId[$row['userid']]['missions_complete'] + 1;
+            $valuesIndexedByUserId[$row['userid']]['total_points_earned'] = $valuesIndexedByUserId[$row['userid']]['total_points_earned'] + $missionPayouts['points_payout'];
+            $valuesIndexedByUserId[$row['userid']]['total_profit_earned'] = $valuesIndexedByUserId[$row['userid']]['total_profit_earned'] + $missionPayouts['points_profit'];
+        }
+
+        foreach ($valuesIndexedByUserId as  $userId => $values) {
+            $db->query("
           INSERT INTO 
             `mission_daily_payout_logs` (user_id, date, missions_complete, total_points_earned, total_profit_earned) 
           VALUES 
             (" . $userId . ", '" . $startDate->format('d-m-Y') . "',  " . $values['missions_complete'] . ",  " . $values['total_points_earned'] . ",  " . $values['total_profit_earned'] . ")
         ");
-        $db->execute();
+            $db->execute();
+        }
+
+
+        $day++;
     }
 
     echo 'done'; exit;
