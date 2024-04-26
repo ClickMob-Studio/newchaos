d7bc5da94f6cbf363d92831fb549ec2664c9bab9
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-26 13:23:23 +0100

    Update testingpush.php

--- testingpush.php
+++ testingpush.php
@@ -1,1 +1,13 @@
-test
+<?php
+require ("header.php");
+
+if($user_class->admin < 1){
+    exit();
+}
+
+$db->query("SELECT sum(credits) as tcredits FROM grpgusers");
+$db->execute();
+
+$r = $db->fetch_row(true);
+
+echo $r['tcredits'];
