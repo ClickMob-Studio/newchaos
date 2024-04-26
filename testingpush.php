8032c2029139f18c471283a42da21bd394750bbb
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-26 13:41:39 +0100

    Update testingpush.php

--- testingpush.php
+++ testingpush.php
@@ -20,7 +20,7 @@ $inactiveUsers = $db->fetch_row(true);
 $inactiveUser = $inactiveUsers['total'];
 
 
-$db->query("SELECT sum(points) as tpoints, sum(bpoints) as bpoints FROM grpgusers");
+$db->query("SELECT sum(points) as tpoints, sum(pbank) as bpoints FROM grpgusers");
 $db->execute();
 
 $a = $db->fetch_row(true);
