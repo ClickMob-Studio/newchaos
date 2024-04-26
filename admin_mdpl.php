e9668a8110efe2ddfd524cdde7a73261f4f7a934
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-04-26 15:40:20 +0100

    Up

--- admin_mdpl.php
+++ admin_mdpl.php
@@ -64,5 +64,65 @@ foreach ($dailyMissionPayoutLogsIndexedByDate as $date => $values) {
 </div>
 
 <?php
+
+$dailyMissionPayoutLogsIndexedByDate = array();
+$userIdsIndexedByDate = array();
+
+while($res = mysql_fetch_array($query, MYSQL_ASSOC)) {
+    if (!isset($dailyMissionPayoutLogsIndexedByDate[$res['date']])) {
+        $dailyMissionPayoutLogsIndexedByDate[$res['date']] = array();
+        $dailyMissionPayoutLogsIndexedByDate[$res['date']]['total_users'] = 0;
+        $dailyMissionPayoutLogsIndexedByDate[$res['date']]['total_points_earned'] = 0;
+        $dailyMissionPayoutLogsIndexedByDate[$res['date']]['total_profit_earned'] = 0;
+    }
+
+    if (!isset($userIdsIndexedByDate[$res['date']])) {
+        $userIdsIndexedByDate[$res['date']] = array();
+    }
+
+    if ($res['missions_complete'] > 12) {
+        if (!in_array($res['user_id'], $userIdsIndexedByDate[$res['date']])) {
+            $dailyMissionPayoutLogsIndexedByDate[$res['date']]['total_users'] += 1;
+        }
+        $dailyMissionPayoutLogsIndexedByDate[$res['date']]['total_points_earned'] += $res['total_points_earned'];
+        $dailyMissionPayoutLogsIndexedByDate[$res['date']]['total_profit_earned'] += $res['total_profit_earned'];
+    }
+}
+
+foreach ($dailyMissionPayoutLogsIndexedByDate as $date => $values) {
+    $dailyMissionPayoutLogsIndexedByDate[$date]['average_points_earned'] = $dailyMissionPayoutLogsIndexedByDate[$date]['total_points_earned'] / $dailyMissionPayoutLogsIndexedByDate[$date]['total_users'];
+    $dailyMissionPayoutLogsIndexedByDate[$date]['average_profit_earned'] = $dailyMissionPayoutLogsIndexedByDate[$date]['total_profit_earned'] / $dailyMissionPayoutLogsIndexedByDate[$date]['total_users'];
+}
+?>
+
+<h1>Active Payout Logs</h1>
+<div class="table-container">
+    <table class="new_table" id="newtables" style="width:100%;">
+        <thead>
+        <tr>
+            <th>Date</th>
+            <th>Users</th>
+            <th>Total Points Earned</th>
+            <th>Total Profit Earned</th>
+            <th>Average Points Earned</th>
+            <th>Average Profit Earned</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($dailyMissionPayoutLogsIndexedByDate as $date => $row): ?>
+            <tr>
+                <td><?php echo $date ?></td>
+                <td><?php echo number_format($row['total_users'], 0) ?></td>
+                <td><?php echo number_format($row['total_points_earned'], 0) ?></td>
+                <td><?php echo number_format($row['total_profit_earned'], 0) ?></td>
+                <td><?php echo number_format($row['average_points_earned'], 0) ?></td>
+                <td><?php echo number_format($row['average_profit_earned'], 0) ?></td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
+
+<?php
 include 'footer.php';
 ?>
