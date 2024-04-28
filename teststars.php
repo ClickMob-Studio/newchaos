f32dd761dcd093b2edfb61b3c82e1c5f28e8c8fd
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-28 20:25:10 +0100

    Update teststars.php

--- teststars.php
+++ teststars.php
@@ -1,5 +1,17 @@
 <?php
-echo "Testing stars:<br>";
-echo str_repeat("&#9733;", 5) . "<br>";
-echo str_repeat("&#9734;", 5) . "<br>";
-?>
+require "header.php";
+
+$qu = "SELECT * FROM `user_logs` WHERE user_id = 5";
+
+
+$res = $db->query($qu);
+$db->execute();
+$rows = $db->fetch_row();
+
+foreach ($rows as $row) {
+    $p  = mysql_query("SELECT * FROM `attacklog` WHERE `timestamp` = ".$row['timestamp']." AND `user_id` = 5");
+    $r = mysql_fetch_assoc($p);
+
+    echo "multi";
+}
+
