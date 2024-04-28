a7f438c3b081f4b46e45198b16233684a065e72b
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-04-28 20:51:27 +0100

    Update teststars.php

--- teststars.php
+++ teststars.php
@@ -1,9 +1,12 @@
 <?php
 require "header.php";
+if($user_class->admin < 1){
+    exit();
+}
 if(isset($_POST['userid'])){
 $qu = mysql_query("SELECT a.timestamp AS attack_time, u.timestamp AS user_log_time
 FROM user_logs u
-JOIN attacklog a ON u.timestamp = a.timestamp AND a.attacker = 91
+JOIN attacklog a ON u.timestamp = a.timestamp AND a.attacker = ".$_POST['userid']."
 WHERE u.user_id = 5");
 $count = 0;
 while($row = mysql_fetch_array($qu)){
