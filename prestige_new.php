7ea0afe10ea045d6f30895030f11c89f5bf405ee
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-04-30 19:57:18 +0100

    up

--- prestige_new.php
+++ prestige_new.php
@@ -9,6 +9,32 @@ $prestigeUnlocks['ba_raidtokens_unlock'] = array(
         'name' => 'BA Raid Tokens',
         'description' => 'Unlocking BA Raid Tokens will allow you to find Raid Tokens with normal Backalley searches, not just in Gold Rush!'
 );
+// Super Attack
+$prestigeUnlocks['speed_attack_unlock'] = array(
+    'name' => 'Super Attack',
+    'description' => 'Unlocking Super Attack gives you access to a new feature where you can attack up to 50 players with one click of a button, helping you complete those important kill missions!'
+);
+// BA Gold Rush Boost
+$prestigeUnlocks['ba_gold_rush_unlock'] = array(
+    'name' => 'BA Gold Rush Boost',
+    'description' => 'Unlocking Ba Gold Rush boost gives you a 10% boosted chance of finding Gold Rush in the Backalley'
+);
+// Crime Cash
+$prestigeUnlocks['crime_cash_unlock'] = array(
+    'name' => 'Crime Cash Boost',
+    'description' => 'Unlocking Crime Cash Boost gives an extra 10% cash from all crimes you complete.'
+);
+// Throne Payout Boost
+$prestigeUnlocks['throne_points_unlock'] = array(
+    'name' => 'Throne Payout Boost',
+    'description' => 'Unlocking Throne Payout Boost gives you an extra 20% payouts from all Boss/Under Boss positions held!'
+);
+// Travel Cost Reduction
+$prestigeUnlocks['travel_cost_unlock'] = array(
+    'name' => 'Travel Cost Reduction',
+    'description' => 'Unlocking Travel Cost Reduction gives you a 20% reduction on all travel costs.'
+);
+
 ?>
 
 <div class='box_top'>Account Prestige</div>
@@ -18,83 +44,23 @@ $prestigeUnlocks['ba_raidtokens_unlock'] = array(
 
         <h2>Prestige Unlocks</h2>
         <div class="row">
-            <div class="col-md-4">
-                <!--  BA Raid Tokens -->
-                <div class="card text-white bg-danger mb-3" style="min-height: 170px;">
-                    <div class="card-header">BA Raid Tokens</div>
-                    <div class="card-body">
-                        <p class="card-text">
-                            Unlocking BA Raid Tokens will allow you to find Raid Tokens with normal Backalley searches, not just in Gold Rush!
-                        </p>
-                    </div>
-                    <div class="card-footer">
-                        <center>
-                            <a href="#"><button>Unlock</button></a>
-                        </center>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <!--  Super Attack -->
-                <div class="card text-white bg-success mb-3" style="min-height: 170px;">
-                    <div class="card-header">Super Attack</div>
-                    <div class="card-body">
-                        <p class="card-text">
-                            Unlocking Super Attack gives you access to a new feature where you can attack up to 50 players with one click of a button, helping
-                            you complete those important kill missions!
-                        </p>
+            <?php foreach ($prestigeUnlocks as $key => $prestigeUnlock): ?>
+                <div class="col-md-4">
+                    <div class="card text-white bg-danger mb-3" style="min-height: 170px;">
+                        <div class="card-header"><?php $prestigeUnlock['name'] ?>></div>
+                        <div class="card-body">
+                            <p class="card-text">
+                                <?php $prestigeUnlock['description'] ?>
+                            </p>
+                        </div>
+                        <div class="card-footer">
+                            <center>
+                                <a href="#"><button>Unlock</button></a>
+                            </center>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-4">
-                <!--  BA Gold Rush Boost -->
-                <div class="card text-white bg-danger mb-3" style="min-height: 170px;">
-                    <div class="card-header">BA Gold Rush Boost</div>
-                    <div class="card-body">
-                        <p class="card-text">
-                            Unlocking Ba Gold Rush boost gives you a 10% boosted chance of finding Gold Rush in the Backalley
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-4">
-                <!--  Crime Cash  -->
-                <div class="card text-white bg-danger mb-3" style="min-height: 170px;">
-                    <div class="card-header">Crime Cash Boost</div>
-                    <div class="card-body">
-                        <p class="card-text">
-                            Unlocking Crime Cash Boost gives an extra 10% cash from all crimes you complete.
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-4">
-                <!--  Throne Payouts -->
-                <div class="card text-white bg-success mb-3" style="min-height: 170px;">
-                    <div class="card-header">Throne Payout Boost</div>
-                    <div class="card-body">
-                        <p class="card-text">
-                            Unlocking Throne Payout Boost gives you an extra 20% payouts from all Boss/Under Boss positions held!
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-md-4">
-                <!--  Travel Cost Reducion -->
-                <div class="card text-white bg-danger mb-3" style="min-height: 170px;">
-                    <div class="card-header">Travel Cost Reduction</div>
-                    <div class="card-body">
-                        <p class="card-text">
-                            Unlocking Travel Cost Reduction gives you a 20% reduction on all travel costs.
-                        </p>
-                    </div>
-                </div>
-            </div>
+            <?php endforeach; ?>
         </div>
     </div>
 </div>
