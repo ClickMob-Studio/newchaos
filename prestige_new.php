a3742aab25f6f4e732e78ca05efb0e9b9d212b16
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-04-30 07:23:07 +0100

    up

---
+++ prestige_new.php
@@ -0,0 +1,30 @@
+<?php
+include "header.php";
+?>
+
+<div class='box_top'>Account Prestige</div>
+<div class='box_middle'>
+    <div class='pad'>
+        <p>Welcome to Account Prestiges!</p>
+
+        <h2>Prestige Unlocks</h2>
+        <div class="row">
+            <div class="col-md-4">
+                <!--  BA Raid Tokens -->
+                <div class="card text-bg-danger mb-3" style="max-width: 18rem;">
+                    <div class="card-header">BA Raid Tokens</div>
+                    <div class="card-body">
+                        <p class="card-text">
+                            Unlocking BA Raid Tokens will allow you to find Raid Tokens with normal Backalley searches, not just in /gold Rush!
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php
+include "footer.php";
+?>
