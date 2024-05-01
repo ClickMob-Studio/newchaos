956ef3000e4a56eedbe668d6a42965ca16e1ecc6
Aaron Newton <aaron.12.newton@gmail.com>
2024-05-01 09:16:25 +0100

    up

--- header.php
+++ header.php
@@ -705,22 +705,22 @@ if ($user_class->view_preference === '1') { ?>
             <div class="flex-grow-1 text-center">
                 <ul class="list-unstyled d-flex flex-row align-items-center justify-content-center">
                     <?php
-                    $now = time();
-                    $result = mysql_query("SELECT * FROM ads WHERE `timestamp` + (`displaymins` * 60) > $now ORDER BY RAND() LIMIT 1");
-                    if (!mysql_num_rows($result)) {
-                        $_messages = [
-                            'Invite your friends to play and receive <strong class="text-warning">50 Gold</strong> for every friend that plays. Hurry and start inviting now!',
-                            'For every friend you successfully refer, you\'ll earn <strong class="text-warning">50 Gold</strong>. Spread the word and let\'s play together!',
-                            'Attention all players! Invite your friends to join in on the fun. <strong class="text-warning">50 Gold</strong> reward for every successful referral.'
-                        ];
-                        $ref_message = $_messages[array_rand($_messages)];
-                        ?>
-                        <li class="flex-grow-1">
-                            <a href="refer.php"><?= $ref_message ?></a>
-                        </li>
-                        <?php
-                    } else {
-                        if (!$user_class->is_ads_disabled) {
+                    if (!$user_class->is_ads_disabled) {
+                        $now = time();
+                        $result = mysql_query("SELECT * FROM ads WHERE `timestamp` + (`displaymins` * 60) > $now ORDER BY RAND() LIMIT 1");
+                        if (!mysql_num_rows($result)) {
+                            $_messages = [
+                                'Invite your friends to play and receive <strong class="text-warning">50 Gold</strong> for every friend that plays. Hurry and start inviting now!',
+                                'For every friend you successfully refer, you\'ll earn <strong class="text-warning">50 Gold</strong>. Spread the word and let\'s play together!',
+                                'Attention all players! Invite your friends to join in on the fun. <strong class="text-warning">50 Gold</strong> reward for every successful referral.'
+                            ];
+                            $ref_message = $_messages[array_rand($_messages)];
+                            ?>
+                            <li class="flex-grow-1">
+                                <a href="refer.php"><?= $ref_message ?></a>
+                            </li>
+                            <?php
+                        } else {
                             $row = mysql_fetch_array($result);
                             $user_ads = new User($row['poster']);
                             $user_ads->avatar = $user_ads->avatar ?: "/images/no-avatar.png";
@@ -730,13 +730,13 @@ if ($user_class->view_preference === '1') { ?>
                             </li>
                             <li>
                                 <a href="#" onClick="reportAd(<?= $row['id'] ?>); return false;">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ff6218" class="bi bi-flag-fill" viewBox="0 0 16 16">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ff6218"
+                                         class="bi bi-flag-fill" viewBox="0 0 16 16">
                                         <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12 12 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A20 20 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a20 20 0 0 0 1.349-.476l.019-.007.004-.002h.001"/>
                                     </svg>
                                 </a>
                             </li>
                             <?php
-
                         }
                     }
                     ?>
