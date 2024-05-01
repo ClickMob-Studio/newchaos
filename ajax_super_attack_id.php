16d370533abe0a6b83af63459b7180e04805b5bc
Aaron Newton <aaron.12.newton@gmail.com>
2024-05-01 15:22:16 +0100

    up

---
+++ ajax_super_attack_id.php
@@ -0,0 +1,61 @@
+<?php
+
+//header('Content-type: application/json');
+session_start();
+
+include "classes.php";
+include "database/pdo_class.php";
+
+function error($msg)
+{
+    $response = array();
+    $response['success'] = false;
+    $response['error'] = $msg;
+
+    return $response;
+}
+
+function success($msg)
+{
+    $response = array();
+    $response['success'] = true;
+    $response['message'] = $msg;
+
+    return $response;
+}
+
+$m = new Memcache();
+$m->addServer('127.0.0.1', 11211, 33);
+
+$user_class = new User($_SESSION['id']);
+session_write_close();
+
+$response = array();
+
+if (!isset($_GET['alv'])) {
+    echo json_encode(error('Something went wrong.'));
+    exit;
+}
+if ($_GET['alv'] !== 'yes') {
+    echo json_encode(error('Something went wrong.'));
+    exit;
+}
+
+$protime = time();
+$sql = "hospital = 0 AND jail = 0 AND aprotection < {$protime} AND (gang <> $user_class->gang || gang = 0)  AND admin < 1 AND hp > (50*level)/4 AND id <> $user_class->id ";
+$db->query("SELECT `id` FROM `grpgusers` WHERE " . $sql . " ORDER BY rand() DESC LIMIT 1");
+$db->execute();
+$attack_id = $db->fetch_single();
+
+if ($attack_id > 0) {
+    echo json_encode(array(
+        'success' => true,
+        'attack_id' => $attack_id
+    ));
+} else {
+    echo json_encode(array(
+        'success' => false
+    ));
+}
+exit;
+
--- super_attack.php
+++ super_attack.php
@@ -19,14 +19,41 @@ if (checkCaptchaRequired($user_class)) {
 <div class='box_top'>Super Attacks</div>
 <div class='box_middle'>
     <div class='pad'>
-        <p>Welcome to Super Attacks, click the button below to start attacking! With every click, you'll attack a random attackable offline player.</p>
+        <center>
+            <p>Welcome to Super Attacks, click the button below to start attacking! With every click, you'll attack a random attackable offline player.</p>
 
-        <br />
-        <a href="#"><button>Commit Attack</button></a>
+            <div class='ajax-message-holder' style='min-height: 60px; display: none;'></div>
+
+            <br />
+            <a href="ajax_super_attack_id.php" id="commit-super-attack-link"><button>Commit Attack</button></a>
+        </center>
 
 
     </div>
 </div>
+
+<script type="text/javascript">
+    $('#commit-super-attack-link').click(function(e) {
+        e.preventDefault();
+
+        var request = $.ajax({
+            url: $(this).attr('href') + '&alv=yes',
+            method: "GET",
+            dataType: "json"
+        });
+        request.done(function (res) {
+            console.log(res);
+            if (res.success == false || res.success == 'false') {
+                var resMes = "<div class='alert alert-danger ajax-alert-div'><p>You don't have anyone you can attack at the moment. Consider trying a different city.</p></div>";
+                $(".ajax-message-holder").html(resMes);
+                $(".ajax-message-holder").show();
+            } else {
+
+            }
+        });
+
+    });
+</script>
 
 <?php
 
