5e72d88d2be8f37e5b83f537ae86e2a81c67fa52
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-02 13:17:32 +0100

    Update headertest.php

--- headertest.php
+++ headertest.php
@@ -655,7 +655,7 @@ if ($user_class->view_preference === '1') { ?>
                                     
                                     <div class="row heroTop heroTop2">
                                             <div class="col-12 col-lg-7 offset-lg-1 g-0 row realMission">
-                                            <?php if(mysql_num_rows($usermission)): ?>
+                                            <?php if($miss): ?>
                                                 <div class=" missionDiv">
                                                     <p class="missionTo">Kills:</p>
                                                     <p><?= $kills; ?></p>
