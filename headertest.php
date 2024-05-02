9024555d526e0bcd39d95c05d2f4cb654e1d6d18
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-02 13:27:01 +0100

    Update headertest.php

--- headertest.php
+++ headertest.php
@@ -651,7 +651,8 @@ if ($user_class->view_preference === '1') { ?>
 
                         <div class="dcBannerButtonsContainerMain">
                             <div class="col-12 col-lg-4">
-                                <div class="p-1 dcPanel dcAvatarPanel">
+                                <div class="p-1 dcPanel dcAvatarPanel" stlye="width: 140px;
+    margin-left: -60px;">
                                     <div class="row mb-3 mission">
                                         <h3 class='box_top'>Mission</h3>
                                     </div>
