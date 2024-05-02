9f98e94e28c2d5c7648d5477db7442857e99fd41
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-02 17:16:40 +0100

    Update headertest.php

--- headertest.php
+++ headertest.php
@@ -558,7 +558,7 @@ echo '<script src="js/java.js?12" type="text/javascript"></script>';
 <?php 
 
 if ($user_class->view_preference === '1') { ?>
-            <meta name="viewport" content="width=700">
+            <meta name="viewport" content="width=20000">
         <?php } else { ?>
             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
         <?php } ?>
