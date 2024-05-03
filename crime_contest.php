a4b4b3fbe97cdfe158429549aa176ae274db6312
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-05-03 06:29:02 +0100

    up

--- crime_contest.php
+++ crime_contest.php
@@ -13,6 +13,14 @@ $dailyCrimeRows = $db->fetch_row();
 $db->query("SELECT * FROM `user_comp_leaderboard` ORDER BY `overall_crimes_complete` DESC LIMIT 15");
 $db->execute();
 $overallCrimeRows = $db->fetch_row();
+
+$db->query("SELECT * FROM `user_comp_leaderboard` ORDER BY `daily_attacks_complete` DESC LIMIT 15");
+$db->execute();
+$dailyAttackRows = $db->fetch_row();
+
+$db->query("SELECT * FROM `user_comp_leaderboard` ORDER BY `overall_attacks_complete` DESC LIMIT 15");
+$db->execute();
+$overallAttackRows = $db->fetch_row();
 ?>
 
     <h1>Crime & Attack Contest</h1>
@@ -21,6 +29,7 @@ $overallCrimeRows = $db->fetch_row();
         some great prizes.
     </p>
 
+    <hr />
     <h2>Crimes</h2>
     <p>Each crime you complete that uses at least 75% of your max nerve will count as 1 point. Any other crime will not count.</p>
 
@@ -72,6 +81,74 @@ $overallCrimeRows = $db->fetch_row();
                                 <td><?php echo $i ?></td>
                                 <td><?php echo formatName($overallCrimeRow['user_id']) ?></td>
                                 <td><?php echo number_format($overallCrimeRow['daily_crimes_complete'], 0) ?></td>
+                            </tr>
+
+                            <?php $i++; ?>
+                        <?php endforeach; ?>
+                    <?php else: ?>
+                        <tr>
+                            <td colspan="3">
+                                No Results
+                            </td>
+                        </tr>
+                    <?php endif; ?>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <hr />
+    <h2>Attacks</h2>
+    <p>Each attack you complete will count as 1 point.</p>
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="table-container">
+                <h3>Daily Leaderboard</h3>
+                <table class="new_table" id="newtables" style="width:100%;">
+                    <tr>
+                        <th>&nbsp;</th>
+                        <th>User</th>
+                        <th>Points</th>
+                    </tr>
+                    <?php if (count($dailyAttackRows) > 0): ?>
+                        <?php $i = 1; ?>
+                        <?php foreach ($dailyAttackRows as $dailyAttackRow): ?>
+                            <tr>
+                                <td><?php echo $i ?></td>
+                                <td><?php echo formatName($dailyAttackRow['user_id']) ?></td>
+                                <td><?php echo number_format($dailyAttackRow['daily_crimes_complete'], 0) ?></td>
+                            </tr>
+
+                            <?php $i++; ?>
+                        <?php endforeach; ?>
+                    <?php else: ?>
+                        <tr>
+                            <td colspan="3">
+                                No Results
+                            </td>
+                        </tr>
+                    <?php endif; ?>
+                </table>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="table-container">
+                <h3>Overall Leaderboard</h3>
+                <table class="new_table" id="newtables" style="width:100%;">
+                    <tr>
+                        <th>&nbsp;</th>
+                        <th>User</th>
+                        <th>Points</th>
+                    </tr>
+                    <?php if (count($overallAttackRows) > 0): ?>
+                        <?php $i = 1; ?>
+                        <?php foreach ($overallAttackRows as $overallAttackRow): ?>
+                            <tr>
+                                <td><?php echo $i ?></td>
+                                <td><?php echo formatName($overallAttackRow['user_id']) ?></td>
+                                <td><?php echo number_format($overallAttackRow['daily_crimes_complete'], 0) ?></td>
                             </tr>
 
                             <?php $i++; ?>
