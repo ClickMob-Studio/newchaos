acd8244d24c475a3593e32ae597ffe6f21147b9f
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-07 12:52:21 +0100

    Update testy.php

--- testy.php
+++ testy.php
@@ -1,5 +1,8 @@
 <?php
 include 'header.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 ?>
 <div class='box_top'>MailBox</div>
 						<div class='box_middle'>
