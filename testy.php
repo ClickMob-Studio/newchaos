a8f8d327587892308023d081bea30e1316717a68
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-07 13:57:53 +0100

    Update testy.php

--- testy.php
+++ testy.php
@@ -422,7 +422,7 @@ $twenty_percent =$owned_points - $owned_points * 0.20;
 <div class="container mt-3">
     <div class="row justify-content-around">
         <!-- Highest Killer in City -->
-        <div class="col-6 col-md-4 mb-4">
+        <div class="col-6 col-md-4 col-lg-4 mb-4">
             <div class="vip-package p-3" style="box-shadow: 0 0 10px rgba(0,0,0,0.5);">
                 <?php if ($king_result): ?>
                     <h4 class="text-center" style="color: green;">Highest killer in <!_-cityname-_!> this hour</h4>
@@ -444,7 +444,7 @@ $twenty_percent =$owned_points - $owned_points * 0.20;
         </div>
 
         <!-- Highest Leveller This Hour -->
-        <div class="col-6 col-md-4 mb-4">
+        <div class="col-6 col-md-4 col-lg-4 mb-4">
             <div class="vip-package p-3" style="box-shadow: 0 0 10px rgba(0,0,0,0.5);">
                 <?php if ($king_result): ?>
                     <h4 class="text-center" style="color: green;">Highest Leveller this hour</h4>
@@ -467,7 +467,7 @@ $twenty_percent =$owned_points - $owned_points * 0.20;
 
         <!-- Highest Buster of the Hour -->
         <!-- Adding offset to center this box on mobile -->
-        <div class="col-12 col-md-4 mb-4 offset-md-4 offset-0">
+        <div class="col-12 col-md-4 col-lg-4 mb-4 offset-md-4 offset-0">
             <div class="vip-package p-3" style="box-shadow: 0 0 10px rgba(0,0,0,0.5);">
                 <?php if ($king_result): ?>
                     <h4 class="text-center" style="color: green;">Highest Buster of the hour</h4>
