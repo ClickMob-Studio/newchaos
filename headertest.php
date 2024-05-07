91654dd726effc9f3796e1249375f2cc80d1b511
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-07 20:09:10 +0100

    Update headertest.php

--- headertest.php
+++ headertest.php
@@ -655,28 +655,30 @@ if ($user_class->view_preference === '1') { ?>
             
         </div>
 
-        <!-- Nerve Bar -->
         <div class="col-3">
+        <p class="text-center"><i class="fa-solid fa-heart-pulse" style="color:#ff6218"></i>
+        <?= $user_class->hppercent;?>%</p>
             <div class="progress">
-                <div class="progress-bar bg-danger" role="progressbar" style="width: 100%" aria-valuenow="15" aria-valuemin="0" aria-valuemax="15">15</div>
+                <div class="progress-bar bg-success" role="progressbar" style="background-color: #ff6218 !important; width: <?= $user_class->hppercent; ?>%" aria-valuenow="<?= $user_class->hppercent; ?>" aria-valuemin="0" aria-valuemax="<?= $user_class->hppercent; ?>"></div>
             </div>
-            <p class="text-center">Nerve</p>
-        </div>
-
-        <!-- Happiness Bar -->
-        <div class="col-3">
-            <div class="progress">
-                <div class="progress-bar bg-warning" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">100</div>
-            </div>
-            <p class="text-center">Happiness</p>
+            
         </div>
         <div class="col-3">
+        <p class="text-center"><i class="fa-solid fa-heart-pulse" style="color:#ff6218"></i>
+        <?= $user_class->hppercent;?>%</p>
             <div class="progress">
-                <div class="progress-bar bg-primary" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">100</div>
+                <div class="progress-bar bg-success" role="progressbar" style="background-color: #ff6218 !important; width: <?= $user_class->hppercent; ?>%" aria-valuenow="<?= $user_class->hppercent; ?>" aria-valuemin="0" aria-valuemax="<?= $user_class->hppercent; ?>"></div>
             </div>
-            <p class="text-center">Life</p>
+            
         </div>
-    </div>
+        <div class="col-3">
+        <p class="text-center"><i class="fa-solid fa-heart-pulse" style="color:#ff6218"></i>
+        <?= $user_class->hppercent;?>%</p>
+            <div class="progress">
+                <div class="progress-bar bg-success" role="progressbar" style="background-color: #ff6218 !important; width: <?= $user_class->hppercent; ?>%" aria-valuenow="<?= $user_class->hppercent; ?>" aria-valuemin="0" aria-valuemax="<?= $user_class->hppercent; ?>"></div>
+            </div>
+            
+        </div>
 
     <!-- Additional Information (Money, Points, Merits) -->
     <div class="row mt-3">
