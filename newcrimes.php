622d5f5d2f80098eb3ba72ff06a15d5905a4b1f5
Aaron Newton <aaron.12.newton@gmail.com>
2024-05-07 14:12:49 +0100

    u

--- newcrimes.php
+++ newcrimes.php
@@ -161,7 +161,7 @@ $crimesave = ($m->get('crimesave' . $user_class->id)) ? $m->get('crimesave' . $u
                                 <div class="col-md-4">
                                     <p>Progress to next star:</p>
                                     <div class="progress pb-star-holder" role="progressbar" aria-valuenow="39.84" aria-valuemin="0" aria-valuemax="100" title="3984/10,000">
-                                        <div class="progress-bar bg-success pb-star-bar" style="background-color: #ff6218 !important;">
+                                        <div class="progress-bar bg-success pb-star-bar" style="background-color: #ff6218 !important; width: 39.84%">
                                             <span class="pb-star-text"></span>
                                         </div>
                                     </div>
