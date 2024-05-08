d0835d70a9a5fee7f15f4ae95aac8bb2f7339d77
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-05-08 04:08:06 +0100

    u

--- jail_new.php
+++ jail_new.php
@@ -6,6 +6,10 @@ if(isset($_GET['jailbreak'])){
     $jailbreak = '';
 }
 
+if (checkCaptchaRequired($user_class)) {
+    header('Location: captcha.php?token=' . $user_class->macro_token . '&page=jail');
+}
+
 if ($user_class->jail_bot_credits < 1) {
     mysql_query("UPDATE `grpgusers` SET `is_jail_bots_active` = 0 WHERE `id` = " . $user_class->id);
     $user_class->is_jail_bots_active = 0;
@@ -24,11 +28,11 @@ if ($jailbreak != ""){
     if(empty($_GET['token'])){
         echo Message("There has been a issue");
     }
-    if($_GET['token'] != $_SESSION['token']){
+    if($_GET['token'] != $_SESSION['tokens']){
         $mes = "Something has gone wrong";
         $error = true;
     }else{
-        unset($_SESSION['token']);
+        unset($_SESSION['tokens']);
     }
 
     if (!$error){
@@ -73,17 +77,23 @@ if ($jailbreak != ""){
 
             }
         } else {
-            $jailed_person = new User($jailbreak);
-            $error = false;
-            if ($jailed_person->formattedname == ""){
-                $_SESSION['message'] = 'That person does not exist.';
-                $error = true;
+            $db->query("SELECT * FROM grpgusers WHERE id = " . $jailbreak);
+            $db->execute();
+            $jailed_person = $db->fetch_row();
+
+            if (!isset($jailed_person[0])) {
+                echo Message('That person does not exist.');
+                include 'footer.php';
+                exit;
             }
-            if ($jailed_person->id == $user_class->id) {
+            $jailed_person = $jailed_person[0];
+
+            $error = false;
+            if ($jailed_person['id'] == $user_class->id) {
                 $_SESSION['message'] = "You can't break yourself out of jail.";
                 $error = true;
             }
-            if ($jailed_person->jail == "0"){
+            if ($jailed_person['jail'] == "0"){
                 $_SESSION['message'] = "That person is not in jail.";
                 $error = true;
             }
@@ -95,7 +105,7 @@ if ($jailbreak != ""){
                 $_SESSION['message'] = "You can't break people out of jail whilst your in jail.";
                 $error = true;
             }
-            $chance = rand(1,(100 * $crime - ($user_class->speed / 25)));
+            $chance = rand(1,(100 * 1 - ($user_class->speed / 25)));
             //$money = 785;
             $nerve = 10;
             $exp = 2500;
@@ -106,8 +116,8 @@ if ($jailbreak != ""){
                         $_SESSION['message'] = "Success! You receive ".$exp." exp and 3 points";
                         $exp = $exp + $user_class->exp;
                         $crimesucceeded = 1 + $user_class->crimesucceeded;
-                        $crimemoney = $money + $user_class->crimemoney;
-                        $money = $money + $user_class->money;
+                        $crimemoney = $user_class->crimemoney;
+                        //$money = $money + $user_class->money;
                         $nerve = $user_class->nerve - $nerve;
                         if ($user_class->gang != 0) {
                             mysql_query("UPDATE gangs SET dailyBusts = dailyBusts + 1 WHERE id = ".$user_class->gang);
@@ -122,9 +132,9 @@ if ($jailbreak != ""){
                         $toadd = array('botd' => 1);
                         ofthes($user_class->id, $toadd);
                         bloodbath('busts', $user_class->id);
-                        $result = mysql_query("UPDATE `grpgusers` SET `jail` = '0' WHERE `id`='".$jailed_person->id."'");
+                        $result = mysql_query("UPDATE `grpgusers` SET `jail` = '0' WHERE `id`='".$jailed_person['id']."'");
                         //send even to that person
-                        Send_Event($jailed_person->id, "You have been busted out of Jail by [-_USERID_-].", $user_class->id);
+                        Send_Event($jailed_person['id'], "You have been busted out of Jail by [-_USERID_-].", $user_class->id);
 
                         //header('Location: jail.php');
                     }elseif ($chance >= 150) {
@@ -211,7 +221,7 @@ if($user_class->jail > 0){
                 $ignore = array($user_class->id);
                 $ignore = implode(',', $ignore);
 
-                $result = mysql_query("SELECT * FROM `grpgusers` ORDER BY `jail` DESC");
+                $result = mysql_query("SELECT `id`, `jail`, `lastactive` FROM `grpgusers` WHERE jail > 0 ORDER BY RAND() LIMIT 4");
                 function generateRandomString($length = 10) {
                     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
                     $randomString = '';
@@ -222,18 +232,19 @@ if($user_class->jail > 0){
                     return $randomString;
                 }
                 $token = generateRandomString(10);
-                $_SESSION['token'] = $token;
+                $_SESSION['tokens'] = $token;
                 if(mysql_num_rows($result) || ($user_class->jail_bot_credits > 0 && $user_class->is_jail_bots_active)){
                     if (mysql_num_rows($result) > 0) {
                         while($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
                             $secondsago = time()-$line['lastactive'];
-                            $user_jail = new User($line['id']);
-                            if (floor($user_jail->jail / 60) != 1) {
+                            $formattedName = formatName($line['id']);
+
+                            if (floor($line['jail'] / 60) != 1) {
                                 $plural = "s";
                             }
 
-                            if($user_jail->jail != 0){
-                                echo "<tr class='jail-cell-row'><td>".$user_jail->formattedname."</td><td>".floor($user_jail->jail / 60)." m"."</td><td><a class='jail-break-link' data-jid='".$user_jail->id."' href='?jailbreak=".$user_jail->id."&token=".$token."'>Break Out</a></td></tr>";
+                            if($line['jail'] != 0){
+                                echo "<tr class='jail-cell-row'><td>".$formattedName."</td><td>".floor($line['jail'] / 60)." m"."</td><td><a class='jail-break-link' data-jid='".$line['id']."' href='?jailbreak=".$line['id']."&token=".$token."' class='btn btn-primary w-100'>Break Out</a></td></tr>";
                             }
                         }
                     }
@@ -246,7 +257,7 @@ if($user_class->jail > 0){
                         }
 
                         while ($i <= $limit) {
-                            echo "<tr class='jail-cell-row'><td>Bot</td><td>2m</td><td><a class='jail-break-link' data-jid='bot' href='?jailbreak=bot&token=".$token."'>Break Out</a></td></tr>";
+                            echo "<tr class='jail-cell-row'><td>Bot</td><td>2m</td><td><a class='jail-break-link' data-jid='bot' href='?jailbreak=bot&token=".$token."' class='btn btn-primary w-100'>Break Out</a></td></tr>";
 
                             $i++;
                         }
@@ -261,32 +272,67 @@ if($user_class->jail > 0){
         </td></tr>
 
     <script type="text/javascript">
+        function sortTable() {
+            //get the parent table for convenience
+            let table = document.getElementById("jail-table");
+
+            //1. get all rows
+            let rowsCollection = table.querySelectorAll("tr");
+
+            //2. convert to array
+            let rows = Array.from(rowsCollection)
+                .slice(1); //skip the header row
+
+            //3. shuffle
+            shuffleArray(rows);
+
+            //4. add back to the DOM
+            for (const row of rows) {
+                table.appendChild(row);
+            }
+        }
+
+
+        /**
+         * Randomize array element order in-place.
+         * Using Durstenfeld shuffle algorithm.
+         * from: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array/12646864#12646864
+         */
+        function shuffleArray(array) {
+            for (var i = array.length - 1; i > 0; i--) {
+                var j = Math.floor(Math.random() * (i + 1));
+                var temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        sortTable();
+
         let jailBreakClicks = 0;
+        let jailRefreshes = 0;
 
         $('.jail-break-link').click(function(e) {
-             if ($(this).data('jid') == 'bot') {
+            if ($(this).data('jid') == 'bot') {
                 e.preventDefault();
 
                 $(this).closest('tr').remove();
 
-                 var request = $.ajax({
-                     url: 'ajax_jail_new.php?jailbreak=bot',
-                     method: "GET",
-                     dataType: "json"
-                 });
-                 request.done(function (res) {
-                     if (res.success == false || res.success == 'false') {
-                         var resMes = "<div class='alert alert-danger ajax-alert-div'><p>" + res.error + "</p></div>";
-                     } else {
-                         var resMes = "<div class='alert alert-info ajax-alert-div'><p>" + res.message + "</p></div>";
-                     }
+                var request = $.ajax({
+                    url: 'ajax_jail_new.php?jailbreak=bot',
+                    method: "GET",
+                    dataType: "json"
+                });
+                request.done(function (res) {
+                    if (res.success == false || res.success == 'false') {
+                        var resMes = "<div class='alert alert-danger ajax-alert-div'><p>" + res.error + "</p></div>";
+                    } else {
+                        var resMes = "<div class='alert alert-info ajax-alert-div'><p>" + res.message + "</p></div>";
+                    }
 
-                     console.log(res.jail_bot_credits);
-
-                     $(".ajax-message-holder").html(resMes);
-                     $(".ajax-message-holder").show();
-                     $('.jail-bot-credit-count').html(res.jail_bot_credits);
-                 });
+                    $(".ajax-message-holder").html(resMes);
+                    $(".ajax-message-holder").show();
+                    $('.jail-bot-credit-count').html(res.jail_bot_credits);
+                });
             } else {
                 $('.jail-break-link').remove();
             }
@@ -296,6 +342,11 @@ if($user_class->jail > 0){
             $.get("ajax_jail_new.php?action=fetch_users", {}, (jailers) => {
                 $('.jail-cell-row').remove();
 
+                jailRefreshes = jailRefreshes + 1;
+                if (jailRefreshes % 30 == 0) {
+                    confirm("You are still here aren't you?");
+                }
+
                 if (jailers != false) {
                     jailers.forEach((data, index) => {
 
@@ -303,14 +354,18 @@ if($user_class->jail > 0){
                             '<tr class="jail-cell-row">' +
                             '<td>' + data.username + '</td>' +
                             '<td>' + data.time + '</td>' +
-                            '<td><a class="jail-break-link" data-jid="' + data.id + '" href="?jailbreak=' + data.id + '&token=<?php echo $token ?>" data-user-id="' + data.id + '" class="break-out-link">Break Out</a></td>' +
+                            '<td><a class="jail-break-link" data-jid="' + data.id + '" href="?jailbreak=' + data.id + '&token=<?php echo $token ?>" data-user-id="' + data.id + '" class="break-out-link btn btn-primary w-100">Break Out</a></td>' +
                             '</tr>'
                         );
 
                     })
+
+                    sortTable();
                 }
 
                 $('.jail-break-link').click(function(e) {
+                    jailRefreshes = 0;
+
                     if ($(this).data('jid') == 'bot') {
                         e.preventDefault();
 
@@ -340,6 +395,37 @@ if($user_class->jail > 0){
                 });
             }, "json")
         }, 4000);
+
+        document.addEventListener("DOMContentLoaded",function(){
+            document.body.addEventListener('click', function(evt) {
+                // Check for an actual mouse click (1, 2 & 3)
+                if (evt.which > 3) {
+                    var request = $.ajax({
+                        url: 'ajax_autoclick_detection.php?page=jail&reason=invalid_click',
+                        method: "GET",
+                        dataType: "json"
+                    });
+                    request.done(function (res) {
+                        console.log(res);
+                    });
+                }
+
+                if (evt.isTrusted) {
+
+                } else {
+                    var request = $.ajax({
+                        url: 'ajax_autoclick_detection.php?page=jail&reason=click_not_trusted',
+                        method: "GET",
+                        dataType: "json"
+                    });
+                    request.done(function (res) {
+                        console.log(res);
+                    });
+                }
+            }, true);
+        });
+
+
     </script>
 <?
 include 'footer.php';
