dc6f08027e2a84c28bbad79bfc188d45c95b51a6
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-08 11:33:32 +0100

    Update headertest.php

--- headertest.php
+++ headertest.php
@@ -735,7 +735,7 @@ if ($user_class->view_preference === '1') { ?>
         <div class="p-2 mt-2 position-relative">
     <a href="/jail.php" class="d-inline-block">
         <i class="fa-solid fa-handcuffs position-relative" style="/* font-size: 9px; */display: inline-block;z-index: 0;" aria-hidden="true">
-            <span class="position-absolute top-0 start-100 tran-middle badge rounded-pill bg-danger" style="font-size: xx-small;transform: translate(50%, -50%);z-index: -1;"><!_-Jail_-!></span>
+            <span class="position-absolute top-0 start-100 tran-middle badge rounded-pill bg-danger" style="font-size: xx-small;transform: translate(50%, -50%);z-index: -1;"><?php echo $jail;?></span>
         </i>
         <p>Jail</p>
     </a>
