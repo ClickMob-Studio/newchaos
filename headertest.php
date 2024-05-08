b1ce3f7afa7571ac222a6b6a76ae3f8185e4f577
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-08 11:48:27 +0100

    Update headertest.php

--- headertest.php
+++ headertest.php
@@ -708,7 +708,11 @@ if ($user_class->view_preference === '1') { ?>
         <div class="p-2 mt-2 position-relative">
           <a href="/pms.php?view=inbox">
             
-          <?php if($mailCount > 0) { 
+          <?php 
+              $db->query("SELECT count(viewed) FROM pms WHERE `to` = ? AND viewed = 1");
+              $db->execute(array($user_class->id));
+              $mailCount = $db->fetch_single();
+          if($mailCount > 0) { 
                 $style='style="color:#dc3545;"';
             } else { 
                 $style= '';
