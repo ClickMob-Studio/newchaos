118bb5e418b5a3cd195ac13fb3ec846e5f11099e
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-08 11:41:11 +0100

    Update headertest.php

--- headertest.php
+++ headertest.php
@@ -746,7 +746,7 @@ if ($user_class->view_preference === '1') { ?>
 
         <div class="p-2 mt-2 position-relative">
           <a href="/hospital.php">
-            <i class="fa-solid fa-message">
+            <i class="fa-solid fa-message" style="z-index:0;">
             <span class="position-absolute top-0 start-100 tran-middle badge rounded-pill bg-danger" style="font-size: xx-small;transform: translate(50%, -50%);z-index: -1;"><?echo $hosp; ?></span>
     
             </i>
