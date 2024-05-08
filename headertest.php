37d08f89be6b3b0481253f0696cc4d9a237b63c8
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-08 11:26:45 +0100

    Update headertest.php

--- headertest.php
+++ headertest.php
@@ -732,9 +732,8 @@ if ($user_class->view_preference === '1') { ?>
         </div>
         <div class="p-2 position-relative">
     <a href="/jail.php" class="d-inline-block">
-        <i class="fa-solid fa-handcuffs position-relative" style="font-size: 24px; display: inline-block;">
-            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
-                style="font-size: xx-small; transform: translate(50%, -50%);">0</span>
+        <i class="fa-solid fa-handcuffs position-relative" style="/* font-size: 9px; */display: inline-block;z-index: 0;" aria-hidden="true">
+            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style="font-size: xx-small;transform: translate(50%, -50%);z-index: -1;"><!_-jail-_!></span>
         </i>
         <p>Jail</p>
     </a>
