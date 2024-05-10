58cd85c1e355efd34ec86187105ebb86302a8bd4
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-10 20:28:59 +0100

    a

---
+++ ajax_gym_stats.php
@@ -0,0 +1,12 @@
+<?php 
+include "ajax_header.php";
+$user_class = new User($_SESSION['id']);
+
+$db->query("SELECT record_date, strength, defense, speed FROM daily_user_stats WHERE user_id = ? ORDER BY record_date ASC LIMIT 20");
+$db->execute([$user_class->id]);
+$gym = $db->fetch_row();
+
+echo json_encode($gym);
+
+?>
+
