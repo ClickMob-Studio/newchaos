71ea1668c46fbbd1003027c713c6d8107cf9cc36
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-10 20:16:54 +0100

    Update testgym.php

--- testgym.php
+++ testgym.php
@@ -3,7 +3,7 @@ include "ajax_header.php";
 
 
 $db->prepare("SELECT record_date, strength, defense, speed FROM daily_user_stats WHERE user_id = ?");
-$db->execute([$$user_class->id]);
+$db->execute([$user_class->id]);
 $results = $db->fetch_row();
 
 
