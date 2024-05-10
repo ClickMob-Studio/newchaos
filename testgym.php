8420ae8913b1553d69be05371459776d0aea36dd
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-10 19:11:26 +0100

    a

---
+++ testgym.php
@@ -0,0 +1,12 @@
+<?php 
+include "ajax_header.php";
+
+
+$db->query("SELECT record_date, strength, defense, speed FROM daily_user_stats WHERE user_id = ?");
+$db->bind(1, $$user_class->id, PDO::PARAM_INT);
+$results = $db->fetch_row();
+
+echo json_encode($results);
+
+?>
+
