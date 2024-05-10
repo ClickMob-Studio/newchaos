f9cb54690a1fe5a1157f7f2794c016c5fc71aad6
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-10 20:20:40 +0100

    Update testgym.php

--- testgym.php
+++ testgym.php
@@ -2,8 +2,9 @@
 include "ajax_header.php";
 $user_class = new User($_SESSION['id']);
 
-$gyms = mysql_query("SELECT record_date, strength, defense, speed FROM daily_user_stats WHERE user_id = ".$user_class->id);
-$gym = mysql_fetch_array($gyms);
+$db->query("SELECT record_date, strength, defense, speed FROM daily_user_stats WHERE user_id = ?");
+$db->execute([$user_class->id]);
+$gym = $db->fetch_row();
 
 var_dump($gym);
 echo json_encode($gym);
