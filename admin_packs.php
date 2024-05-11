4a0c5ef889b2a0be85db8160c7424f5b53666991
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-11 22:30:10 +0100

    Create admin_packs.php

---
+++ admin_packs.php
@@ -0,0 +1,25 @@
+<?php
+require "header.php";
+if($user_class->admin < 1 ){
+    die();
+}
+$db->query("SELECT * FROM pack_logs ORDER BY `id ` DESC LIMIT 25");
+$db->execute();
+$fetch= $db->fetch_row(); 
+?>
+
+<h1>Last 25 Packs Bought</h1>
+<table>
+<?php
+foreach ($fetch as $row){
+?>
+<tr>
+    <td><?= $row['userid'];?></td>
+    <td><?= $row['pack'];?></td>
+    <td><?= $row['credits_before']; ?></td>
+    <td><?= $row['credits_after']; ?></td>
+</tr>
+<?php
+
+}
+?>
