a487fd989e11c327889901f9f8954525bdcd8ca9
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-11 21:51:05 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <?php
 
 session_start();
+header('Content-Type: text/html; charset=utf-8');
 
 
 // Get the name of the current script and the full request URI to check for specific query parameters
