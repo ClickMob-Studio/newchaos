f10f5600970055f2a6f5fae96a2842ef01bd3f53
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-11 22:32:21 +0100

    Update admin_packs.php

--- admin_packs.php
+++ admin_packs.php
@@ -1,25 +1,25 @@
 <?php
 require "header.php";
-if($user_class->admin < 1 ){
+if ($user_class->admin < 1) {
     die();
 }
 $db->query("SELECT * FROM pack_logs ORDER BY `id` DESC LIMIT 25");
 $db->execute();
-$fetch= $db->fetch_row(); 
+$fetch = $db->fetch_row();
 ?>
 
 <h1>Last 25 Packs Bought</h1>
 <table>
-<?php
-foreach ($fetch as $row){
-?>
-<tr>
-    <td><?= $row['userid'];?></td>
-    <td><?= $row['pack'];?></td>
-    <td><?= $row['credits_before']; ?></td>
-    <td><?= $row['credits_after']; ?></td>
-</tr>
-<?php
+    <?php
+    foreach ($fetch as $row) {
+        ?>
+        <tr>
+            <td><?= formatName($row['userid']); ?></td>
+            <td><?= $row['pack']; ?></td>
+            <td><?= $row['credits_before']; ?></td>
+            <td><?= $row['credits_after']; ?></td>
+        </tr>
+        <?php
 
-}
-?>
+    }
+    ?>
