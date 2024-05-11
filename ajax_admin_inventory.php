48327f0aba464f307b5263180d0154b4ce4a3b58
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-11 17:13:36 +0100

    a

--- ajax_admin_inventory.php
+++ ajax_admin_inventory.php
@@ -4,6 +4,7 @@ $user_class = new User($_SESSION['id']);
 if($User_class->admin < 1) {
     die();
 }
+echo "HELLLOOOOO";
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['action'] == 'update_item') {
     $userid = $_POST['userid'];
     $itemid = $_POST['itemid'];
