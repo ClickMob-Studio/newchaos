8dd4e04ebd3cdd587e3d4a07b7b667f7396725df
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-12 10:31:33 +0100

    Update admin_panel.php

--- admin_panel.php
+++ admin_panel.php
@@ -15,6 +15,7 @@ if ($user_class->admin < 1) {
     <li><a href="admin_mdpl.php">Daily Mission Payout Logs</a></li>
     <li><a href="admin_rbl.php">Raid Battle Logs</a></li>
     <li><a href="admin_gang_vault_logs.php">Gang Vault Logs</a></li>
+    <li><a href="admin_packs.php">Pack Logs</a></li>
 </ul>
 
 <?php
