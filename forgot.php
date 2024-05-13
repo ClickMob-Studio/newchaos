3f1f71da6952f59e32fd8d5c7d460f926b0750a8
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-13 20:15:47 +0100

    Update forgot.php

--- forgot.php
+++ forgot.php
@@ -102,9 +102,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['username'])) {
        'FromEmail' => "admin@chaoscity.co.uk",
        'FromName' => "Chaos City",
        'Subject' => "Forgot Password",
-       'Text-part' => "You have requested a password reset at ChaosCity!",
-       'Html-part' => "<h3>Dear passenger, welcome to <a href=\"https://www.mailjet.com/\">Mailjet</a>!<br />May the delivery force be with you!",
-       'Recipients' => [
+       'TextPart' => "You have requested a password reset at ChaosCity!",
+       'HTMLPart' => "<h3>Dear $username, You have requested a new password reset at <a href='http://www.chaoscity.co.uk'>Chaos City</a>.<br><a href='https://www.chaoscity.co.uk/forgot.php?action=reset&token=$token'>Click Here</a> to reset your password</h3>",
+        'Recipients' => [
            [
                'Email' => $email
            ]
