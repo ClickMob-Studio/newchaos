0554a67d25ee9f52052d4821e26c1930952dc1a4
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-13 16:34:02 +0100

    Update testing.php

--- testing.php
+++ testing.php
@@ -1,23 +1,23 @@
 <?php
 require 'vendor/autoload.php';
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-//use \Mailjet\Resources;
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+// //use \Mailjet\Resources;
 
 
-$apikey = '7dc2ad83e7f15563b1dee7d48109dbb7';
-$apisecret = '15326068ed7ef53039e03ca05662bde2';
+// $apikey = '7dc2ad83e7f15563b1dee7d48109dbb7';
+// $apisecret = '15326068ed7ef53039e03ca05662bde2';
 
-//$mj = new \Mailjet\Client($apikey, $apisecret);
+// //$mj = new \Mailjet\Client($apikey, $apisecret);
 
-//$response = $mj->get(Resources::$Contact);
+// //$response = $mj->get(Resources::$Contact);
 
-/*
-  Read the response
-*/
-if ($response->success())
-  var_dump($response->getData());
-else
-  var_dump($response->getStatus());
-?>
+// /*
+//   Read the response
+// */
+// if ($response->success())
+//   var_dump($response->getData());
+// else
+//   var_dump($response->getStatus());
+// ?>
