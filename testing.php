aa3246088e62d55698c3b501a41d61265fde19d8
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-13 16:54:39 +0100

    Update testing.php

--- testing.php
+++ testing.php
@@ -4,21 +4,20 @@
  error_reporting(E_ALL);
 require 'vendor/autoload.php';
 
-// //use \Mailjet\Resources;
+use \Mailjet\Resources;
 
 
-// $apikey = '7dc2ad83e7f15563b1dee7d48109dbb7';
-// $apisecret = '15326068ed7ef53039e03ca05662bde2';
+ $apikey = '7dc2ad83e7f15563b1dee7d48109dbb7';
+ $apisecret = '15326068ed7ef53039e03ca05662bde2';
+$mj = new \Mailjet\Client($apikey, $apisecret);
 
-// //$mj = new \Mailjet\Client($apikey, $apisecret);
+$response = $mj->get(Resources::$Contact);
 
-// //$response = $mj->get(Resources::$Contact);
-
-// /*
-//   Read the response
-// */
-// if ($response->success())
-//   var_dump($response->getData());
-// else
-//   var_dump($response->getStatus());
-// ?>
+ /*
+  Read the response
+ */
+if ($response->success())
+  var_dump($response->getData());
+ else
+   var_dump($response->getStatus());
+ ?>
