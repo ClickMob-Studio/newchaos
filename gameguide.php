08938eeaefa6f5f71643d635e70994787356b17d
Aaron Newton <aaron.12.newton@gmail.com>
2024-05-21 11:03:17 +0100

    u

--- gameguide.php
+++ gameguide.php
@@ -1,111 +1,22 @@
 <?php
 include 'header.php';
 ?>
-<tr><td class="contentspacer"></td></tr><tr><td class="contenthead">Game Guide</td></tr>
-<tr><td class="contentcontent">
-<center><b>Mafia-Revenge Game Guide</b></center><br /><br />
-<center><b>Contents</b></center>
-<table width="100%" style="border: 1px solid #444444;" cellpadding="3" cellspacing="0">
-    <tr>
-        <td style="border-right: 1px solid #444444; border-bottom: 1px solid #444444;">1. <a href="#1">Top Status Bar</a></td>
-        <td style="border-right: 1px solid #444444; border-bottom: 1px solid #444444;">2. <a href="#2">The Home/Index Page</a></td>
-        <td style="border-bottom: 1px solid #444444;">3. <a href="#3">The Updates List</a></td>
-    </tr>
-    <tr>
-        <td style="border-right: 1px solid #444444; border-bottom: 1px solid #444444;">4. <a href="#4">Inventory</a></td>
-        <td style="border-right: 1px solid #444444; border-bottom: 1px solid #444444;">5. <a href="#5">Your City</a></td>
-        <td style="border-bottom: 1px solid #444444;">6. <a href="#6"></a></td>
-    </tr>
-    <tr>
-        <td>7. </td>
-        <td>8. </td>
-        <td>9. </td>
-    </tr>
-</table>
-<br /><br />
-<a href="#" name="1"></a>
-<b><u>1. Top Status Bar</u></b> - <a href="#top">Top</a><br />
-The stats on the top of the screen show your gang tags and your username (example: [Mod] SpecialOps0). Below that it shows your current level, under that it shows how much money you currently have outside of the bank. Under that it says bank which is a clickable to deposit money and next to that it shows how much money is in your bank currently. Below that it shows how many points you have, under that is how many credits you have. The use option takes you to a page where you can spend your points and the buy option takes you to the page where you can spend $USD to buy in game items).<br />
-On the right hand side on the top are images of your current stats. This displays your current HP, energy, awake, nerve, and experience. If you hover over the bar it will show how much you have, out of how much your max currently is. The more green the bar is the better.<br /><br />
-<a href="#" name="2"></a>
-<b><u>2. The Home/Index Page</u></b> - <a href="#top">Top</a><br />
-The Home button contains a lot of useful information. Under General Information it shows your name, your level, your cash, your cash in your bank, your current experience (with an exp guide), marijuana, HP, energy, awake, nerve, work EXP, and GCSEs. They are all displayed in numbers for ease.<br />
-Below that is Attributes, all this does is tell your current strength, speed, defense, and total stats.<br />
-Next up is Modded Attributes, this shows your current stats with the percentage your weapon, armor, and shoes add on to them.<br />
-Under that is your Battle Stats, showing how many battles you've won, lost, the total added together of win and loss and how much money you've gained, or lost from killing people.<br />
-Crime Stats; this shows how many crimes you have succeeded, failed, the total of them and how much money you've made off of crimes.<br />
-Daily Statistics show how many kills you've had today and how much EXP you've gained today. This will reset to 0 each day for you to train back up and show it.<br />
-Next up is My Badges, which is a link to view your badges. Badges are just awards for various things that have been done in the game itself.<br /><br />
-<a href="#" name="3"></a>
-<b><u>3. The Updates List</u></b> - <a href="#top">Top</a><br />
-This is a list of what is currently being updated or what will be updated if known in advance by the staff and will be beneficial for you to check it out to see what is happening in game.<br /><br />
-<a href="#" name="4"></a>
-<b><u>4. Inventory</u></b> - <a href="#top">Top</a><br />
-Your inventory shows items that you currently own. It also shows your equipped weapon, armor and shoes. From your inventory you can control your items by selling them, marketing them, equipping them. You can only have 250 items max, in your inventory at one time.<br /><br />
-<a href="#" name="5"></a>
-<b><u>5. Your City</u></b> - <a href="#top">Top</a><br />
-This is a very important option to use. You will click this nearly the most in the entire game. You can do countless options from this menu. This clickable brings up over 20 options to choose from as well as showing today's top hitman with how many kills and the top exp gainer with how much.<br /><ul type="disc">
-    <li> The weapons store allows you to buy weapons for a set amount of cash, the item market which I will talk about below is a better deal for already bought items, which people are selling.<br />
-    <li>The armour store allows you to buy armor for a set amount of cash, the item market again will be a better deal always check there.<br />
-    <li>The shoes market will allow you to buy shoes at a set amount of cash, the item market is a better deal.<br />
-    <li>The point shop allows you to refill your nerve, and energy for 10 points, it will refill awake for 1 point every %, you can also sell 5 points for 5k if you are in desperate need, however I'm sure others will gladly buy them for a lot more.<br />
-    <li>The point store allows you to buy points 500 points for 5 million dollars, 1,000 points for 9 million, 2,000 for 16 million, 5,000 for 35 million, which are great deals if you have the cash to spare.<br />
-    <li>The Pharmacy sells medical certificates and medical kits. The certs will get you out of the hospital, and the kits will heal your HP.<br />
-    <li>The car lot sells cars which I will explain in one of the next topics.
-        <br /><br />
-    <li>Move house allows you to purchase/sell a house which refill your awake.<br />
-    <li>Mailbox, also on the menu option shows your mail, the new mail appears in yellow standing for how many messages you have.<br />
-    <li>Events appear in yellow as well, showing how many you have if it's yellow.<br />
-    <li>Inventory is already explained in section 4.<br />
-    <li>Manage land (RM Only) allows you to plant weed plants and other drug plants on your land.<br />
-    <li>My pet allows you to view your pet (more to be added).
-        <br /><br />
-    <li>Item market is where people sell their used and or new weapons, armor, shoes, med certs/kits and items of any kind. They are usually a lot cheaper than the store price.<br />
-    <li>The points market is where players sell their points they have for a set amount where you can buy. It's always good to check the current point ratio for the best buy or sell possible. <br />
-    <li>The pet market is where you can buy or sell your pets (dogs).
-        <br /><br />
-    <li>The hall of fame shows who is in the lead for their level, strength, defense, speed, money (out of bank), points, total stats, RM days, bank, kills, deaths and forum posts. This can be selected for search in a certain city as well.<br />
-    <li>The gang hall of fame displays who is in lead for gang level, vault money, vault points, and total member stats.<br />
-    <li>Top/bottom rated is a list of mobsters who have the highest and lowest ranking  showing their rank in the highest/lowest rated and their rating.<br />
-    <li>World Leaders is an option in which all the staff are listed. You are free to contact us at any time for help.<br />
-    <li>Mobster search will take you to an option to search for mobsters, you can customize the search based on their ID number, their name, level, cash they have on them, their location, online or offline, attackable, and their name. This search even allows you to save up to 3 search options!<br />
-    <li>Mobsters List allows you to browse through the list of all the players in the game based on their ID numbers first to last.<br />
-    <li>Mobsters online shows which players are online, their ID number, and when they were last active.<br />
-    <li>World stats allows you to see how many mobsters have been created, the amount of respected mobsters, the percent and number of male VS. female ratio, the total cash, the total cash in banks, the total points, the total crimes accomplished, the total kills, the total deaths, the total gangs, and the total gang cash.
-        <br /><br />
-    <li>Bus station allows you to travel to certain cities for an amount of cash, however your level will depend on which cities you can go to, it will also so the population in each city. Some cities are RM only.<br />
-    <li>The drive option, if you have your own car allows you to drive to any city for free as long as you have the required level. <br />
-    <li>The airport allows you to take a plane to another country for 25k each country. The countries depend on your level going up by 100, then 50 each country after that. Some countries are RM only.<br />
-    <li>Your garage allows you to see the cars you have.
-        <br /><br />
-    <li>Drug dealer (RM only) is where you can buy marijuana seeds for your land and sell your weed once it's been grown.<br />
-    <li>Lucky Boxes is something to do EVERY hour. There are 20 boxes/places to choose from so hurry because they fill fast! Each person chooses one and one of the places are chosen to win 20 points randomly each time.<br />
-    <li>Russian Roulette allows you to pick how many bullets you use 1, 2, 3, or 4. These will get you prizes, however you also have the option to go to the hospital for 20 minutes if you are shot.<br />
-    <li>Point smuggling is something to do EVERY day. What you do is you click on a country to smuggle points from and you gain 0<li>4 points usually. There is a limit of tries you have and it's just for fun you can not die and nothing will happen other than gaining points.
-        <br /><br />
-    <li>The cash lottery allows you to buy 2 tickets a day at max for 10k cash each. At the end of the day a winner is randomly chosen and you will receive 95% of the winnings.<br />
-    <li>The points lottery is the exact same except with points. 2 tickets are the max each day, however they are 30 points a ticket and you get 95% of the prize. 1 winner is randomly chosen at the end of the day to receive the prize.<br />
-    <li>Lucky dip is something to do EVERY day, you use 1k cash and you receive up to 20k cash back from it. <br />
-    <li>The slot machines are exactly what you think they are, you pay 100$ a pull and usually end up with more than you spend but only by a few hundred.<br />
-    <li>50/50 game (cash) allows players to bet a certain amount of cash and if someone else accepts that bet one winner is randomly chosen and they receive 95% of the winnings there is a minimum bet of 1k. This is between 2 players.<br />
-    <li>50/50 game (points) allows players to bet points against each other and accept bets. There is a minimum bet of 10 points and the randomly selected winner receives 95% of the winnings. This is between 2 players.<br />
-    <li>Roulette costs 1k each spin, there is 20 spins a day and all you do is guess 1 number, if your number is picked you win cash.
-        <br /><br />
-    <li>Search downtown is a MUST every day. What happens is you click search downtown and you get 100 random events including cash, points, and possibly items. The amount of cash, and points depend on your level, where as items depend on luck.<br />
-        The higher the level the more cash and points you get from searching downtown.<br />
-    <li>Job center is something to check every once in a while, depending on your stats you can choose a job that pays you cash every single day!<br />
-    <li>Gang list lists all the gangs starting from highest gang level to lowest. It shows the current members, the gang level, and the gang leader.<br />
-    <li>Your gang takes you to your gang homepage.<br />
-    <li>Real estate agency (RM only) allows you to buy 1-100,000 acres of land. In each city there is a limit of 100,000 acres to everyone in the game. <br />
-    <li>School allows you to choose a course to take over a period of time. In turn this raises your strength, speed, defense, and GCSE's. Each course requires a certain amount of GCSE's and costs some money.
-        <br /><br />
-    <li>Pet Hall of Fame allows you to view who's pet has the highest strength, defense, and speed as well as their pet's name and type.<br />
-    <li>Pet shop allows you to buy different types of dogs for a certain amount of cash, which you can raise and train yourself.<br />
-    <li>Pet upgrades is where you can buy different shots to get your pet better in strength, defense, and speed for a hefty sum of cash.<br />
-    <li>My pets allows you to view your pet and train them/use items on them.<br />
-    <li>Pet tracks allows you to choose to race another dog and hold bets of cash to see who will win.<br />
-</ul>
-</td></tr>
+
+<div class='box_top'>Quick Start Guide</div>
+<div class='box_middle'>
+    <div class='pad'>
+
+        <p>
+            Welcome to the Chaos City Quick Start Guide. This guide is composed to help get you up and running quickly on the game with the basic knowledge of how to start building your account.
+            If you have any issues or questions then be sure to join our Discord linked below which is full of our helpful community who will give any advice/answer any questions you need.
+        </p>
+        <a class="btn btn-primary" href="https://discord.gg/Pb7sTfhCnm">Join our Discord</a>
+
+        <h2 id="quick-start-guide">Quick Start Guide</h2>
+
+    </div>
+</div>
+
 <?php
 include 'footer.php';
 ?>
