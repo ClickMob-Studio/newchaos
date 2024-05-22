29ecfa8091a4bac6c4b7281c7c41b206d568c5ed
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-22 18:55:36 +0100

    Update testing.php

--- testing.php
+++ testing.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-//$radiobutton = $_POST['radiobutton'] ?? 0;
+$radiobutton = $_POST['radiobutton'];
 
 
 //$chance = explode("-", $user_class->gtachance);
