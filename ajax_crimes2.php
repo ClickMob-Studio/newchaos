2b2247f72187f6d81a88c38841a8650f25c02e7a
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-05-22 22:09:05 +0100

    u

--- ajax_crimes2.php
+++ ajax_crimes2.php
@@ -246,6 +246,10 @@ if (isset($_POST['id']) || isset($input['id'])) {
             return 0;
         }
 
+        if ($user_class->id == 2) {
+            Send_Event(2, $nerveneeded . ' - ' . $user_class->maxnerve . ' - ' . $cost, 2);
+        }
+
         $tempItemUse = getItemTempUse($user_class->id);
         $now = time();
         if ($tempItemUse['nerve_vial_time'] > $now) {
