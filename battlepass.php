d9ccf56391b9c3b27e951ddc3ab0c30c7db9696c
Aaron Newton <aaron.12.newton@gmail.com>
2024-05-24 11:54:58 +0100

    u

--- battlepass.php
+++ battlepass.php
@@ -30,12 +30,12 @@ $bpCategoryUser = getBpCategoryUser($bpCategory, $user_class);
 
         <h3>Your Stats</h3>
         <ul>
-            <li>Points: <?php echo $bpCategoryPrize['type'] ?></li>
-            <li>Crimes: <?php echo $bpCategoryPrize['crimes'] ?></li>
-            <li>Attacks: <?php echo $bpCategoryPrize['attacks'] ?></li>
-            <li>Mugs: <?php echo $bpCategoryPrize['mugs'] ?></li>
-            <li>Busts: <?php echo $bpCategoryPrize['busts'] ?></li>
-            <li>BA: <?php echo $bpCategoryPrize['backalley'] ?></li>
+            <li>Points: <?php echo $bpCategoryUser['type'] ?></li>
+            <li>Crimes: <?php echo $bpCategoryUser['crimes'] ?></li>
+            <li>Attacks: <?php echo $bpCategoryUser['attacks'] ?></li>
+            <li>Mugs: <?php echo $bpCategoryUser['mugs'] ?></li>
+            <li>Busts: <?php echo $bpCategoryUser['busts'] ?></li>
+            <li>BA: <?php echo $bpCategoryUser['backalley'] ?></li>
         </ul>
 
     </div>
