abf4a07e723accb8fdbdc1e42b83cd61e7e557c1
Aaron Newton <aaron.12.newton@gmail.com>
2024-05-24 11:32:00 +0100

    u

--- battlepass.php
+++ battlepass.php
@@ -14,6 +14,19 @@ $bpCategoryUser = getBpCategoryUser($bpCategory, $user_class);
 <div class='box_top'><h1>Battle Pass</h1></div>
 <div class='box_middle'>
     <div class='pad'>
+        <h2>Challenges</h2>
+        <ul>
+            <?php foreach ($bpCategoryChallenges as $bpCategoryChallenge): ?>
+                <li><?php echo $bpCategoryChallenge['type'] ?> - <?php echo $bpCategoryChallenge['amount'] ?> - <?php echo $bpCategoryChallenge['prize'] ?></li>
+            <?php endforeach; ?>
+        </ul>
+
+        <h2>Prizes</h2>
+        <ul>
+            <?php foreach ($bpCategoryPrizes as $bpCategoryPrize): ?>
+                <li><?php echo $bpCategoryPrize['type'] ?> - <?php echo $bpCategoryPrize['amount'] ?> - <?php echo $bpCategoryChallenge['cost'] ?></li>
+            <?php endforeach; ?>
+        </ul>
     </div>
 </div>
 
