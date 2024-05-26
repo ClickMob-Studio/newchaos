dca9fa8dc528d6848ce1a157ae5e0a846febfb7f
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-26 11:39:00 +0100

    Create store.php

---
+++ menu_items/store.php
@@ -0,0 +1,6 @@
+<div class="p-2 mt-2 position-relative" data-id="search">
+          <a href="/store.php">
+          <i class="fa-solid fa-cart-shopping"></i>
+            <p>Store</p>
+          </a>
+        </div>
