f6e6ac4e4e5472bb502b1411eab48c94119fb8d5
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-05-27 07:37:08 +0100

    u

--- battlepass.php
+++ battlepass.php
@@ -112,24 +112,22 @@ if (isset($_GET['claim_prize']) && (int)$_GET['claim_prize']) {
                     <td>
 
                         <?php foreach ($bpCategoryChallenges as $bpCategoryChallenge): ?>
-                            <div class="col-md-4">
-                                <div class="card text-white bg-danger mb-3">
-                                    <div class="card-header"><?php echo number_format($bpCategoryChallenge['amount'], 0) ?> x <?php echo ucfirst($bpCategoryChallenge['type']) ?></div>
-                                    <div class="card-body">
-                                        <p class="card-text">
-                                            <?php echo number_format($bpCategoryUser[$bpCategoryChallenge['type']], 0) ?>/<?php echo number_format($bpCategoryChallenge['amount'], 0) ?><br />
-                                            Points:  <?php echo $bpCategoryChallenge['prize'] ?>
-                                        </p>
-                                    </div>
-                                    <div class="card-footer">
-                                        <center>
-                                            <?php if ($bpCategoryUser[$bpCategoryChallenge['type']] >= $bpCategoryChallenge['amount'] && !in_array($bpCategoryChallenge['id'], $challengesClaimed)): ?>
-                                                <a href="battlepass.php?claim_challenge=<?php echo $bpCategoryChallenge['id'] ?>" class="btn btn-primary">(Complete)</a>
-                                            <?php elseif (in_array($bpCategoryChallenge['id'], $challengesClaimed)): ?>
-                                                <span style="color: green">Completed</span>
-                                            <?php endif; ?>
-                                        </center>
-                                    </div>
+                            <div class="card text-white bg-danger mb-3">
+                                <div class="card-header"><?php echo number_format($bpCategoryChallenge['amount'], 0) ?> x <?php echo ucfirst($bpCategoryChallenge['type']) ?></div>
+                                <div class="card-body">
+                                    <p class="card-text">
+                                        <?php echo number_format($bpCategoryUser[$bpCategoryChallenge['type']], 0) ?>/<?php echo number_format($bpCategoryChallenge['amount'], 0) ?><br />
+                                        Points:  <?php echo $bpCategoryChallenge['prize'] ?>
+                                    </p>
+                                </div>
+                                <div class="card-footer">
+                                    <center>
+                                        <?php if ($bpCategoryUser[$bpCategoryChallenge['type']] >= $bpCategoryChallenge['amount'] && !in_array($bpCategoryChallenge['id'], $challengesClaimed)): ?>
+                                            <a href="battlepass.php?claim_challenge=<?php echo $bpCategoryChallenge['id'] ?>" class="btn btn-primary">(Complete)</a>
+                                        <?php elseif (in_array($bpCategoryChallenge['id'], $challengesClaimed)): ?>
+                                            <span style="color: green">Completed</span>
+                                        <?php endif; ?>
+                                    </center>
                                 </div>
                             </div>
                         <?php endforeach; ?>
@@ -167,18 +165,6 @@ if (isset($_GET['claim_prize']) && (int)$_GET['claim_prize']) {
 
             </table>
         </div>
-
-        <h3>Your Stats</h3>
-        <ul>
-            <li>Points: <?php echo $bpCategoryUser['points'] ?></li>
-            <li>Crimes: <?php echo $bpCategoryUser['crimes'] ?></li>
-            <li>Attacks: <?php echo $bpCategoryUser['attacks'] ?></li>
-            <li>Mugs: <?php echo $bpCategoryUser['mugs'] ?></li>
-            <li>Busts: <?php echo $bpCategoryUser['busts'] ?></li>
-            <li>BA: <?php echo $bpCategoryUser['backalley'] ?></li>
-            <li>Trains: <?php echo $bpCategoryUser['trains'] ?></li>
-        </ul>
-
     </div>
 </div>
 
