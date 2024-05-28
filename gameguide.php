0fa10280bef1f7b7ec5a32f3ce190203bd544858
Aaron Newton <aaron.12.newton@gmail.com>
2024-05-28 15:49:46 +0100

    u

--- gameguide.php
+++ gameguide.php
@@ -12,8 +12,92 @@ include 'header.php';
         </p>
         <a class="btn btn-primary" href="https://discord.gg/Pb7sTfhCnm">Join our Discord</a>
 
+        <hr /><br />
+
         <h2 id="quick-start-guide">Quick Start Guide</h2>
 
+        <p>The first thing you need to know about when playing Chaos City are your account refilables, these consist of:</p>
+        <ul>
+            <li>
+                <strong>Health:</strong> This is how much health you have to be used when attacking
+            </li>
+            <li>
+                <strong>Energy:</strong> Energy is used to train and complete other actions in the game such as attacking. The more energy you have, the more you'll gain from training.
+            </li>
+            <li>
+                <strong>Nerve:</strong> Nerve allows you to commit crimes, the more nerve you have the better the crimes you'll be able to commit, earning you more money and more exp.
+            </li>
+            <li>
+                <strong>Awake:</strong> Awake is effected by which house you have and also impacts the amount you gain when training your stats in the gym.
+            </li>
+        </ul>
+
+        <p>
+            You also have level & EXP. The higher you level the more health, energy and nerve you'll have. You'll also gain access to higher cities with better items and equipment. Your EXP bar shows how close
+            you are to increasing your level.
+        </p>
+
+        <p>For the quick start guide, we'll discuss the core features within the game and how they can help you to start progressing quickly.</p>
+
+        <h3>Crimes</h3>
+
+        <p>
+            Crimes are the a core part of the game and they'll help you gain cash and EXP as well as complete missions. Make sure at the bottom of the crimes page you enable auto refills,
+            will make criming much faster for you. You need to pay for and enable auto refills everyday.
+        </p>
+
+        <h3>Gym</h3>
+
+        <p>
+            The gym is where you can train your stats ready for battle. You have the following stats available to train:
+        </p>
+        <ul>
+            <li>Strength - This is your attack power.</li>
+            <li>Defense - This is for defending against your opponents Strength.</li>
+            <li>Speed - If yours is higher, it gives you the first hit in an attack.</li>
+        </ul>
+
+        <p>It's always smart to hold back on training until you can gain a better house and increase your level to get more from your trains.</p>
+
+        <h3>Backalley</h3>
+
+        <p>The Backalley is a great place to earn some EXP, Cash and Points. It takes energy to search in the Backalley and each search you'll either find something or end up in the hospital. </p>
+        <p>If your going to commit to spending some time searching the Backalley then make sure you have some 100% Med Cert's available as you'll need them! They can be purchased from the Pharmacy on the City page.</p>
+
+        <h3>Missions</h3>
+
+        <p>
+            Missions are the main place you can earn points and are a great source of gaining EXP when you start to become a higher level. Each mission gives you a different task in order for you to gain points and EXP.
+            Missions are available to complete every 24-hours.
+        </p>
+
+        <p>There are 6 different types of missions to complete:</p>
+        <ul>
+            <li>Mission Sets: These are missions which require you to complete multiple different actions to complete them</li>
+            <li>Kill Missions: These require you to complete attacks on other players in order to complete them.</li>
+            <li>Bust Missions: These require you to bust other players or bots out of jail in order to complete them.</li>
+            <li>Crime Missions: These require you to complete crimes. (TIP: When completing your missions, run the 1 nerve Crime on 10x multiple to complete the mission as quickly as possible for the most points profit)</li>
+            <li>Mug Missions: These require you to successfully mug another player to complete them</li>
+            <li>BA Missions: These require you to complete Backalley searches to complete them</li>
+        </ul>
+
+        <h3>Raids</h3>
+
+        <p>Embark on epic raids, where you challenge fearsome bosses in intense battles. As your level grows, so does the might of the adversaries you confront. Every boss presents distinct challenges and promises enticing rewards. Strategize your difficulty selection judiciously and join forces with fellow adventurers to conquer these formidable foes!</p>
+
+        <h3>Battle Pass</h3>
+
+        <p>The Battle Pass allows you to complete challenges to earn prizes. The Battle Pass resets & changes monthly. You have the option of just running the free Battle Pass, or you can update via the store to unlock the months Premium Battle Pass challenges & prizes too.</p>
+
+        <h3>Persistent Tasks</h3>
+
+        <p>There are several tasks in the game which you can do every day/hour/minute. These are:</p>
+        <ul>
+            <li><a href="thecity.php">Search the City - Refills every hour</a></li>
+            <li><a href="maze.php">Maze - Refills 1 search every minute</a></li>
+            <li><a href="psmuggling.php">Points Smuggling - Refills every day</a></li>
+            <li><a href="raidtokensmuggling.php">Raid Token Smuggling - Refills every day</a></li>
+        </ul>
     </div>
 </div>
 
