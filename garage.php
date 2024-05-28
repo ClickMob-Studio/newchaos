4ce4ae3bbb64fd9171c74ee0908fa6c51601173c
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-05-28 20:43:49 +0100

    Update garage.php

--- garage.php
+++ garage.php
@@ -13,4 +13,56 @@ $carsData = $db->fetch_row();
 
 $db->query("SELECT * FROM cities");
 $citiesData = $db->fetch_row();
+
+$carsList = array();
+foreach ($carsData as $car) {
+    $carsList[$car['id']] = $car;
+}
+
+$citiesList = array();
+foreach ($citiesData as $city) {
+    $citiesList[$city['id']] = $city;
+}
+
+if (isset($_POST['sell'])) {
+    if (is_array($_POST['car'])) {
+        $cars = count($_POST['car']);
+        $i = 0;
+        $totalmoney = 0;
+        foreach ($_POST['car'] as $car) {
+            $db->query("SELECT * FROM garage WHERE id=:car");
+            $db->bind(':car', $car);
+            $array = $db->fetch_row(true);
+
+            if ($array['owner'] == $username) {
+                $totalmoney += $array['worth'];
+                $db->query("UPDATE accounts SET money=money+:worth WHERE username=:username");
+                $db->bind(':worth', $array['worth']);
+                $db->bind(':username', $username);
+                $db->execute();
+                
+                $db->query("DELETE FROM garage WHERE id=:car");
+                $db->bind(':car', $car);
+                $db->execute();
+
+                if (($i + 1) == $cars) {
+                    if ($cars == 1) {
+                        echo "You sold the car for &pound;" . number_format($array['worth']) . "";
+                    } else {
+                        echo "You sold $cars cars for &pound;" . number_format($totalmoney) . ".";
+                    }
+                }
+            } else {
+                if (($i + 1) == $cars) {
+                    if ($cars == 1) {
+                        echo "You do not own this car.\n";
+                    } else {
+                        echo "You do not own these cars.\n";
+                    }
+                }
+            }
+            $i++;
+        }
+    }
+}
 exit;
