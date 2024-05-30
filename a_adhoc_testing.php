387039121f386fed8131c86b7d3d552e089da2ef
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-05-30 21:44:48 +0100

    u

--- a_adhoc_testing.php
+++ a_adhoc_testing.php
@@ -5,18 +5,38 @@ if ($_GET['key'] === 'srunit') {
     $now = new \DateTime();
     $hour = $now->format('H');
 
-    echo $hour; exit;
-    
-    $db->query("SELECT * FROM grpgusers WHERE is_auto_user = 1");
-    $db->execute();
-    $rows = $db->fetch_row();
+    // Only run between 8am and 10pm to make it look more legit
+    if ($hour > 8 && $hour < 22) {
+        $db->query("SELECT * FROM grpgusers WHERE is_auto_user = 1");
+        $db->execute();
+        $rows = $db->fetch_row();
 
-    foreach ($rows as $r) {
-        $user = new User($r['id']);
+        foreach ($rows as $r) {
+            $user = new User($r['id']);
+
+            $db->query("SELECT * FROM missions WHERE userid= " . $user_class->id . " AND completed='no' LIMIT 1");
+            $db->execute();
+            $check = $db->fetch_row();
+
+            if (isset($check[0]['id'])) {
+                // Run with active mission
+                $activeMission = $check[0]['id'];
+
+                $db->query("SELECT category FROM mission WHERE id = " . $activeMission['mid'] . " LIMIT 1");
+                $db->execute();
+                $mCategory = $db->fetch_single();
+
+                echo $mCategory; exit;
 
 
+            }
+
+            // Check whether to start an active mission
+
+
+
+        }
     }
-
 }
 
 
