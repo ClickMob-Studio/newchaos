6f94dc6c15637dc27afe81d76a93517d0314a2c1
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-05-30 21:13:50 +0100

    up

--- battlepass.php
+++ battlepass.php
@@ -4,16 +4,7 @@ include 'header.php';
 
 $now = new \DateTime();
 
-
-if ($user_class->admin > 0) {
-    $db->query("SELECT * FROM bp_category WHERE month_year = '06-2024' LIMIT 1");
-    $db->execute();
-    $r = $db->fetch_row();
-    $bpCategory = $r[0];
-
-} else {
-    $bpCategory = getBpCategory();
-}
+$bpCategory = getBpCategory();
 $bpCategoryPrizes = getBpCategoryPrizes($bpCategory);
 $bpCategoryChallenges = getBpCategoryChallenges($bpCategory);
 $bpCategoryUser = getBpCategoryUser($bpCategory, $user_class);
