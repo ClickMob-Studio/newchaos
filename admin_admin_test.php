78d49c927b516d72e111144811d71e27dcc4823d
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-06-02 22:26:19 +0100

    Update admin_admin_test.php

--- admin_admin_test.php
+++ admin_admin_test.php
@@ -1,8 +1,21 @@
 <?php
 require "ajax_header.php";
+
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+
+$headers = apache_request_headers();
+if (isset($headers['Authorization'])) {
+    $session_id = str_replace('Bearer ', '', $headers['Authorization']);
+    session_id($session_id);
+}
+
+session_start();
+
+file_put_contents('php://stderr', print_r($_POST, TRUE));
+file_put_contents('php://stderr', print_r($_SESSION, TRUE)); 
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $data = json_decode(file_get_contents("php://input"), true);
@@ -15,7 +28,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
     $user_class = new User($_SESSION['id']);
-
 
     echo json_encode(["success" => true, "user" => $user_class]);
 } else {
