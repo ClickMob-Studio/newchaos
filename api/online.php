a700b6bfa55b2c590c4ba93ccc528ecdf271d90a
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-06-05 19:35:14 +0100

    Create online.php

---
+++ api/online.php
@@ -0,0 +1,31 @@
+<?php
+include "../database/pdo_class.php";
+include "../classes.php";
+include "../codeparser.php";
+include "../includes/functions.php";
+
+header('Content-Type: application/json');
+
+$db->query("SELECT id FROM grpgusers WHERE lastactive > UNIX_TIMESTAMP() - 3600 ORDER BY lastactive DESC");
+$rows = $db->fetch_row();
+
+$store = [];
+foreach ($rows as $row) {
+    $user_online = new User($row['id']);
+    $store[] = array(
+        'avatar' => $user_online->avatar,
+        'formattedname' => $user_online->formattedname,
+        'level' => $user_online->level,
+        'money' => $user_online->money,
+        'id' => $user_online->id,
+        'formattedgang' => $user_online->formattedgang,
+        'type' => $user_online->type,
+        'cityname' => $user_online->cityname,
+        'cityid' => $user_online->city,
+        'hospital' => $user_online->hospital,
+        'jail' => $user_online->jail,
+        'lastactive' => howlongago($user_online->lastactive)
+    );
+}
+
+echo json_encode(['users_online' => $store]);
