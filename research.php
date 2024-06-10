5fbe4ba636f3c48835234ccba67d4c24f7a9ffab
Aaron Newton <aaron.12.newton@gmail.com>
2024-06-10 12:55:50 +0100

    u

---
+++ research.php
@@ -0,0 +1,133 @@
+<?php
+include 'header.php';
+?>
+
+<div class='box_top'>Research</div>
+<div class='box_middle'>
+    <!-- Combat Research -->
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Combat Research</h2>
+
+            <div class="table-container">
+                <table class="new_table" id="newtables" style="width:100%;">
+                    <tr>
+                        <!-- LEVEL 1 -->
+                        <td>
+                            <div class="card text-white bg-info mb-3">
+                                <div class="card-header">
+                                    Research 1
+                                </div>
+                                <div class="card-body">
+                                    <p class="card-text">
+                                        Some information here about what your researching
+                                    </p>
+                                </div>
+                                <div class="card-footer">
+                                    <a href="#" class="btn btn-primary">Research</a>
+                                </div>
+                            </div>
+                        </td>
+
+                        <!-- LEVEL 2 -->
+                        <td>
+                            <div class="card text-white bg-info mb-3">
+                                <div class="card-header">
+                                    Research 2
+                                </div>
+                                <div class="card-body">
+                                    <p class="card-text">
+                                        Some information here about what your researching
+                                    </p>
+                                </div>
+                                <div class="card-footer">
+                                    <a href="#" class="btn btn-primary">Research</a>
+                                </div>
+                            </div>
+
+                            <br /><br />
+
+                            <div class="card text-white bg-info mb-3">
+                                <div class="card-header">
+                                    Research 2
+                                </div>
+                                <div class="card-body">
+                                    <p class="card-text">
+                                        Some information here about what your researching
+                                    </p>
+                                </div>
+                                <div class="card-footer">
+                                    <a href="#" class="btn btn-primary">Research</a>
+                                </div>
+                            </div>
+
+                        </td>
+
+                        <!-- LEVEL 3 -->
+                        <td>
+                            <div class="card text-white bg-info mb-3">
+                                <div class="card-header">
+                                    Research 2
+                                </div>
+                                <div class="card-body">
+                                    <p class="card-text">
+                                        Some information here about what your researching
+                                    </p>
+                                </div>
+                                <div class="card-footer">
+                                    <a href="#" class="btn btn-primary">Research</a>
+                                </div>
+                            </div>
+
+                            <br /><br />
+
+                            <div class="card text-white bg-info mb-3">
+                                <div class="card-header">
+                                    Research 2
+                                </div>
+                                <div class="card-body">
+                                    <p class="card-text">
+                                        Some information here about what your researching
+                                    </p>
+                                </div>
+                                <div class="card-footer">
+                                    <a href="#" class="btn btn-primary">Research</a>
+                                </div>
+                            </div>
+
+                            <br /><br />
+
+                            <div class="card text-white bg-info mb-3">
+                                <div class="card-header">
+                                    Research 2
+                                </div>
+                                <div class="card-body">
+                                    <p class="card-text">
+                                        Some information here about what your researching
+                                    </p>
+                                </div>
+                                <div class="card-footer">
+                                    <a href="#" class="btn btn-primary">Research</a>
+                                </div>
+                            </div>
+
+                        </td>
+                    </tr>
+
+
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <!-- Economic Research -->
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Economic Research</h2>
+        </div>
+    </div>
+</div>
+
+<?php
+include 'footer.php'
+?>
