940fe28efbbedd1d7d41029bb7f496892373351c
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-06-11 13:21:54 +0100

    Update Events.php

--- api/Events.php
+++ api/Events.php
@@ -2,7 +2,11 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
-
+// Handle preflight requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 include "../database/pdo_class.php";
 
 function handleEventsRequest($method, $path_info)
