2387831d132d4a9c5da0557b11c3bd07e073a9f0
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-06-11 12:31:48 +0100

    u

--- auto_run_users.php
+++ auto_run_users.php
@@ -58,7 +58,7 @@ if ($_GET['key'] === 'srunit') {
                                 curl_setopt ($ch, CURLOPT_FAILONERROR, 1);
                                 curl_setopt($ch, CURLOPT_POST, 1);
                                 curl_setopt($ch, CURLOPT_POSTFIELDS,
-                                    "id=1&cm=20");
+                                    "id=1&cm=30");
                                 $dinf = curl_exec ($ch);
                                 if(!curl_errno($ch) ){
 
