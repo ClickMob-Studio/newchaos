9f9c624f6610d523088f84a6e95d741bb8f05a2e
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-06-14 19:59:40 +0100

    Update poker_index.php

--- poker_index.php
+++ poker_index.php
@@ -7,7 +7,9 @@ if($pagename!="poker"){
 }else{
 	session_start();
 }
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 if (! in_array($pagename, array('approve', 'logout')))
 	require('includes/gen_inc.php');
 $frontpage = "frontpages/{$pagename}.php";
