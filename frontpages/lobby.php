2f24134fe0e78f2e755894d398e9738bae20960e
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-06-14 11:26:43 +0100

    a

---
+++ frontpages/lobby.php
@@ -0,0 +1,44 @@
+<?php 
+require('includes/inc_lobby.php');
+
+$addons->get_hooks(array(), array(
+    'page'     => 'lobby.php',
+    'location'  => 'page_start'
+));
+
+include 'templates/header.php';
+
+echo $addons->get_hooks(array(), array(
+    'page'     => 'lobby.php',
+    'location'  => 'html_start'
+));
+
+$addons->get_hooks(array(), array(
+    'page'     => 'lobby.php',
+    'location'  => 'tabs_left'
+));
+
+
+$opsTheme->addVariable('tab', array(
+	'id'   => 'tables',
+	'name' => 'All Tables',
+	'html' => $opsTheme->viewPart('lobby-gamelist-tabpanel')
+));
+
+$tabpanelHtml .= $opsTheme->viewPart('lobby-tabpanel');
+
+$opsTheme->addVariable('lobby', array(
+	'tabs'      => $navHtml,
+	'tabpanels' => $tabpanelHtml
+));
+
+echo $opsTheme->viewPage('lobby');
+
+echo $addons->get_hooks(array(), array(
+    'page'     => 'lobby.php',
+    'location'  => 'html_end'
+));
+
+include 'templates/footer.php';
+
+?>
---
+++ poker_index.php
@@ -0,0 +1,34 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$pagename = (isset($_GET['pagename'])) ? $_GET['pagename'] : 'index';
+
+if($pagename!="poker"){
+	require_once 'header.php';
+}else{
+	session_start();
+}
+
+if (! in_array($pagename, array('approve', 'logout')))
+	require('includes/gen_inc.php');
+
+$frontpage = "frontpages/{$pagename}.php";
+
+if (file_exists($frontpage)){
+	include $frontpage;
+}else{
+	echo $addons->get_hooks(
+		array(
+			'page' => $pagename
+		),
+		array(
+			'page'     => 'general',
+			'location'  => 'frontpage'
+		)
+	);
+}
+
+if($pagename!="poker"){
+	require_once 'footer.php';
+}
