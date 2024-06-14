8fa5f2c492a0261af58e8d1ae0669fa2a571c023
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-06-14 12:30:34 +0100

    Update poker_index.php

--- poker_index.php
+++ poker_index.php
@@ -15,21 +15,21 @@ if($pagename!="poker"){
 if (! in_array($pagename, array('approve', 'logout')))
 	require('includes/gen_inc.php');
 
-$frontpage = "frontpages/{$pagename}.php";
+//$frontpage = "frontpages/{$pagename}.php";
 
-// if (file_exists($frontpage)){
-// 	include $frontpage;
-// }else{
-// 	echo $addons->get_hooks(
-// 		array(
-// 			'page' => $pagename
-// 		),
-// 		array(
-// 			'page'     => 'general',
-// 			'location'  => 'frontpage'
-// 		)
-// 	);
-// }
+if (file_exists($frontpage)){
+	include $frontpage;
+}else{
+	echo $addons->get_hooks(
+		array(
+			'page' => $pagename
+		),
+		array(
+			'page'     => 'general',
+			'location'  => 'frontpage'
+		)
+	);
+}
 
 if($pagename!="poker"){
 	require_once 'footer.php';
