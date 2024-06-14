d48b07b840cd49d9901210f733dcd2c14a4d6884
Aaron Newton <aaron.12.newton@gmail.com>
2024-06-14 09:53:07 +0100

    u

--- a_adhoc_testing.php
+++ a_adhoc_testing.php
@@ -9,5 +9,7 @@ $activityContestTypes = array(
 );
 $typeToUse = $activityContestTypes[mt_rand(0, count($activityContestTypes) - 1)];
 
+$sql = "UPDATE `activity_contest` SET `type` = " . $typeToUse;
+echo $sql;
 mysql_query("UPDATE `activity_contest` SET `type` = " . $typeToUse);
 ?>
