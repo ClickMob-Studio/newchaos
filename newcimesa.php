a36ca1ce3345d83a9e5236e5314f7ec18fa8b8ab
Aaron Newton <aaron.12.newton@gmail.com>
2024-06-18 09:17:38 +0100

    u

--- newcimesa.php
+++ newcimesa.php
@@ -1,6 +1,6 @@
 <?php
 include 'header.php';
-if($user_class->id != 1){
+if($user_class->admin < 1){
     exit();
 }
 $db->query("UPDATE grpgusers SET crimes = 'newcrimes', lastactive = unix_timestamp() WHERE id = ?");
