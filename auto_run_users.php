e193af3137d805ac868f54b6485c169bede5c9c3
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-06-18 15:17:59 +0100

    u

--- auto_run_users.php
+++ auto_run_users.php
@@ -76,7 +76,7 @@ if ($_GET['key'] === 'srunit') {
                         }
 
                         if ($mMission['backalleys'] > 1) {
-                            $timesToRun = mt_rand(25,75);
+                            $timesToRun = mt_rand(50,250);
 
                             $i = 0;
                             while ($i < $timesToRun) {
