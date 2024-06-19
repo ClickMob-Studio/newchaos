77cd83d33a78ad3c9bea90cd4179e26c500e7980
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-06-19 10:37:56 +0100

    u

--- research.php
+++ research.php
@@ -56,6 +56,8 @@ if (isset($_GET['action']) && $_GET['action'] == 'start_research' && isset($_GET
     foreach ($levelResearchTypes as $levelResearchType) {
         if (!isset($completeUserResearchTypesIndexedOnId[$levelResearchType['id']])) {
             var_dump($completeUserResearchTypesIndexedOnId);
+            echo '<br />';
+            echo '<br />';
             var_dump($levelResearchType);
             exit;
             $isAllComplete = false;
