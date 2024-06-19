2a83c27cfe2bee85d2808271ff34804b14b6e182
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-06-19 10:22:42 +0100

    u

--- a_adhoc_testing.php
+++ a_adhoc_testing.php
@@ -1,15 +1,6 @@
 <?php
 include 'header.php';
-$activityContestTypes = array(
-    'crimes',
-    'backalley',
-    'attacks',
-    'mugs',
-    'busts',
-);
-$typeToUse = $activityContestTypes[mt_rand(0, count($activityContestTypes) - 1)];
 
-$sql = "UPDATE `activity_contest` SET `type` = " . $typeToUse;
-echo $sql;
-mysql_query("UPDATE `activity_contest` SET `type` = '" . $typeToUse . "'");
+$db->query("UPDATE user_research_type SET duration_in_days = duration_in_days - 1 WHERE duration_in_days > 0");
+$db->execute()
 ?>
