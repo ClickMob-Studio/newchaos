041eb9e1448755deb8e2049d31c20ea29137a2ad
Aaron Newton <aaron.12.newton@gmail.com>
2024-06-25 10:32:28 +0100

    u

--- claim_mystery_box.php
+++ claim_mystery_box.php
@@ -11,19 +11,19 @@ macroTokenCheck($user_class);
 
 
 if ($user_class->box_hunt_count < 2) {
-    Give_Item(256, $user_class->id, 1);
+    Give_Item(276, $user_class->id, 1);
     mysql_query("UPDATE grpgusers SET box_hunt_count = box_hunt_count + 1 WHERE id = " . $user_class->id);
 
     echo "
     <div class='alert alert-info'>
-        <p>Your Nerve Vial has been added to your inventory!</p>
+        <p>Your Research Token has been added to your inventory!</p>
     </div>
     ";
 
 } else {
     echo "
     <div class='alert alert-danger'>
-        <p>You have claimed all of your Nerve Vial!</p>
+        <p>You have claimed all of your Research Tokens!</p>
     </div>
     ";
 
