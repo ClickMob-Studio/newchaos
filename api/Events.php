e5e6d55ddcde609cc492938b057c95e6a4d58a60
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-06-26 21:28:40 +0100

    Update Events.php

--- api/Events.php
+++ api/Events.php
@@ -82,7 +82,7 @@ function getEvents($db)
 
 function replaceUserIdWithUsername($db, $text, $userId)
 {
-    return generateFormattedName($userId);
+    return str_replace('[-_USERID_-]', generateFormattedName($userId), $text);
 }
 
 function generateFormattedName($id, $nogang = 0)
@@ -146,35 +146,12 @@ function generateFormattedName($id, $nogang = 0)
         $name .= "<a title='" . $title . " [" . $row['username'] . "]' href='profiles.php?id=" . $id . "'>";
         $name .= "<img src='{$row['image_name']}' style='max-width:84px; max-height:50px;' title='" . $row['username'] . "' />";
         $name .= "</a>";
-    } elseif ($row['gndays']) {
-        $name .= "<a href='profiles.php?id=" . $id . "'>" . nameGen($row['gndays'], $row['rmdays'], $row['uninfo'], $row['username']) . "</a>";
-    } elseif (!empty($row['colours']) && $row['gradient'] == 2 && $row['gndays']) {
-        $row['colours'] = str_replace('#', '', $row['colours']);
-        $colours = explode("~", $row['colours']);
-        $gradient = text_gradient($colours[0], $colours[1], 1, $row['username']);
-        $name .= "<b><i><a title='$title' href='profiles.php?id=$id'>$gradient</a></i></b>";
-    } elseif (!empty($row['colours']) && $row['gradient'] == 3 && $row['gndays']) {
-        $row['colours'] = str_replace('#', '', $row['colours']);
-        $gn = explode("~", $row['colours']);
-        $username = $row['username'];
-        $half = (int)((strlen($username) / 2));
-        $left = substr($username, 0, $half);
-        $right = substr($username, $half);
-        $gradient = text_gradient($gn[0], $gn[1], 1, $left);
-        $gradient .= text_gradient($gn[1], $gn[2], 1, $right);
-        $name .= "<b><i><a title='$title' href='profiles.php?id=$id'>$gradient</a></i></b>";
-    } elseif ($id == 146) {
-        $name .= "<a title='$title' href='profiles.php?id=$id'>{$row['username']}</a>";
-    } elseif ($row['admin'] == 1 || $row['gm'] == 1) {
-        $name .= "<i><b><a title='$title' href='profiles.php?id=$id'><font color='$whichfont'>{$row['username']}</font></a></b></i>";
-    } elseif ($row['rmdays'] > 0) {
-        $name .= "<b><a title='$title' href='profiles.php?id=$id'><font color='$whichfont'>{$row['username']}</font></a></b>";
     } else {
         $name .= "<a title='$title' href='profiles.php?id=$id'><font color='$whichfont'>{$row['username']}</font></a>";
     }
 
     if ($row['prestige'] > 0) {
-        $name .= " <img src='images/skullpres_" . $row['prestige'] . ".png' title='Prestige ({$row['prestige']})' />";
+        $name .= " <img src='https://chaoscity.co.uk/images/skullpres_" . $row['prestige'] . ".png' title='Prestige ({$row['prestige']})' />";
     }
 
     if ($nogang == 0) {
@@ -183,10 +160,6 @@ function generateFormattedName($id, $nogang = 0)
 
     return $name;
 }
-
-
-
-
 
 // Routing logic
 $method = $_SERVER['REQUEST_METHOD'];
