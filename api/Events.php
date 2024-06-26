ccc611f23356faa8f9e9cf47d9acfc3dcfbfbe0a
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-06-26 21:02:28 +0100

    a

--- api/Events.php
+++ api/Events.php
@@ -27,25 +27,7 @@ function handleEventsRequest($method, $path_info)
 
     switch ($method) {
         case 'POST':
-            $input = json_decode(file_get_contents('php://input'), true);
-            if (isset($input['action'])) {
-                $action = $input['action'];
-                if ($action == 'get') {
-                    getEvents($db, $input);
-                } elseif ($action == 'deleteAll') {
-                    deleteAllEvents($db, $input);
-                } elseif ($action == 'deleteByType') {
-                    deleteEventsByType($db, $input);
-                } elseif ($action == 'deleteById') {
-                    deleteEventById($db, $input);
-                } else {
-                    http_response_code(400);
-                    echo json_encode(['message' => 'Invalid Request']);
-                }
-            } else {
-                http_response_code(400);
-                echo json_encode(['message' => 'Action not specified']);
-            }
+            getEvents($db);
             break;
         default:
             http_response_code(405);
@@ -54,9 +36,10 @@ function handleEventsRequest($method, $path_info)
     }
 }
 
-function getEvents($db, $input)
+function getEvents($db)
 {
-    $user_id = $input['user_id'];
+    $input = json_decode(file_get_contents('php://input'), true);
+    $user_id = isset($input['userId']) ? $input['userId'] : null;
     $search = isset($input['search']) ? '%' . $input['search'] . '%' : null;
     $page = isset($input['page']) ? (int)$input['page'] : 1;
     $rowsperpage = 30;
@@ -96,15 +79,36 @@ function getEvents($db, $input)
 
     echo json_encode($events);
 }
-function replaceUserIdWithUsername($db, $text, $userId) {
-    global $m;
+
+function replaceUserIdWithUsername($db, $text, $userId)
+{
+    return generateFormattedName($userId);
+}
+
+function generateFormattedName($id, $nogang = 0)
+{
+    global $db, $m;
     $name = "";
+
+    if ($nogang == 0 && $id != 864 and !empty($rtn = $m->get('generateFormattedName.' . $id)))
+        return $rtn;
+
     $db->query("SELECT username, gang, admin, rmdays, gm, colours, image_name, pdimgname, gradient, gndays, leader, g.tag, formattedTag, prestige, uninfo FROM grpgusers gu LEFT JOIN gangs g ON g.id = gu.gang WHERE gu.id = ?");
-    $db->execute(array($userId));
+    $db->execute(array($id));
     $row = $db->fetch_row(true);
 
+    // Gang logic
+    if ($row['gang'] != 0 && $nogang != 1) {
+        if ($row['formattedTag'] == "Yes") {
+            $name .= "[" . ($row['leader'] == $id ? "[" . gradientTag($row['gang']) . "]" : gradientTag($row['gang'])) . "] ";
+        } else {
+            $name .= "[" . ($row['leader'] == $id ? "<b>{$row['tag']}</b>" : "{$row['tag']}") . "] ";
+        }
+    }
+
+    // Determine title and font color based on user status
     $db->query("SELECT days FROM bans WHERE id = ? AND type IN ('perm','freeze')");
-    $db->execute(array($userId));
+    $db->execute(array($id));
     $bdays = $db->fetch_single();
 
     if ($bdays) {
@@ -124,109 +128,18 @@ function replaceUserIdWithUsername($db, $text, $userId) {
         $whichfont = "#009102";
     }
 
-    $usernameElement = "<span style='color: $whichfont; display: inline-block;'>{$row['username']}</span>";
-
-    $name .= "<div class='d-flex align-items-center flex-wrap' style='gap: 5px;'>";
-
-    if ($row['gang'] != 0) {
-        $name .= "<span class='text-gray' style='display: inline-block;'>[<b>{$row['tag']}</b>]</span>";
-    }
-
-    if ($bdays) {
-        $name .= $usernameElement;
-    } elseif (!empty($row['image_name']) && $row['pdimgname'] > 0) {
-        $name .= "<img src='{$row['image_name']}' class='img-fluid' style='max-width:84px; max-height:50px; display: inline-block; vertical-align: middle;' title='{$row['username']}' />";
-    } elseif ($row['gndays']) {
-        $name .= "<span style='color: $whichfont; display: inline-block;'>" . nameGen($row['gndays'], $row['rmdays'], $row['uninfo'], $row['username']) . "</span>";
-    } elseif (!empty($row['colours']) && $row['gradient'] == 2 && $row['gndays']) {
-        $row['colours'] = str_replace('#', '', $row['colours']);
-        $colours = explode("~", $row['colours']);
-        $gradient = text_gradient($colours[0], $colours[1], 1, $row['username']);
-        $name .= "<span style='color: $whichfont; display: inline-block;'><b><i>{$gradient}</i></b></span>";
-    } elseif (!empty($row['colours']) && $row['gradient'] == 3 && $row['gndays']) {
-        $row['colours'] = str_replace('#', '', $row['colours']);
-        $gn = explode("~", $row['colours']);
-        $username = $row['username'];
-        $half = (int) ((strlen($username) / 2));
-        $left = substr($username, 0, $half);
-        $right = substr($username, $half);
-        $gradient = text_gradient($gn[0], $gn[1], 1, $left);
-        $gradient .= text_gradient($gn[1], $gn[2], 1, $right);
-        if ($userId == 146) $gradient = "<span style='text-shadow: 0 0 2px #404200;letter-spacing:-1px;font-weight:900;font-size:16px;'>$gradient</span>";
-        $name .= "<span style='color: $whichfont; display: inline-block;'><b><i>{$gradient}</i></b></span>";
-    } elseif ($userId == 146) {
-        $name .= $usernameElement;
-    } elseif ($row['admin'] == 1 || $row['gm'] == 1) {
-        $name .= "<span style='color: $whichfont; display: inline-block;'><i><b>{$row['username']}</b></i></span>";
-    } elseif ($row['rmdays'] > 0) {
-        $name .= "<span style='color: $whichfont; display: inline-block;'><b>{$row['username']}</b></span>";
-    } else {
-        $name .= $usernameElement;
-    }
+    // User name with image and prestige image
+    $name .= $row['username'];
 
     if ($row['prestige'] > 0) {
-        if ($row['prestige'] >= 10) {
-            $db->query("SELECT skull FROM prestige_skull WHERE `user_id` = ?");
-            $db->execute(array($userId));
-            $skull = $db->fetch_single();
-            if ($skull !== false) {
-                $name .= " <img src='https://chaoscity.co.uk/images/skullpres_" . $skull . ".png' class='img-fluid' style='display: inline-block; vertical-align: middle;' title='Prestige ({$row['prestige']})' />";
-            } else {
-                $name .= " <img src='https://chaoscity.co.uk/images/skullpres_" . $row['prestige'] . ".png' class='img-fluid' style='display: inline-block; vertical-align: middle;' title='Prestige ({$row['prestige']})' />";
-            }
-        } else {
-            $name .= " <img src='https://chaoscity.co.uk/images/skullpres_" . $row['prestige'] . ".png' class='img-fluid' style='display: inline-block; vertical-align: middle;' title='Prestige ({$row['prestige']})' />";
-        }
+        $name .= " <img src='images/skullpres_" . $row['prestige'] . ".png' title='Prestige ({$row['prestige']})' />";
     }
 
-    $name .= "</div>";
-
-    $m->set('formatName.' . $userId, $name, false, 60);
-    return str_replace('[-_USERID_-]', $name, $text);
-}
-
-function deleteAllEvents($db, $input)
-{
-    $user_id = $input['user_id'];
-    $db->query("DELETE FROM events WHERE `to` = :user_id");
-    $db->bind(':user_id', $user_id, PDO::PARAM_INT);
-    $db->execute();
-    echo json_encode(['message' => 'All events deleted']);
-}
-
-function deleteEventsByType($db, $input)
-{
-    $user_id = $input['user_id'];
-    $type1 = $input['type1'];
-    $type2 = isset($input['type2']) ? $input['type2'] : null;
-    
-    $query = "DELETE FROM events WHERE `to` = :user_id AND (`text` LIKE :type1";
-    if ($type2) {
-        $query .= " OR `text` LIKE :type2";
+    if ($nogang == 0) {
+        $m->set('generateFormattedName.' . $id, $name, false, 60);
     }
-    $query .= ")";
-    
-    $db->query($query);
-    $type1 = '%' . $type1 . '%';
-    $db->bind(':user_id', $user_id, PDO::PARAM_INT);
-    $db->bind(':type1', $type1);
-    if ($type2) {
-        $type2 = '%' . $type2 . '%';
-        $db->bind(':type2', $type2);
-    }
-    $db->execute();
-    echo json_encode(['message' => 'Events deleted']);
-}
 
-function deleteEventById($db, $input)
-{
-    $user_id = $input['user_id'];
-    $userId = $input['id'];
-    $db->query("DELETE FROM events WHERE id = :id AND `to` = :user_id");
-    $db->bind(':id', $userId, PDO::PARAM_INT);
-    $db->bind(':user_id', $user_id, PDO::PARAM_INT);
-    $db->execute();
-    echo json_encode(['message' => 'Event deleted']);
+    return $name;
 }
 
 // Routing logic
