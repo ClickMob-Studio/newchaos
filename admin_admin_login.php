905d06acfadfb92ad3101f0dfb5aae82b6a51cbc
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-06-26 13:01:48 +0100

    Update admin_admin_login.php

--- admin_admin_login.php
+++ admin_admin_login.php
@@ -4,14 +4,10 @@ header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type");
 
-
-// Custom log file for debugging
-$log_file = '/home/chaoscit/api_error.log'; // Update this path as needed
-
 session_start();
-file_put_contents($log_file, "Session ID: " . session_id() . "\n", FILE_APPEND);
 
 require 'database/pdo_class.php';
+
 $data = json_decode(file_get_contents("php://input"), true);
 $username = $data['username'];
 $password = $data['password'];
@@ -29,11 +25,8 @@ if ($result) {
     $_SESSION['username'] = $result['username'];
     $_SESSION['loggedin'] = true;
 
-    file_put_contents($log_file, "Login successful. Session data: " . print_r($_SESSION, true) . "\n", FILE_APPEND);
-
     echo json_encode(["success" => true, "user" => $result, "token" => session_id()]);
 } else {
-    file_put_contents($log_file, "Invalid credentials\n", FILE_APPEND);
     echo json_encode(["success" => false, "message" => "Invalid credentials"]);
 }
 ?>
