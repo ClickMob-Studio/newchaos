e4856f23a384f751c0b23a095836c01492c4114b
Aaron Newton <aaron.12.newton@gmail.com>
2024-07-04 11:03:49 +0100

    u

--- admin_panel.php
+++ admin_panel.php
@@ -17,6 +17,7 @@ if ($user_class->admin < 1) {
     <li><a href="admin_gang_vault_logs.php">Gang Vault Logs</a></li>
     <li><a href="admin_packs.php">Pack Logs</a></li>
     <li><a href="admin_pmlogs.php">PM Logs</a></li>
+    <li><a href="admin_eco.php">Economy Logs</a></li>
 </ul>
 
 <?php
