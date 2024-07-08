68e6e1e92c2c1307270e7963aa2e7f5553de2dad
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-07-08 22:15:19 +0100

    u

--- inventory.php
+++ inventory.php
@@ -25,7 +25,9 @@ if ($user_class->gang > 0) {
     if ($tempItemUse['gang_double_exp_hours'] > 0 && $tempItemUse['gang_double_exp_time'] < $now) {
         echo '
             <hr />
-            <a href="trigger_doublexp_hour.php" onclick="return confirm(\'Are you sure you want to trigger double EXP?\');"><font color=red>You have ' . $tempItemUse['gang_double_exp_hours'] . ' hours of double EXP! Click to run 1 hour of double exp.</font></a>
+            <center>
+             <a href="trigger_doublexp_hour.php" onclick="return confirm(\'Are you sure you want to trigger double EXP?\');"><font color=red>You have ' . $tempItemUse['gang_double_exp_hours'] . ' hours of double EXP! Click to run 1 hour of double exp.</font></a>
+            </center>
             <hr />
         ';
     }
