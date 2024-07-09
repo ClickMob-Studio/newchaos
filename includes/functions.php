95ae68e6cd9833c007d192ed308bd623139ce3d2
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-07-09 21:02:47 +0100

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -1841,28 +1841,7 @@ function Get_Item_name($id){
         return 'Unknown Item';
     }
 }
-   global $user_class;
-    if ($user_class->gang != 0) {
-        $checkActiveMission = mysql_query("SELECT agm.kills, agm.busts, agm.crimes, agm.mugs, gm.name, gm.kills AS target_kills, gm.busts AS target_busts, gm.crimes AS target_crimes, gm.mugs AS target_mugs, gm.reward, gm.time AS 'mission_time', UNIX_TIMESTAMP() AS 'current_time', agm.end_time FROM active_gang_missions agm JOIN gang_missions gm ON agm.mission_id = gm.id WHERE agm.gangid = {$user_class->gang} AND agm.completed = 0 LIMIT 1");
-        if (!$checkActiveMission) {
-            die('Invalid query: ' . mysql_error());
-        }
 
-        if ($activeMission = mysql_fetch_assoc($checkActiveMission)) {
-            // Sanitize the field name to prevent SQL injection
-            $allowed_fields = ['kills', 'busts', 'crimes', 'mugs'];
-            if (!in_array($field, $allowed_fields)) {
-                die('Invalid field specified.');
-            }
-
-            // Update the field in the active mission
-            $updateQuery = "UPDATE active_gang_missions SET {$field} = {$field} + {$value} WHERE gangid = '{$user_class->gang}' AND completed = 0 LIMIT 1";
-            if (!mysql_query($updateQuery)) {
-                die('Failed to update the mission: ' . mysql_error());
-            }
-        }
-    }
-}
 
 function generateMacroToken($length = 10) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
