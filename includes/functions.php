24fbad7c360dd533c547ca1292b592448290433b
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-07-09 21:02:27 +0100

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -1841,19 +1841,20 @@ function Get_Item_name($id){
         return 'Unknown Item';
     }
 }
-// Function to check if the user is in a gang with a current mission and update the active mission
-function updateGangActiveMission( $field, $value) {
-    global $db, $user_class;
-    // Check if the user is in a gang
+   global $user_class;
     if ($user_class->gang != 0) {
-        // Check for an active mission
-        $checkActiveMission = mysql_query("SELECT agm.kills, agm.busts, agm.crimes, agm.mugs, gm.name, gm.kills AS target_kills, gm.busts AS target_busts, gm.crimes AS target_crimes, gm.mugs AS target_mugs, gm.reward, gm.time AS 'mission_time', UNIX_TIMESTAMP() AS 'current_time', agm.end_time FROM active_gang_missions agm JOIN gang_missions gm ON agm.mission_id = gm.id WHERE agm.gangid = '{$user_class->gang}' AND agm.completed = 0 LIMIT 1");
+        $checkActiveMission = mysql_query("SELECT agm.kills, agm.busts, agm.crimes, agm.mugs, gm.name, gm.kills AS target_kills, gm.busts AS target_busts, gm.crimes AS target_crimes, gm.mugs AS target_mugs, gm.reward, gm.time AS 'mission_time', UNIX_TIMESTAMP() AS 'current_time', agm.end_time FROM active_gang_missions agm JOIN gang_missions gm ON agm.mission_id = gm.id WHERE agm.gangid = {$user_class->gang} AND agm.completed = 0 LIMIT 1");
         if (!$checkActiveMission) {
             die('Invalid query: ' . mysql_error());
         }
 
-        // If there's an active mission, update the specified field
         if ($activeMission = mysql_fetch_assoc($checkActiveMission)) {
+            // Sanitize the field name to prevent SQL injection
+            $allowed_fields = ['kills', 'busts', 'crimes', 'mugs'];
+            if (!in_array($field, $allowed_fields)) {
+                die('Invalid field specified.');
+            }
+
             // Update the field in the active mission
             $updateQuery = "UPDATE active_gang_missions SET {$field} = {$field} + {$value} WHERE gangid = '{$user_class->gang}' AND completed = 0 LIMIT 1";
             if (!mysql_query($updateQuery)) {
@@ -1862,6 +1863,7 @@ function updateGangActiveMission( $field, $value) {
         }
     }
 }
+
 function generateMacroToken($length = 10) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $randomString = '';
