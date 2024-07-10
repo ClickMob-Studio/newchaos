b8750c8bba4413ebcf89b7aba3061d37d3e8d127
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-07-10 11:37:29 +0100

    u

--- admin_gang_territory_battles.php
+++ admin_gang_territory_battles.php
@@ -4,7 +4,7 @@ if($user_class->admin < 1) {
     exit();
 }
 
-$db->query("SELECT * FROM gang_territory_zone_battle WHERE is_complete = 1 ORDER BY `time_started` DESC");
+$db->query("SELECT * FROM gang_territory_zone_battle WHERE is_complete = 1 ORDER BY `time_started` DESC LIMIT 10");
 $db->execute();
 $fetch = $db->fetch_row();
 
@@ -19,9 +19,14 @@ $fetch = $db->fetch_row();
     </thead>
     <tbody>
     <?php foreach ($fetch as $row):?>
+        <?php
+        $attackingGang = new Gang($row['attacking_gang_id']);
+        $defendingGang = new Gang($row['defending_gang_id']);
+        ?>
         <tr>
-            <td>ID: <?php $row['attacking_gang_id'] ?></td>
-            <td>ID: <?php $row['defending_gang_id'] ?></td>
+            <td><?php echo $attackingGang->name ?></td>
+            <td><?php echo $defendingGang->name ?></td>
+            <td><?php echo date( "m/d/Y", strtotime($row['time_started'])); ?></td>
             <td><a href="gang_territory_battle_result.php?id=<?php echo $row['id'] ?>">View</a></td>
         </tr>
     <?php endforeach;?>
