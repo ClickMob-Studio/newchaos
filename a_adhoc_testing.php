bf4ebab0a9ffc1cefd258dc448cbf6ff796335b5
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-07-12 07:11:05 +0100

    u

--- a_adhoc_testing.php
+++ a_adhoc_testing.php
@@ -1,33 +1,18 @@
 <?php
-exit;
+
 include 'header.php';
 
-//$db->query("UPDATE grpgusers SET points = points + 1000000 WHERE id = 295");
-//$db->execute();
-//
-//
-//Give_Item(276, 295, 1);
-//Give_Item(262, 295, 2);
-//Give_Item(272, 295, 1);
-//
-//Send_Event(295, 'Your prizes for finishing first in the recent contest have been awarded.');
-//
-//
-//
-//$db->query("UPDATE grpgusers SET points = points + 500000 WHERE id = 152");
-//$db->execute();
-//
-//Give_Item(271, 152, 1);
-//Give_Item(265, 152, 1);
-//
-//Send_Event(152, 'Your prizes for finishing second in the recent contest have been awarded.');
-//
-//$db->query("UPDATE grpgusers SET points = points + 250000 WHERE id = 7");
-//$db->execute();
-//
-//Give_Item(270, 7, 5);
-//
-//Send_Event(7, 'Your prizes for finishing third in the recent contest have been awarded.');
-//
-//echo 'done'; exit;
+$attack_person = new User(825);
+
+$hitChance = 50;
+$maxDamage = 100;
+$criticalHit = 1;
+$counterAttack = 0;
+
+$damageDifferential = ($attack_person->moddedstrength - $user_class->moddeddefense) / $user_class->moddeddefense;
+
+var_dump($damageDifferential);
+exit;
+
+echo 'done'; exit;
 ?>
