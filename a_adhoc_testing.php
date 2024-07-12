8089fc3002ed422dedba177b052226032979c9d8
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-07-12 07:17:00 +0100

    u

--- a_adhoc_testing.php
+++ a_adhoc_testing.php
@@ -9,6 +9,14 @@ $maxDamage = 100;
 $criticalHit = 1;
 $counterAttack = 0;
 
+echo 'Defender Strength: ' . $attack_person->moddedstrength . '<br />';
+echo 'Defender Defense: ' . $attack_person->moddeddefense . '<br />';
+
+echo 'Attacker Strength: ' . $user_class->moddedstrength . '<br />';
+echo 'Attacker Defense: ' . $user_class->moddeddefense . '<br />';
+
+echo '<hr />';
+
 $damageDifferential = ($attack_person->moddedstrength - $user_class->moddeddefense) / $user_class->moddeddefense;
 
 var_dump($damageDifferential);
