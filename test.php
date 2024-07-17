0f88d2e4035aec76ef5ec9d776c72a437cfab0b9
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-07-17 10:04:17 +0100

    Update test.php

--- test.php
+++ test.php
@@ -5,3 +5,9 @@ $two_weeks_ago = time() - (14 * 24 * 60 * 60);
 // Prepare the SQL query to delete records older than two weeks
 $sql = "DELETE FROM events WHERE timesent < $two_weeks_ago";
 mysql_query($sql);
+$sql = "DELETE FROM attlog WHERE timesent < $two_weeks_ago";
+mysql_query($sql);
+$sql = "DELETE FROM attacklog WHERE timesent < $two_weeks_ago";
+mysql_query($sql);
+$sql = "DELETE FROM cityevents WHERE timesent < $two_weeks_ago";
+mysql_query($sql);
