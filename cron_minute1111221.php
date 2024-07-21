784624e95161e81966fa998dd7d15d4a1ce9e274
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-07-21 12:31:47 +0100

    u

--- cron_minute1111221.php
+++ cron_minute1111221.php
@@ -44,80 +44,6 @@ while ($line = mysql_fetch_assoc($result)) {
             }
         }
         
-        $activeMissionsQuery = "SELECT agm.id AS mission_id, agm.gangid, agm.time, agm.end_time, agm.kills, agm.busts, agm.crimes, agm.mugs 
-        FROM active_gang_missions agm 
-        JOIN gang_missions gm ON agm.mission_id = gm.id 
-        WHERE agm.completed = 0";
-        $activeMissionsResult = mysql_query($activeMissionsQuery);
-        
-        if (!$activeMissionsResult) {
-        die('Failed to query active missions: ' . mysql_error());
-        }
-        
-        $currentTime = time();
-        
-        while ($mission = mysql_fetch_assoc($activeMissionsResult)) {
-        $missionId = $mission['mission_id'];
-        $gangId = $mission['gangid'];
-        $startTime = strtotime($mission['time']);
-        
-        
-        // Fetch the target criteria from the gang_missions table
-        $missionDetailsQuery = "SELECT gm.kills AS target_kills, gm.busts AS target_busts, gm.crimes AS target_crimes, gm.mugs AS target_mugs, gm.reward 
-            FROM gang_missions gm 
-            WHERE gm.id = (SELECT mission_id FROM active_gang_missions WHERE id = $missionId) LIMIT 1";
-        $missionDetailsResult = mysql_query($missionDetailsQuery);
-        if (!$missionDetailsResult) {
-        continue;  // Skip if details can't be fetched
-        }
-        $missionDetails = mysql_fetch_assoc($missionDetailsResult);
-        
-        $allTargetsMet = true;
-        
-        // Check each mission type separately
-        foreach (['kills', 'busts', 'crimes', 'mugs'] as $type) {
-        if ($mission[$type] < $missionDetails["target_$type"]) {
-        $allTargetsMet = false;
-        break;  // No need to check further if any target is not met
-        }
-        }
-        
-        if ($allTargetsMet) {
-        // Update gang points and notify members about successful mission completion
-        $rewardQuery = "UPDATE gangs SET pointsvault = pointsvault + {$missionDetails['reward']} WHERE id = $gangId";
-        mysql_query($rewardQuery);
-        
-        
-        $successMessage = "Your gang has successfully completed the mission and earned a reward of {$missionDetails['reward']} points.";
-        
-        $gangMembersQuery = "SELECT id FROM grpgusers WHERE gang = $gangId";
-        $gangMembersResult = mysql_query($gangMembersQuery);
-        while ($member = mysql_fetch_assoc($gangMembersResult)) {
-        $userId = $member['id'];
-        Send_event($userId, "Congratulations! " . $successMessage);
-        }
-        $endTime = $mission['end_time'];
-        // Mark the mission as completed
-        $markCompletedQuery = "UPDATE active_gang_missions SET completed = 1 WHERE id = $missionId";
-        mysql_query($markCompletedQuery);
-        } elseif ($currentTime >= $endTime) {
-        // Notify gang members about mission failure due to time running out
-        $failureMessage = "The mission was not completed in time.";
-        
-        $gangMembersQuery = "SELECT id FROM grpgusers WHERE gang = $gangId";
-        $gangMembersResult = mysql_query($gangMembersQuery);
-        while ($member = mysql_fetch_assoc($gangMembersResult)) {
-        $userId = $member['id'];
-        Send_event($userId, $failureMessage);
-        }
-        
-        // Mark the mission as completed
-        $markCompletedQuery = "UPDATE active_gang_missions SET completed = 1 WHERE id = $missionId";
-        mysql_query($markCompletedQuery);
-        }
-        }
-        
-        
         // Correctly fetch and check the gang's upgrade9 level
 if ($updates_user->gang > 0) {
     $gangResult = mysql_query("SELECT upgrade9 FROM gangs WHERE id = " . intval($updates_user->gang), $conn);
@@ -937,6 +863,77 @@ if (!$result) {
 } else {
     echo "Duplicate rows deleted successfully.";
 }
+
+$activeMissionsQuery = "SELECT agm.id AS mission_id, agm.gangid, agm.time, agm.end_time, agm.kills, agm.busts, agm.crimes, agm.mugs 
+        FROM active_gang_missions agm 
+        JOIN gang_missions gm ON agm.mission_id = gm.id 
+        WHERE agm.completed = 0";
+$activeMissionsResult = mysql_query($activeMissionsQuery);
+
+if ($activeMissionsResult) {
+    $currentTime = time();
+
+    while ($mission = mysql_fetch_assoc($activeMissionsResult)) {
+        $missionId = $mission['mission_id'];
+        $gangId = $mission['gangid'];
+        $startTime = strtotime($mission['time']);
+
+
+        // Fetch the target criteria from the gang_missions table
+        $missionDetailsQuery = "SELECT gm.kills AS target_kills, gm.busts AS target_busts, gm.crimes AS target_crimes, gm.mugs AS target_mugs, gm.reward 
+            FROM gang_missions gm 
+            WHERE gm.id = (SELECT mission_id FROM active_gang_missions WHERE id = $missionId) LIMIT 1";
+        $missionDetailsResult = mysql_query($missionDetailsQuery);
+        if (!$missionDetailsResult) {
+            continue;  // Skip if details can't be fetched
+        }
+        $missionDetails = mysql_fetch_assoc($missionDetailsResult);
+
+        $allTargetsMet = true;
+
+        // Check each mission type separately
+        foreach (['kills', 'busts', 'crimes', 'mugs'] as $type) {
+            if ($mission[$type] < $missionDetails["target_$type"]) {
+                $allTargetsMet = false;
+                break;  // No need to check further if any target is not met
+            }
+        }
+
+        if ($allTargetsMet) {
+            // Update gang points and notify members about successful mission completion
+            $rewardQuery = "UPDATE gangs SET pointsvault = pointsvault + {$missionDetails['reward']} WHERE id = $gangId";
+            mysql_query($rewardQuery);
+
+            $successMessage = "Your gang has successfully completed the mission and earned a reward of {$missionDetails['reward']} points.";
+
+            $gangMembersQuery = "SELECT id FROM grpgusers WHERE gang = $gangId";
+            $gangMembersResult = mysql_query($gangMembersQuery);
+            while ($member = mysql_fetch_assoc($gangMembersResult)) {
+                $userId = $member['id'];
+                Send_event($userId, "Congratulations! " . $successMessage);
+            }
+            $endTime = $mission['end_time'];
+            // Mark the mission as completed
+            $markCompletedQuery = "UPDATE active_gang_missions SET completed = 1 WHERE id = $missionId";
+            mysql_query($markCompletedQuery);
+        } elseif ($currentTime < $endTime) {
+            // Notify gang members about mission failure due to time running out
+            $failureMessage = "The mission was not completed in time.";
+
+            $gangMembersQuery = "SELECT id FROM grpgusers WHERE gang = $gangId";
+            $gangMembersResult = mysql_query($gangMembersQuery);
+            while ($member = mysql_fetch_assoc($gangMembersResult)) {
+                $userId = $member['id'];
+                Send_event($userId, $failureMessage);
+            }
+
+            // Mark the mission as completed
+            $markCompletedQuery = "UPDATE active_gang_missions SET completed = 1 WHERE id = $missionId";
+            mysql_query($markCompletedQuery);
+        }
+    }
+}
+
 
 
 
