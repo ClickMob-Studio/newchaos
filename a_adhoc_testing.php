31e862bac5889b31403c3a976885859fd8137007
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-07-22 09:35:51 +0100

    u

--- a_adhoc_testing.php
+++ a_adhoc_testing.php
@@ -1,189 +1,82 @@
 <?php
-include 'header.php';
-$m->set('lastcrimeload.'.$user_class->id, time());
-?>
-    <div class='box_top'>Speed Gym</div>
-    <div class='box_middle'>
-    <div class='pad'>
-    <p><a href="gym.php" class="dcSecondaryButton"> Go to Standard Gym </a></p>
-    <?php
-    if ($user_class->hospital > 0) {
-        echo Message("You can't train at the gym if you are in the hospital.");
-        include 'footer.php';
-        die();
-    }
-    if ($printcaptcha != "") {
-        echo $printcaptcha;
-    } else {
-        ?>
-        <script>
-            var doingtrain = false, what = "";
-            function start(statName) {
-                what = statName;
-                doingtrain = true;
-                var isMegaTrain = $('#mega_train').is(':checked') ? 'yes' : 'no';
-                var intervalId = setInterval(function() {
-                    if (doingtrain && what) {
-                        trainrefill(what, isMegaTrain);
-                    } else {
-                        clearInterval(intervalId);
-                        intervalId = null;
-                    }
-                }, 30);
-            }
 
-            function finish() {
-                if (doingtrain) {
-                    location.reload();
-                }
-                what = "";
-                doingtrain = false;
-            }
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-            function trainrefill(stat, isMegaTrain) {
-                $("#noti").html("<img src='images/ajax-loader.gif?' />");
-                $.post("ajax_supergym.php", {
-                    amnt: <?php echo $user_class->maxenergy ?>,
-                    stat: stat,
-                    what: "trainrefill",
-                    mega_train: isMegaTrain // Pass the mega train status to the server
-                }, function(response) {
-                    var info = response.split("|");
-                    $(".hidden-alert").show();
-                    $("#noti").html(info[0]);
-                    $(".points").html(info[1]);
-                    $("#" + stat + "amnt").html(info[2]);
-                    if (info[3]) {
-                        $("#strength").val(info[3]);
-                        $("#defense").val(info[3]);
-                        $("#speed").val(info[3]);
-                        $("#agility").val(info[3]);
-                    }
-                });
-            }
+include 'dbcon.php';
+include 'classes.php';
+include 'database/pdo_class.php';
+print "working";
 
-            document.onblur = function() { finish(); };
-            window.onblur = function() { finish(); };
-            document.body.onmouseup = function() { finish(); };
-        </script>
+$activeMissionsQuery = "SELECT agm.id AS mission_id, agm.gangid, agm.time, agm.end_time, agm.kills, agm.busts, agm.crimes, agm.mugs 
+        FROM active_gang_missions agm 
+        JOIN gang_missions gm ON agm.mission_id = gm.id 
+        WHERE agm.completed = 0";
+$activeMissionsResult = mysql_query($activeMissionsQuery);
 
-        <div style="display:flex;min-height:60px;flex-direction:row;">
-            <div class="alert alert-info hidden-alert" style="display: none;">
-                <p><div id="noti"></div></p>
-            </div>
-        </div>
+if ($activeMissionsResult) {
+    $currentTime = time();
 
-        <?php
-        echo "
-    <br />
-    
-<div class='contenthead floaty'>
-<span style='margin: 0; line-height: 27px; text-transform: uppercase; font-size: 20px; text-align: left; text-indent: 25px;'>
-    <table id='newtables' class='altcolors' style='width:100%;'>
-        <tr>
-            <th>STRENGTH</th>
-            <th>DEFENSE</th>
-            <th>SPEED</th>
-            <th>AGILITY</th>
-        </tr>
-        <tr>
-            <td><span id='strengthamnt'>".prettynum($user_class->strength)."</span> [Ranked: ".getRank("$user_class->id", "strength")."]</td>
-            <td><span id='defenseamnt'>".prettynum($user_class->defense)."</span> [Ranked: ".getRank("$user_class->id", "defense")."]</td>
-            <td><span id='speedamnt'>".prettynum($user_class->speed)."</span> [Ranked: ".getRank("$user_class->id", "speed")."]</td>
-            <td><span id='agilityamnt'>".prettynum($user_class->agility)."</span> [Ranked: ".getRank("$user_class->id", "agility")."]</td>
-        </tr>
-        <tr>
-            <td><button onmousedown='start(\"strength\");' onmouseup='finish();' ontouchend='finish();' onmouseleave='finish();' ontouchstart='start(\"strength\");'>Strength + Refills</button></td>
-            <td><button onmousedown='start(\"defense\");' onmouseup='finish();' ontouchend='finish();' onmouseleave='finish();' ontouchstart='start(\"defense\");'>Defense + Refills</button></td>
-            <td><button onmousedown='start(\"speed\");' onmouseup='finish();' ontouchend='finish();' onmouseleave='finish();' ontouchstart='start(\"speed\");'>Speed + Refills</button></td>
-            <td><button onmousedown='start(\"agility\");' onmouseup='finish();' ontouchend='finish();' onmouseleave='finish();' ontouchstart='start(\"agility\");'>Agility + Refills</button></td>
-        </tr>
-        <tr>
-            <td colspan='4'><span style='color:red;font-weight:bold;'>Click and hold down the mouse on the stat + refills button.</span></td>
-        </tr>
-    </table></div>";
-    }
-    ?>
-    <h1>Daily Stats</h1>
-    <p>Here you will find your historical gym stats, these update at rollover</p>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    while ($mission = mysql_fetch_assoc($activeMissionsResult)) {
+        $missionId = $mission['mission_id'];
+        $gangId = $mission['gangid'];
+        $startTime = strtotime($mission['time']);
 
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-    <canvas id="statsChart" width="100" height="100"></canvas>
-    <script>
-        fetch('ajax_gym_stats.php')
-            .then(response => response.json())
-        .then(data => {
-            const dates = data.map(item => item.record_date);
-        const strengths = data.map(item => item.strength);
-        const defenses = data.map(item => item.defense);
-        const speeds = data.map(item => item.speed);
-        const agilitys = data.map(item => item.agility);
+        // Fetch the target criteria from the gang_missions table
+        $missionDetailsQuery = "SELECT gm.kills AS target_kills, gm.busts AS target_busts, gm.crimes AS target_crimes, gm.mugs AS target_mugs, gm.reward 
+            FROM gang_missions gm 
+            WHERE gm.id = (SELECT mission_id FROM active_gang_missions WHERE id = $missionId) LIMIT 1";
+        $missionDetailsResult = mysql_query($missionDetailsQuery);
+        if (!$missionDetailsResult) {
+            continue;  // Skip if details can't be fetched
+        }
+        $missionDetails = mysql_fetch_assoc($missionDetailsResult);
 
-        const ctx = document.getElementById('statsChart').getContext('2d');
-        const statsChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: dates,
-                datasets: [
-                    {
-                        label: 'Strength',
-                        data: strengths,
-                        borderColor: 'red',
-                        backgroundColor: 'rgba(255, 0, 0, 0.1)',
-                    },
-                    {
-                        label: 'Defense',
-                        data: defenses,
-                        borderColor: 'green',
-                        backgroundColor: 'rgba(0, 255, 0, 0.1)',
-                    },
-                    {
-                        label: 'Speed',
-                        data: speeds,
-                        borderColor: 'blue',
-                        backgroundColor: 'rgba(0, 0, 255, 0.1)',
-                    },
-                    {
-                        label: 'Agility',
-                        data: agilitys,
-                        borderColor: 'yellow',
-                        backgroundColor: 'rgba(0, 0, 255, 0.1)',
-                    }
-                ]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true,
-                        ticks: {
-                            color: 'white' // Set Y-axis tick labels to white
-                        }
-                    },
-                    x: {
-                        ticks: {
-                            color: 'white' // Set X-axis tick labels to white
-                        }
-                    }
-                },
-                legend: {
-                    labels: {
-                        color: 'white' // Set legend labels to white
-                    }
-                },
-                plugins: {
-                    legend: {
-                        labels: {
-                            color: 'white' // Ensures text color is white
-                        }
-                    }
+        $allTargetsMet = true;
+
+        // Check each mission type separately
+        foreach (['kills', 'busts', 'crimes', 'mugs'] as $type) {
+            if ($missionDetails['target_' . $type] > 0) {
+                if ($mission[$type] < $missionDetails['target_' . $type]) {
+                    $allTargetsMet = false;
+                    break;  // No need to check further if any target is not met
                 }
             }
-        });
-        })
-        .catch(error => console.error('Error loading the data: ', error));
-    </script>
-<?php
-include 'footer.php';
-?>
+        }
+
+        if ($allTargetsMet) {
+            // Update gang points and notify members about successful mission completion
+            $rewardQuery = "UPDATE gangs SET pointsvault = pointsvault + {$missionDetails['reward']} WHERE id = $gangId";
+            mysql_query($rewardQuery);
+
+            $successMessage = "Your gang has successfully completed the mission and earned a reward of {$missionDetails['reward']} points.";
+
+            $gangMembersQuery = "SELECT id FROM grpgusers WHERE gang = $gangId";
+            $gangMembersResult = mysql_query($gangMembersQuery);
+            while ($member = mysql_fetch_assoc($gangMembersResult)) {
+                $userId = $member['id'];
+                Send_event($userId, "Congratulations! " . $successMessage);
+            }
+            $endTime = $mission['end_time'];
+            // Mark the mission as completed
+            $markCompletedQuery = "UPDATE active_gang_missions SET completed = 1 WHERE id = $missionId";
+            mysql_query($markCompletedQuery);
+        } elseif ($currentTime < $endTime) {
+            // Notify gang members about mission failure due to time running out
+            $failureMessage = "The mission was not completed in time.";
+
+            $gangMembersQuery = "SELECT id FROM grpgusers WHERE gang = $gangId";
+            $gangMembersResult = mysql_query($gangMembersQuery);
+            while ($member = mysql_fetch_assoc($gangMembersResult)) {
+                $userId = $member['id'];
+                Send_event($userId, $failureMessage);
+            }
+
+            // Mark the mission as completed
+            $markCompletedQuery = "UPDATE active_gang_missions SET completed = 1 WHERE id = $missionId";
+            mysql_query($markCompletedQuery);
+        }
+    }
+}
