c5f8727d1f15b1b8c3dab4fcf955b24435f4beb2
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-08-07 22:51:05 +0100

    u

--- a_adhoc_testing.php
+++ a_adhoc_testing.php
@@ -1,25 +1,7 @@
 <?php
-exit;
+
 include 'header.php';
 
 
-$db->query("SELECT * FROM `events` WHERE `text` LIKE '%Crimson Canyons Bank%'");
-$events = $db->fetch_row();
-
-$rows = array();
-foreach ($events as $event) {
-    if (!isset($rows[$event['to']])) {
-        $rows[$event['to']] = 0;
-    }
-
-    $rows[$event['to']]++;
-}
-
-foreach ($rows as $userid => $amount) {
-    $reward = 150000000 * $amount;
-    echo 'UPDATE grpgusers SET bank = bank + ' . $reward . ' WHERE id = ' . $userid;
-    echo '<br />';
-
-    $db->query('UPDATE grpgusers SET bank = bank + ' . $reward . ' WHERE id = ' . $userid);
-    $db->execute();
-}
+$db->query("DELETE FROM `events` ORDER BY `timesent` ASC LIMIT 100000");
+$db->execute();
