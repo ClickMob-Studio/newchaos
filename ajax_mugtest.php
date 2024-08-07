ae7092afb8d832d3f3f9870ebc806221b9ba367f
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-08-07 12:41:19 +0100

    Update ajax_mugtest.php

--- ajax_mugtest.php
+++ ajax_mugtest.php
@@ -1,7 +1,9 @@
 <?php
 
 session_start();
-
+error_reporting(E_ALL);
+error_reporting(-1);
+ini_set('error_reporting', E_ALL);
 function error($msg) {
     return array(
         'success' => 'false',
