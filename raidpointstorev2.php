fec8189e82b20243f9b7573d424ea61cc97feb65
Aaron Newton <aaron.12.newton@gmail.com>
2024-08-08 12:37:35 +0100

    u

--- raidpointstorev2.php
+++ raidpointstorev2.php
@@ -30,6 +30,7 @@ $items = array(
 if(isset($_GET['buy'])){
     foreach($items as $item) {
         if($_GET['buy'] == $item[0]) {
+            echo $_POST['qty']; exit;
             //Send_Event(2, $user_class->formattedname . ' RAID STORE: ' . $item[1] . ' - ' . $item[2] . ' - ' . $user_class->raidpoints, 2);
             if(purchaseItem($item[2], $user_class, $db)) {
                 // Handle the purchase based on item code
