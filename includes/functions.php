5299eeb1dbbffc2206637b2b0ff84c30def2ed2b
Aaron Newton <aaron.12.newton@gmail.com>
2024-08-08 10:47:04 +0100

    u

--- includes/functions.php
+++ includes/functions.php
@@ -910,12 +910,12 @@ function formatName($id, $nogang = 0)
             $skull = $db->fetch_single();
 
             if ($skull !== false) {
-                $name .= " <img src='images/skullpres_" . $skull . ".png?v=1' title='Prestige ({$row['prestige']})' height='25' width='25'' />";
+                $name .= " <img src='images/skullpres_" . $skull . ".png?v=2' title='Prestige ({$row['prestige']})' height='25' width='25'' />";
             } else {
-                $name .= " <img src='images/skullpres_" . $row['prestige'] . ".png?v=1' title='Prestige ({$row['prestige']})' height='25'' width='25'' />";
+                $name .= " <img src='images/skullpres_" . $row['prestige'] . ".png?v=2' title='Prestige ({$row['prestige']})' height='25'' width='25'' />";
             }
         } else {
-            $name .= " <img src='images/skullpres_" . $row['prestige'] . ".png?v=1' title='Prestige ({$row['prestige']})' height='25'' width='25'' />";
+            $name .= " <img src='images/skullpres_" . $row['prestige'] . ".png?v=2' title='Prestige ({$row['prestige']})' height='25'' width='25'' />";
         }
     }
     if ($nogang == 0)
