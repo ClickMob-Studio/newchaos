238990d0c74e1f577e5fa109fb33ef84e5306709
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-08-08 08:59:23 +0100

    Update jail.php

--- jail.php
+++ jail.php
@@ -360,7 +360,7 @@ if($user_class->jail > 0){
 
                 var request = $.ajax({
                     <?php if($user_class->admin > 0){
-                    echo "url: 'ajax_jail_new.php?jailbreak=bot',";
+                    echo "url: 'ajax_jail_test.php?jailbreak=bot',";
                     }else{
                         echo "url: 'ajax_jail_new.php?jailbreak=bot',";
                     }
