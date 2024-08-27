b84cac0af2762844bcd66c256baac6e01f3f14b8
Aaron Newton <aaron.12.newton@gmail.com>
2024-08-27 15:38:13 +0100

    up

---
+++ trainingdummies.php
@@ -0,0 +1,33 @@
+<?php
+include 'header.php';
+
+$db->query("SELECT * FROM trainingdummies");
+$db->execute();
+$trainingDummies = $db->fetch_row();
+
+?>
+
+<div class='box_top'>Training Dummies</div>
+<div class='box_middle'>
+    <div class='pad'>
+        <div class="table-responsive">
+            <table class="new_table" id="newtables">
+                <tr>
+                    <th>Dummy</th>
+                    <th>EXP</th>
+                    <th>Reward</th>
+                    <th>&nbsp;</th>
+                </tr>
+                <?php foreach ($trainingDummies as $trainingDummy): ?>
+                    <tr>
+                        <td><?php echo $trainingDummies['name'] ?></td>
+                        <td>
+                            <!-- TODO -->
+                        </td>
+                        <td><?php echo Item_Name($trainingDummies['item_id']) ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+        </div>
+    </div>
+</div>
