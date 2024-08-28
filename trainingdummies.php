21569537a829656cfd0d984e4333c383b26ea257
Aaron Newton <aaron.12.newton@gmail.com>
2024-08-28 14:55:08 +0100

    u

--- trainingdummies.php
+++ trainingdummies.php
@@ -5,6 +5,11 @@ $db->query("SELECT * FROM training_dummy");
 $db->execute();
 $trainingDummies = $db->fetch_row();
 
+$trainingDummiesIndexed = array();
+foreach ($trainingDummies as $trainingDummy) {
+    $trainingDummiesIndexed[$trainingDummy['id']] = $trainingDummy;
+}
+
 $db->query("SELECT * FROM training_dummy_user WHERE user_id = ?");
 $db->execute(array($user_class->id));
 $trainingDummyUsers = $db->fetch_row();
@@ -22,6 +27,127 @@ if (count($trainingDummyUsers) < 1) {
 $trainingDummyUsersIndexed = array();
 foreach ($trainingDummyUsers as $trainingDummyUser) {
     $trainingDummyUsersIndexed[$trainingDummyUser['training_dummy_id']] = $trainingDummyUsers;
+}
+
+if (isset($_GET['attack']) && (int)$_GET['attack'] && (int)$_GET['attack'] > 0) {
+    $attack = (int)$_GET['attack'];
+
+    if ($user_class->hospital > 0) {
+        diefun('You can\'t attack a training dummy when your in hospital. <a href="trainingdummies.php">Go Back</a>.');
+    }
+
+    if ($user_class->jail > 0) {
+        diefun('You can\'t attack a training dummy when your in jail. <a href="trainingdummies.php">Go Back</a>.');
+    }
+
+    if ($user_class->energy < $user_class->maxenergy) {
+        diefun('You need full energy to perform a training dummy attack! <a href="trainingdummies.php">Go Back</a>.');
+    }
+
+    if (!isset($trainingDummyUsersIndexed[$attack])) {
+        if (isset($trainingDummiesIndexed[$attack])) {
+            $db->query("INSERT INTO training_dummy_user (training_dummy_id, user_id, level, exp, is_fight_available) VALUES (?, ?, 1, 0, 1)");
+            $db->execute(array($attack, $user_class->id));
+
+            header('Location: trainingdummies.php?attack=' . $attack);
+            exit();
+        } else {
+            diefun('Something went wrong. Please DM an admin if this issue persists. <a href="trainingdummies.php">Go Back</a>.');
+        }
+    }
+
+    $trainingDummyToUse = $trainingDummiesIndexed[$attack];
+    $trainingDummyUserToUse = $trainingDummyUsersIndexed[$attack];
+
+    $totalUserHealth = $user_class->hp;
+    $totalUserSpeed = $user_class->moddedspeed;
+    $totalUserDefense = $user_class->moddeddefense;
+    $totalUserStrength = $user_class->moddedstrength;
+
+    $botStrength = $trainingDummyToUse['strength'] * $trainingDummyToUse['level'];
+    $botDefence = $trainingDummyToUse['defence'] * $trainingDummyToUse['level'];
+    $botSpeed = $trainingDummyToUse['speed'] * $trainingDummyToUse['level'];
+    $bossHp = $trainingDummyToUse['health'] * $trainingDummyToUse['level'];
+
+    while ($totalUserHealth > 0 && $bossHp > 0) {
+        $damage = $botStrength - $totalUserDefense;
+        $damage = ($damage < 1) ? 1 : $damage;
+        $damage = rand(1, $damage);
+
+        if ($damage == 1) {
+            if ($botSpeed < 100) {
+                $damage = rand(1, 3);
+            } else if ($botSpeed < 500) {
+                $damage = rand(2, 6);
+            } elseif ($botSpeed < 2500) {
+                $damage = rand(5, 25);
+            } elseif ($botSpeed < 25000) {
+                $damage = rand(25, 100);
+            } else {
+                $damage = rand(100, 200);
+            }
+        }
+
+        $totalUserHealth = $totalUserHealth - $damage;
+
+        if ($totalUserHealth > 0) {
+            $damage = $totalUserStrength - $botDefence;
+            $damage = ($damage < 1) ? 1 : $damage;
+            $damage = rand(1, $damage);
+            if ($damage == 1) {
+                if ($totalUserSpeed < 100) {
+                    $damage = rand(1, 3);
+                } elseif ($totalUserSpeed < 500) {
+                    $damage = rand(2, 6);
+                } elseif ($totalUserSpeed < 2500) {
+                    $damage = rand(5, 25);
+                } elseif ($totalUserSpeed < 25000) {
+                    $damage = rand(25, 100);
+                } else {
+                    $damage = rand(100, 200);
+                }
+            }
+
+            $bossHp = $bossHp - $damage;
+        }
+    }
+
+    if ($bossHp <= 0) {
+        // Won Fight
+        $expBoost = mt_rand(5,10);
+
+        $newExp = $trainingDummyUserToUse['exp'] + $expBoost;
+        if ($newExp > 100) {
+            Give_Item($trainingDummyToUse['reward_item_id'], $user_class->id, 1);
+
+            Send_Event($user_class->id, 'You earned the special reward for beating the training dummy and 1 x ' . Item_Name($trainingDummyToUse['reward_item_id']) . ' has been added to your inventory');
+
+            $newExp = 0;
+        }
+        $expReward = $user_class->maxexp / 10000;
+        $expReward = $expReward * mt_rand(1,2);
+        $expReward = ceil($expReward);
+        if ($expReward < 10) {
+            $expReward = mt_rand(1,10);
+        }
+
+        $cashReward = mt_rand(100000, 1000000);
+
+        $db->query('UPDATE grpgusers SET energy = 0, money = money + ?, exp = exp + ? WHERE id = ?');
+        $db->execute(array($cashReward, $expReward, $user_class->id));
+
+        $db->query('UPDATE training_dummy_user SET level = level + 1, exp + ?, last_fight_time = ? WHERE id = ?');
+        $db->execute(array($newExp, time(), $trainingDummyUserToUse['id']));
+
+        diefun('You have successfully beaten the training dummy and you have been rewarded ' . number_format($expReward, 0) . ' EXP & $' . number_format($cashReward, 0) .'! <a href="trainingdummies.php">Go Back</a>.');
+    } else {
+        // Lost Fight
+        $db->query('UPDATE grpgusers SET hp = 0, energy = 0, hospital = 10 WHERE id = ?');
+        $db->execute(array($user_class->id));
+
+
+        diefun('You lost to the training dummy and you will now need to spend 10 minutes in hospital! <a href="trainingdummies.php">Go Back</a>.');
+    }
 }
 ?>
 
@@ -77,7 +203,7 @@ foreach ($trainingDummyUsers as $trainingDummyUser) {
                             </center>
                         </td>
                         <td>
-                            <a href="#" class="btn btn-primary">Attack</a>
+                            <a href="trainingdummies.php?attack=<?php echo $trainingDummy['id'] ?>" class="btn btn-primary">Attack</a>
                         </td>
                     </tr>
                 <?php endforeach; ?>
