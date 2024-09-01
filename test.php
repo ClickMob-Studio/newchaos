111d603837657300de41dd77254821e7e7cb3109
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-09-01 12:38:48 +0100

    Update test.php

--- test.php
+++ test.php
@@ -8,11 +8,12 @@ require "header.php";
         $ownedByGang = new Gang($gangTerritoryZone['owned_by_gang_id']);
 
         foreach ($ownedByGang->memberids as $memberid) {
-            
+            $us = $memberid['id'];
             if ($gangTerritoryZone['daily_money_payout'] > 50000000) {
                 $db->query("UPDATE grpgusers SET bank = bank + " . $gangTerritoryZone['daily_money_payout'] . " WHERE id = 1");
                 $db->execute();
 
+                mysql_query("INSERT INTO bank_log VALUES('', 1, ".$gangTerritoryZone['daily_money_payout'].", 'mdep', $us->bank, unix_timestamp())");
                 Send_Event(1, 'You gained $' . number_format($gangTerritoryZone['daily_money_payout'], 0) . ' for your gangs protection racket ' . $gangTerritoryZone['name']);
             }
             }
