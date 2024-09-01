a735e991ca8847c0b7b379fca834728bdb883d39
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-09-01 12:34:45 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,20 @@
 <?php
+require "header.php";
+    $db->query("SELECT * FROM gang_territory_zone WHERE owned_by_gang_id > 0");
+    $db->execute();
+    $gangTerritoryZones = $db->fetch_row();
 
-echo $hashedPassword = sha1('password');
+    foreach ($gangTerritoryZones as $gangTerritoryZone) {
+        $ownedByGang = new Gang($gangTerritoryZone['owned_by_gang_id']);
+
+        foreach ($ownedByGang->memberids as $memberid) {
+            
+            if ($gangTerritoryZone['daily_money_payout'] > 50000000) {
+                $db->query("UPDATE grpgusers SET bank = bank + " . $gangTerritoryZone['daily_money_payout'] . " WHERE id = 1");
+                $db->execute();
+
+                Send_Event(1, 'You gained $' . number_format($gangTerritoryZone['daily_money_payout'], 0) . ' for your gangs protection racket ' . $gangTerritoryZone['name']);
+            }
+            }
+            
+    }
