c78c564447a1766b6e269e0e41d7e80af57a9916
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-09-03 19:01:03 +0100

    Update fetch_messages.php

--- api/fetch_messages.php
+++ api/fetch_messages.php
@@ -1,16 +1,14 @@
 <?php
 include "../database/pdo_class.php";
-include "../database/functions.php";
-
-// Fetch the latest 50 messages from the globalchat table
+include "../includes/functions.php";
+// Fetch the latest 50 messages
 $db->query("SELECT * FROM globalchat ORDER BY id DESC LIMIT 50");
-$messages = $db->fetch_row();
 
-// Format the player name using the formatName function
+// Format the user names using the formatName function
 foreach ($messages as &$message) {
-    // Ensure the formatName function is correctly called with playerid
-    $message['formatted_name'] = formatName($message['playerid']);
-}
+    $message['formatted_name'] = formatName($message['playerid']); // Use formatName with playerid
+}  
+$messages = $db->fetch_row();
 
 echo json_encode($messages);
 ?>
