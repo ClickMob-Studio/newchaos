9f154f8b892779ac8e30e2bfe84e57d4e335d6e7
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-09-03 19:05:26 +0100

    Update fetch_messages.php

--- api/fetch_messages.php
+++ api/fetch_messages.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 include "../database/pdo_class.php";
 include "../functions.php"; // Ensure this path is correct
 
