e8b4f556e60ef0169092c844dbb2e29e92affe77
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-09-03 20:28:21 +0100

    Update test.php

--- includes/test.php
+++ includes/test.php
@@ -12,16 +12,9 @@ require "header.php";
     </div>
     <div id="chatbox" class="border rounded mt-2" style="height: 200px; overflow-y: auto; background-color: #2e2e2a; display: none; color: #fff;"></div>
     <div class="input-group mt-2" id="chat-input-container" style="display: none;">
-        <button class="btn btn-secondary" id="emoji-picker" style="background-color: rgba(78, 77, 72, 0.8); border-color: rgba(78, 77, 72, 0.8);">😀</button> <!-- Emoji Picker Button -->
         <input type="text" id="message" class="form-control" placeholder="Type a message..." style="background-color: #3a3935; color: #fff; border: 1px solid rgba(78, 77, 72, 0.8);">
         <button class="btn text-white" id="send" style="background-color: rgba(78, 77, 72, 0.8); border-color: rgba(78, 77, 72, 0.8);">Send</button>
     </div>
-</div>
-
-<!-- Emoji Picker Modal -->
-<div id="emoji-modal" style="display: none; position: fixed; top: 20%; left: 50%; transform: translate(-50%, -50%); width: 300px; max-height: 300px; overflow-y: auto; background-color: #2e2e2a; border: 1px solid #4e4d48; padding: 10px; z-index: 1050;">
-    <button id="close-emoji-modal" style="float: right; background-color: #4e4d48; border: none; color: white;">Close</button>
-    <div id="emoji-list"><?php emotes(); ?></div> <!-- Populate emojis using the emotes() function -->
 </div>
 
 <script>
@@ -75,24 +68,6 @@ $(document).ready(function () {
     function scrollToBottom() {
         $('#chatbox').scrollTop($('#chatbox')[0].scrollHeight);
     }
-
-    // Show the emoji picker modal when the button is clicked
-    $('#emoji-picker').on('click', function () {
-        $('#emoji-modal').show();
-    });
-
-    // Close the emoji picker modal
-    $('#close-emoji-modal').on('click', function () {
-        $('#emoji-modal').hide();
-    });
-
-    // Function to add an emoji to the message input
-    window.addsmiley = function(code) {
-        let message = $('#message').val();
-        $('#message').val(message + ' ' + code);
-        $('#message').focus();
-        $('#emoji-modal').hide();
-    };
 
     // Send message on button click
     $('#send').on('click', function () {
