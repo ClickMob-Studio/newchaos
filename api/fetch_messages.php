84ea9a35430c56f062f5c807ddf086027d3e23b6
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-09-03 19:14:12 +0100

    Update fetch_messages.php

--- api/fetch_messages.php
+++ api/fetch_messages.php
@@ -1,7 +1,5 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 include "../database/pdo_class.php";
 $m = new Memcache();
 $m->addServer('127.0.0.1', 11212, 33);
