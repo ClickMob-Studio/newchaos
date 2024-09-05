ee382ea2b0a8ee7b2cdb15ce9ed32e34b725b681
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-09-05 16:29:06 +0100

    Update auto_move.php

--- includes/auto_move.php
+++ includes/auto_move.php
@@ -1,48 +1,50 @@
 <?php
 require('sec_inc.php');
 
-if ($gID == '' || $gID == 0)
+if ($gID == '' || $gID == 0) {
     die();
+}
 
 header('Content-Type: text/javascript');
+
 echo $addons->get_hooks(
     array(),
     array(
         'page'     => 'includes/auto_move.php',
-        'location'  => 'page_start'
+        'location' => 'page_start'
     )
 );
 
-$time    = time();
+$time = time();
 $timenow = $time;
 
 $pdo->query("UPDATE " . DB_PLAYERS . " SET timetag = $timenow WHERE username = '$plyrname' AND gID = " . $gameID);
 
-$tpq        = $pdo->query("SELECT * FROM " . DB_POKER . " WHERE gameID = $gameID");
-$tpr        = $tpq->fetch(PDO::FETCH_ASSOC);
-$hand       = $tpr['hand'];
-$nexthand   = $hand + 1;
+$tpq = $pdo->query("SELECT * FROM " . DB_POKER . " WHERE gameID = $gameID");
+$tpr = $tpq->fetch(PDO::FETCH_ASSOC);
+$hand = $tpr['hand'];
+$nexthand = $hand + 1;
 $autoplayer = $tpr['move'];
-$movetimer  = MOVETIMER;
+$movetimer = MOVETIMER;
 $lmovetimer = KICKTIMER;
-$distimer   = DISCONNECT;
-$kick       = $time - ($lmovetimer * 60);
-$timekick   = $time - $distimer;
-$lastmove   = $tpr['lastmove'];
-$diff       = $time - $lastmove;
-$dealer     = $tpr['dealer'];
-$tablepot   = $tpr['pot'];
-$tabletype  = $tpr['tabletype'];
+$distimer = DISCONNECT;
+$kick = $time - ($lmovetimer * 60);
+$timekick = $time - $distimer;
+$lastmove = $tpr['lastmove'];
+$diff = $time - $lastmove;
+$dealer = $tpr['dealer'];
+$tablepot = $tpr['pot'];
+$tabletype = $tpr['tabletype'];
 $tablelimit = $tpr['tablelimit'];
-$sbamount   = $tpr['sbamount'];
-$bbamount   = $tpr['bbamount'];
-$tablebet   = $tpr['bet'];
-$lastbet    = $tpr['lastbet'];
+$sbamount = $tpr['sbamount'];
+$bbamount = $tpr['bbamount'];
+$tablebet = $tpr['bet'];
+$lastbet = $tpr['lastbet'];
 $game_style = ($tpr['gamestyle'] == 'o') ? GAME_OMAHA : GAME_TEXAS;
-$gamestyle  = $tpr['gamestyle'];
+$gamestyle = $tpr['gamestyle'];
 
-$delayed    = ($lastmove < $time)       ? true : false;
-$interplay  = ($hand > 4 && $hand < 12) ? true : false;
+$delayed = ($lastmove < $time) ? true : false;
+$interplay = ($hand > 4 && $hand < 12) ? true : false;
 
 $opsTheme->addVariable('movetimer', $movetimer);
 
@@ -54,58 +56,64 @@ echo $addons->get_hooks(
     )
 );
 
-if ($hand < 0 || $autoplayer < 1)
+if ($hand < 0 || $autoplayer < 1) {
     die();
+}
 
-$showshort  = false;
-$allpl      = 0;
+$showshort = false;
+$allpl = 0;
 
-if ($autoplayer > 0)
-{
+if ($autoplayer > 0) {
     $nextup = nextplayer($autoplayer);
 
-    $autoname   = $tpr['p' . $autoplayer . 'name'];
-    $autopot    = $tpr['p' . $autoplayer . 'pot'];
-    $autobet    = $tpr['p' . $autoplayer . 'bet'];
-    $autofold   = substr($autobet, 0, 1);
+    $autoname = $tpr['p' . $autoplayer . 'name'];
+    $autopot = $tpr['p' . $autoplayer . 'pot'];
+    $autobet = $tpr['p' . $autoplayer . 'bet'];
+    $autofold = substr($autobet, 0, 1);
     $autostatus = 'live';
 
-    if ($autopot == 0 && $autobet > 0 && $interplay)
+    if ($autopot == 0 && $autobet > 0 && $interplay) {
         $autostatus = 'allin';
+    }
 
-    if ($autofold == 'F' && $interplay)
+    if ($autofold == 'F' && $interplay) {
         $autostatus = 'fold';
+    }
 
-    if ($autopot == 0 && ($autobet == 0 || $autostatus == 'fold'))
+    if ($autopot == 0 && ($autobet == 0 || $autostatus == 'fold')) {
         $autostatus = 'bust';
+    }
 
     $np = 0;
     $ai = 0;
     $fo = 0;
     $bu = 0;
 
-    for ($i = 1; $i < 11; $i++)
-    {
-        $usr   = $tpr['p' . $i . 'name'];
-        $upot  = $tpr['p' . $i . 'pot'];
-        $ubet  = $tpr['p' . $i . 'bet'];
+    for ($i = 1; $i < 11; $i++) {
+        $usr = $tpr['p' . $i . 'name'];
+        $upot = $tpr['p' . $i . 'pot'];
+        $ubet = $tpr['p' . $i . 'bet'];
         $ufold = substr($ubet, 0, 1);
 
-        if (empty($usr))
+        if (empty($usr)) {
             continue;
+        }
 
         $np++;
 
-        if ($upot == 0 && $ubet > 0 && $ufold != 'F' && ($hand > 4 && $hand < 15))
+        if ($upot == 0 && $ubet > 0 && $ufold != 'F' && ($hand > 4 && $hand < 15)) {
             $ai++;
+        }
 
-        if ($ufold == 'F' && $upot > 0 && ($hand > 4 && $hand < 15))
+        if ($ufold == 'F' && $upot > 0 && ($hand > 4 && $hand < 15)) {
             $fo++;
+        }
 
-        if (($ubet == 0 || $ufold == 'F') && $upot == 0)
+        if (($ubet == 0 || $ufold == 'F') && $upot == 0) {
             $bu++;
+        }
 
-        $tkick   = false;
+        $tkick = false;
         $ttq_sql = $addons->get_hooks(
             array(
                 'content' => "SELECT gID, timetag, lastmove, banned FROM " . DB_PLAYERS . " WHERE username = '{$usr}'"
@@ -118,8 +126,9 @@ if ($autoplayer > 0)
         $ttq = $pdo->query($ttq_sql);
         $ttr = $ttq->fetch(PDO::FETCH_ASSOC);
 
-        if ($ttr['timetag'] < $timekick || ($ttr['lastmove'] < $kick && $hand > 5) || ($ttr['banned'] == 1) || ($ttr['gID'] != $gameID))
+        if ($ttr['timetag'] < $timekick || ($ttr['lastmove'] < $kick && $hand > 5) || ($ttr['banned'] == 1) || ($ttr['gID'] != $gameID)) {
             $tkick = true;
+        }
 
         $tkick = $addons->get_hooks(
             array(
@@ -144,7 +153,7 @@ if ($autoplayer > 0)
         );
 
         $kickPlayer = ($busted || $tkick) ? true : false;
-        
+
         $addons->get_hooks(
             array(),
             array(
@@ -153,14 +162,15 @@ if ($autoplayer > 0)
             )
         );
 
-        if (! $kickPlayer)
+        if (!$kickPlayer) {
             continue;
+        }
 
         // Kick Player
         $pdo->query("UPDATE " . DB_POKER . " SET p{$i}name = '', p{$i}bet = '', p{$i}pot = '', lastmove = {$timenow} WHERE gameID = {$gameID}");
         $pdo->query("UPDATE " . DB_PLAYERS . " SET gID = 0 WHERE username = '{$usr}'");
 
-        /* --- Add To Bank */
+        // Add To Bank
         $addToBank = true;
         $addToBank = $addons->get_hooks(
             array(
@@ -168,43 +178,36 @@ if ($autoplayer > 0)
             ),
             array(
                 'page'     => 'includes/auto_move.php',
-                'location'  => 'add_to_bank_bool'
+                'location' => 'add_to_bank_bool'
             )
         );
 
-        if ($addToBank)
-            //$pdo->query("UPDATE " . DB_STATS . " SET winpot = winpot + $upot WHERE player = '{$usr}'");
-            $bankorpoints = ($gamestyle=="p") ? 'points' : 'bank';
+        if ($addToBank) {
+            $bankorpoints = ($gamestyle == "p") ? 'points' : 'bank';
             $pdo->query("UPDATE grpgusers SET $bankorpoints = $bankorpoints + $upot WHERE username = '{$usr}'");
-        /* Add To Bank --- */
+        }
 
-        if ($tkick == true)
-        {
+        if ($tkick == true) {
             poker_log($usr, GAME_MSG_LOST_CONNECTION, $gameID);
 
-            if ($i == $dealer)
-            {
+            if ($i == $dealer) {
                 $nxtdeal = nextdealer($dealer);
 
-                if ($nxtdeal != '')
-                {
+                if ($nxtdeal != '') {
                     $pdo->query("UPDATE " . DB_POKER . " SET lastmove = {$timenow}, dealer = {$nxtdeal} WHERE gameID = {$gameID}");
-                    poker_log( get_name($nxtdeal), GAME_MSG_DEALER_BUTTON, $gameID );
+                    poker_log(get_name($nxtdeal), GAME_MSG_DEALER_BUTTON, $gameID);
                 }
             }
 
-            if ($_SESSION['playername'] == $usr)
-            {
-                $wait    = (int) WAITIMER;
+            if ($_SESSION['playername'] == $usr) {
+                $wait = (int)WAITIMER;
                 $setwait = $time + $wait;
-                $result  = $pdo->query("UPDATE " . DB_PLAYERS . " SET waitimer = {$setwait} WHERE username = '{$plyrname}'");
-                $url     = 'sitout.php';
+                $result = $pdo->query("UPDATE " . DB_PLAYERS . " SET waitimer = {$setwait} WHERE username = '{$plyrname}'");
+                $url = 'sitout.php';
 
                 echo 'parent.document.location.href = "' . $url . '";';
             }
-        }
-        else
-        {
+        } else {
             poker_log($usr, GAME_MSG_PLAYER_BUSTED, $gameID);
             echo $addons->get_hooks(
                 array(
@@ -212,7 +215,7 @@ if ($autoplayer > 0)
                 ),
                 array(
                     'page'     => 'includes/auto_move.php',
-                    'location'  => 'after_player_busted'
+                    'location' => 'after_player_busted'
                 )
             );
         }
@@ -227,42 +230,40 @@ if ($autoplayer > 0)
     }
 
     $checkbets = check_bets();
-    $lastman   = '';
-    $allpl     = $np - $bu;
+    $lastman = '';
+    $allpl = $np - $bu;
 
-    if ($allpl == 1)
+    if ($allpl == 1) {
         $lastman = last_player();
+    }
 
-    $nfpl       = $allpl - $fo;
-    $lipl       = $nfpl - $ai;
+    $nfpl = $allpl - $fo;
+    $lipl = $nfpl - $ai;
     $gamestatus = 'live';
 
-    if ($interplay)
-    {
-        if ($nfpl == 1 && $allpl > 1)
+    if ($interplay) {
+        if ($nfpl == 1 && $allpl > 1) {
             $gamestatus = 'allfold';
+        }
 
-        if ($lipl < 2 && $allpl > 1 && $checkbets == true && $ai > 0)
+        if ($lipl < 2 && $allpl > 1 && $checkbets == true && $ai > 0) {
             $gamestatus = 'showdown';
-    }
-    else
-    {
-        if ($nfpl == 1 && $allpl > 1)
+        }
+    } else {
+        if ($nfpl == 1 && $allpl > 1) {
             $showshort = true;
+        }
     }
 
-    if ($allpl == 1 && $hand >= 0)
-    {
+    if ($allpl == 1 && $hand >= 0) {
         $winpot = $tpr["p{$lastman}pot"] + $tablepot;
-        $logger  = '';
-        $msg    = GAME_MSG_PLAYERS_JOINING;
+        $logger = '';
+        $msg = GAME_MSG_PLAYERS_JOINING;
 
-        if ($tabletype === 't' && $allpl == 1)
-        {
-            if ($autoname != '')
-            {
+        if ($tabletype === 't' && $allpl == 1) {
+            if ($autoname != '') {
                 $logger = $autoname;
-                $msg    = GAME_MSG_WON_TOURNAMENT;
+                $msg = GAME_MSG_WON_TOURNAMENT;
             }
 
             $pdo->query("UPDATE " . DB_STATS . " SET tournamentswon = tournamentswon + 1 WHERE player  = '{$autoname}'");
@@ -274,7 +275,7 @@ if ($autoplayer > 0)
             ),
             array(
                 'page'     => 'includes/auto_move.php',
-                'location'  => 'log_message_1player'
+                'location' => 'log_message_1player'
             )
         );
 
@@ -285,47 +286,47 @@ if ($autoplayer > 0)
         die();
     }
 
-    if ($autoname == '' || $autostatus == 'bust' && $allpl > 1)
-    {
+    if ($autoname == '' || $autostatus == 'bust' && $allpl > 1) {
         $pdo->query("UPDATE " . DB_POKER . " SET move = {$nextup}, lastmove = $timenow WHERE gameID = " . $gameID);
         die();
     }
 
-    $checkpass    = ($autoplayer == last_bet() && $checkbets == true && $gamestatus == 'live') ? true : false;
+    $checkpass = ($autoplayer == last_bet() && $checkbets == true && $gamestatus == 'live') ? true : false;
     $postShowdown = ($hand === 0 || (in_array($hand, array(5, 7, 9, 11)) && $checkpass)) ? true : false;
-    $preShowdown  = ($postShowdown) ? false : true;
+    $preShowdown = ($postShowdown) ? false : true;
 
-    if ($preShowdown)
+    if ($preShowdown) {
         include 'playtime/showdown.php';
+    }
 }
 
 // Timers
-if ($interplay) // when playes can perform actions such as bet/check/fold
+if ($interplay) {
+    // when players can perform actions such as bet/check/fold
     require 'playtime/timers/interplay.php';
-else
+} else {
     require 'playtime/timers/non-interplay.php';
-
+}
 
 // Hands
 require "playtime/hands/hand{$hand}.php";
 
-if ($postShowdown)
+if ($postShowdown) {
     include 'playtime/showdown.php';
+}
 
 die();
-
 
 // Page End
 echo $addons->get_hooks(
     array(),
     array(
         'page'     => 'includes/auto_move.php',
-        'location'  => 'page_end'
+        'location' => 'page_end'
     )
 );
 
-function hand_hook()
-{
+function hand_hook() {
     global $addons, $hand;
     $addons->get_hooks(
         array(
@@ -333,7 +334,7 @@ function hand_hook()
         ),
         array(
             'page'     => 'includes/auto_move.php',
-            'location'  => 'hand_change'
+            'location' => 'hand_change'
         )
     );
 }
