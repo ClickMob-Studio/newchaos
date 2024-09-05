fb47c9ed64b591a0f2d938660715ee12c59890a9
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-09-05 11:56:28 +0100

    Update gen_inc.php

--- includes/gen_inc.php
+++ includes/gen_inc.php
@@ -16,7 +16,7 @@ if (session_status() == PHP_SESSION_NONE) {
 
 // Include necessary files
 require_once '../dbcon.php';
-require_once '../includes/tables.php';
+//require_once '../includes/tables.php';
 require_once '../includes/settings.php';
 
 /* THEME */
