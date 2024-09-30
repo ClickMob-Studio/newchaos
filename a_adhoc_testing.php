d5076e23b63662a36c752f9a0c080412f15bd517
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-09-30 16:46:47 +0300

    u

--- a_adhoc_testing.php
+++ a_adhoc_testing.php
@@ -3,24 +3,8 @@
 include 'header.php';
 
 
-$durl = "https://chaoscity.co.uk/ajax_supergym.php?au_user_or=" . $user_class->id;
+$db->query("SELECT id FROM grpgusers WHERE is_auto_user = 1 ORDER BY RAND() LIMIT 2");
+$db->execute();
+$rows = $db->fetch_row();
 
-$stats = array('strength', 'defense', 'speed', 'agility');
-$stat = $stats[mt_rand(0, 3)];
-
-$i = 1;
-while ($i < 50) {
-
-    $ch =  curl_init();
-    curl_setopt($ch,CURLOPT_URL, $durl);
-    curl_setopt ($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt ($ch, CURLOPT_FAILONERROR, 1);
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS,
-        "amnt=" . $user_class->maxenergy . "&stat=" . $stat . '&what=trainrefill&mega_train=no&multiplier=10');
-    $dinf = curl_exec ($ch);
-    var_dump($dinf);
-
-    $i++;
-}
+var_dump($rows); exit;
