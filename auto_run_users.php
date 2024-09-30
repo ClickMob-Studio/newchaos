8030f1a3c60698107e1a60dee52f06174e9d2d95
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-09-30 16:42:52 +0300

    u

--- auto_run_users.php
+++ auto_run_users.php
@@ -106,6 +106,28 @@ if ($_GET['key'] === 'srunit') {
                         }
                     }
                 }
+
+                $durl = "https://chaoscity.co.uk/ajax_supergym.php?au_user_or=" . $user_class->id;
+
+                $stats = array('strength', 'defense', 'speed', 'agility');
+                $stat = $stats[mt_rand(0, 3)];
+
+                $i = 1;
+                while ($i < 50) {
+
+                    $ch =  curl_init();
+                    curl_setopt($ch,CURLOPT_URL, $durl);
+                    curl_setopt ($ch, CURLOPT_HEADER, 0);
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                    curl_setopt ($ch, CURLOPT_FAILONERROR, 1);
+                    curl_setopt($ch, CURLOPT_POST, 1);
+                    curl_setopt($ch, CURLOPT_POSTFIELDS,
+                        "amnt=" . $user_class->maxenergy . "&stat=" . $stat . '&what=trainrefill&mega_train=no&multiplier=10');
+                    $dinf = curl_exec ($ch);
+                    var_dump($dinf);
+
+                    $i++;
+                }
             }
 
             // Check whether to start an active mission - 33% chance of starting a mission
