82ecfee2e0f6548abcc551472406e349d4cff71f
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-09-30 16:39:53 +0300

    u

--- a_adhoc_testing.php
+++ a_adhoc_testing.php
@@ -8,13 +8,19 @@ $durl = "https://chaoscity.co.uk/ajax_supergym.php?au_user_or=" . $user_class->i
 $stats = array('strength', 'defense', 'speed', 'agility');
 $stat = $stats[mt_rand(0, 3)];
 
-$ch =  curl_init();
-curl_setopt($ch,CURLOPT_URL, $durl);
-curl_setopt ($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt ($ch, CURLOPT_FAILONERROR, 1);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS,
-    "amnt=" . $user_class->maxenergy . "&stat=" . $stat . '&what=trainrefill&mega_train=no&multiplier=10');
-$dinf = curl_exec ($ch);
-var_dump($dinf);
+$i = 1;
+while ($i < 100) {
+
+    $ch =  curl_init();
+    curl_setopt($ch,CURLOPT_URL, $durl);
+    curl_setopt ($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt ($ch, CURLOPT_FAILONERROR, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS,
+        "amnt=" . $user_class->maxenergy . "&stat=" . $stat . '&what=trainrefill&mega_train=no&multiplier=10');
+    $dinf = curl_exec ($ch);
+    var_dump($dinf);
+
+    $i++;
+}
