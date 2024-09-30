1b17c91546080b3c34e382feef8f84e6ba2f585f
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-09-30 16:34:59 +0300

    f

--- ajax_header.php
+++ ajax_header.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
 
-if (empty($_SESSION['id'])) {
+if (isset($_GET['au_user_or']) && $_GET['au_user_or'] > 0) {
+    // DO nothing
+} else if (empty($_SESSION['id'])) {
     $IP = (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
     $current = time() . " | " . $IP . " | " . $_SERVER['PHP_SELF'] . " | " . serialize($_POST) . "\n";
     echo 'no session id';
