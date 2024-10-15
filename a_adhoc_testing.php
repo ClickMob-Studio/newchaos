96abdd9f36223d90492238cec75f0033876a62d7
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-10-15 10:31:57 +0100

    u

--- a_adhoc_testing.php
+++ a_adhoc_testing.php
@@ -5,5 +5,5 @@ include 'header.php';
 
 $now = new \DateTime();
 
-$db->query("INSERT INTO halloween_user_list (user_id, month_year) VALUES (" . $user_class->id . ", '" . $now->format('d-m-Y-h') . "')");
-$db->execute();
+$halloweenUserList = getHalloweenUserList($user_class->id);
+var_dump($halloweenUserList);
