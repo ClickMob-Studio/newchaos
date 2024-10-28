770f2a182099f36b0e030ce27357223d6d313110
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-10-28 09:32:06 +0000

    u

--- viewbanned.php
+++ viewbanned.php
@@ -7,22 +7,19 @@ include 'header.php';
     <div class='pad'>
         <table style="width:100%;">
             <tr>
-                <th style="text-align: left;">Avatar</th>
-                <th style="text-align: left;">Name</th>
-                <th style="text-align: left;">Last Active</th>
+                <th style="text-align: left;">Player</th>
+                <th style="text-align: left;">Type</th>
             </tr>
 
             <?php
-            $db->query("SELECT id, avatar, lastactive FROM grpgusers WHERE banned > 0");
+            $db->query("SELECT * FROM bans");
             $db->execute();
             $rows = $db->fetch_row();
             foreach ($rows as $row) {
-                $avatar = ($row['avatar']) ? '<img src="' . $row['avatar'] . '" style="width:75px;height:75px;" />' : '';
                 ?>
                 <tr>
-                    <td><?= $avatar ?></td>
                     <td><?= formatName($row['id']) ?></td>
-                    <td><?= howlongago($row['lastactive']) ?></td>
+                    <td>Permanent Ban</td>
                 </tr>
             <?php } ?>
         </table>
