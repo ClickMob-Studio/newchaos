3161591a62e7d35e79076df1791624fdf183079e
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-10-29 15:16:14 +0000

    u

--- quest.php
+++ quest.php
@@ -11,8 +11,10 @@ if ($questSeasonUser) {
 
 ?>
 
-<h1><?php echo $currentQuestSeason['name'] ?>: <?php echo $questSeasonMission['name'] ?></h1><hr />
+<h1>Quest: <?php echo $currentQuestSeason['name'] ?></h1><hr />
+<p><?php echo $currentQuestSeason['description'] ?></p>
 
+<h2>Mission: <?php echo $questSeasonMission['name'] ?></h2>
 <p><?php echo $questSeasonMission['description'] ?></p>
 
 <div class="row">
