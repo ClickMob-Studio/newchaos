e6e1710f711fe7de78867fe867d2e396ae57e169
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-10-29 14:40:00 +0000

    u

--- quest.php
+++ quest.php
@@ -3,7 +3,25 @@
 require "header.php";
 
 $currentQuestSeason = getCurrentQuestSeasonForUser($user_class);
-var_dump($currentQuestSeason);
 $questSeasonUser = getQuestSeasonUser($user_class->id, $currentQuestSeason['id']);
 
-var_dump($questSeasonUser);
+if ($questSeasonUser) {
+
+?>
+
+<h1><?php echo $currentQuestSeason['name'] ?>: </h1><hr />
+
+<p>
+    Welcome to operations, complete activities to earn rewards with each category hosting 7 levels of challenges! Operations run monthly and reset for you
+    to complete on the first day of each month.
+</p>
+
+<div class="row">
+    <div class="col-md-12">
+    </div>
+</div>
+<?php
+
+} else {
+
+}
