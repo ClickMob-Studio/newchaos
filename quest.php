16516ddff8d4d24b870397f8d6e12e73c76c8fe2
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-10-29 13:31:05 +0000

    u

--- quest.php
+++ quest.php
@@ -3,6 +3,7 @@
 require "header.php";
 
 $currentQuestSeason = getCurrentQuestSeasonForUser($user_class);
+var_dump($currentQuestSeason);
 $questSeasonUser = getQuestSeasonUser($user_class->id, $currentQuestSeason['id']);
 
 var_dump($questSeasonUser);
