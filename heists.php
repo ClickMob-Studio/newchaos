739ddc40575b5da75a175422fc2321e8365f26c5
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-11-10 13:49:29 +0000

    Update heists.php

--- heists.php
+++ heists.php
@@ -1,9 +1,7 @@
 <?php
 
 include_once "header.php";
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 if ($user_class->jail > 0) {
     diefun("You cannot access this page while in jail");
 }
