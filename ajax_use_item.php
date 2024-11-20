a2cd95861b929e5107eabf73b347f9ee6caa3e63
codemonkey2704 <55356484+codemonkey2704@users.noreply.github.com>
2024-11-20 11:29:28 +0000

    Update ajax_use_item.php

--- ajax_use_item.php
+++ ajax_use_item.php
@@ -165,6 +165,10 @@ if (isset($_GET['use'])) {
                     $db->execute(array($newhosp, $hp, $user_class->id));
                 
                     // Send success response
+                    if (ob_get_length()) {
+                        ob_clean();
+                    }
+                    header('Content-Type: application/json');
                     $response['success'] = true;
                     $response['message'] = "You successfully used a Med Cert.";
                     echo json_encode($response);
