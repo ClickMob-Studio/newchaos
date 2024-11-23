930cdc247260f840ed2b35fb85dada765bdbc316
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-11-23 10:35:43 +0000

    u

--- ajax_crimes2.php
+++ ajax_crimes2.php
@@ -494,9 +494,9 @@ if (isset($_POST['id']) || isset($input['id'])) {
                     $questSeasonMissionUser = getQuestSeasonMissionUser($user_class->id, $currentQuestSeason['id']);
                     $questSeasonMission = getQuestSeasonMission($user_class->id, $currentQuestSeason['id']);
 
-//                    if (isset($questSeasonMission['requirements']->crime_cash)) {
-//                        updateQuestSeasonMissionUserProgress($questSeasonMissionUser, 'crime_cash', $money);
-//                    }
+                    if (isset($questSeasonMission['requirements']->crime_cash)) {
+                        updateQuestSeasonMissionUserProgress($questSeasonMissionUser, 'crime_cash', $money);
+                    }
                 }
             }
 
