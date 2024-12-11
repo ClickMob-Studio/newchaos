b9f9401341d2fe81275577be323c7be7d17e799c
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-12-11 10:22:36 +0000

    u

--- santasgrotto.php
+++ santasgrotto.php
@@ -11,6 +11,24 @@ include 'header.php';
         <div class="row">
             <div class="col-md-12">
                 <img src="css/images/NewGameImages/santas-grotto.jpeg" class="img-responsive" style="width:100%;height:auto;max-width:100%;max-height:100%;"/>
+
+                <center>
+                    <p>
+                        Welcome to Santa's Grotto. Being in the Mafia isn't just about killing, committing crimes and earning the big bucks. We've got to let
+                        our community know that we support them to bring the future mafia bosses into the world. Santa's Grotto is a place where you can donate
+                        Christmas Gifts to the community and in return you'll be given a random gift back. The more you donate, the more you'll receive in return.
+                    </p>
+
+                    <div class="row">
+                        <div class="col-md-4"></div>
+                        <div class="col-md-4">
+                            <div class="progress">
+                                <div id="progress-bar" class="progress-bar progress-bar-blue" role="progressbar" style="width: 50%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                        <div class="col-md-4"></div>
+                    </div>
+                </center>
             </div>
         </div>
     </div>
