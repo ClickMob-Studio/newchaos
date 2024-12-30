d08bd9a70749be2267618704489a699c4f1d33f5
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2024-12-30 13:21:22 +0000

    u

--- header.php
+++ header.php
@@ -1575,7 +1575,7 @@ if ($bonus_row['Time'] > 0) {
 //$messages[] = '<a href="ucl_contest.php"><font color=red>Activity Contest: Complete ' . ucfirst($activityContest['type']) . ' </font></a>';
 //$messages[] = '<a href="ucl_raids_contest.php"><font color=red>Raid Contest</font></a>';
 //$messages[] = '<a href="adventcalendar.php"><font color=red>Advent Calendar</font></a>';
-$messages[] = '<a href="santasgrotto.php"><font color=red>Santas Grotto</font></a>';
+//$messages[] = '<a href="santasgrotto.php"><font color=red>Santas Grotto</font></a>';
 
 //if ($user_class->gang > 0) {
 //    $messages[] = '<a href="gang_doubleexp.php"><font color=red>Gang Challenge</font></a>';
