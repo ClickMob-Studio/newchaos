eaec8ef6761d6a532f08f6004801687c68cd8a04
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2025-01-07 15:24:40 +0000

    u

--- quest/follow_salvatore.php
+++ quest/follow_salvatore.php
@@ -12,11 +12,11 @@ if ($user_class->jail > 0 || $user_class->hospital > 0) {
 
 <div class="contenthead floaty" style="text-align: center; padding: 20px; margin-bottom: 20px; border-radius: 8px; width: 88%;">
     <br>
-    <button class="direction-button" href="#">North</button><br><br>
-    <button class="direction-button" href="#">West</button>
-    <button class="direction-button" href="#">Search</button>
-    <button class="direction-button" href="#">East</button><br><br>
-    <button class="direction-button" href="#">South</button>
+    <button class="direction-button" href="?mode=follow_salvatore&follow_salvatore=north">North</button><br><br>
+    <button class="direction-button" href="?mode=follow_salvatore&follow_salvatore=west">West</button>
+    <button class="direction-button" href="#" onclick="return false;">Search</button>
+    <button class="direction-button" href="?mode=follow_salvatore&follow_salvatore=east">East</button><br><br>
+    <button class="direction-button" href="?mode=follow_salvatore&follow_salvatore=south">South</button>
 </div>
 
 <?php
