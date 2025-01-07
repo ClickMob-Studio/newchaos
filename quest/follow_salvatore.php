c1bfcf34a88da7c53278028ad512ac4dada70461
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2025-01-07 14:47:01 +0000

    u

--- quest/follow_salvatore.php
+++ quest/follow_salvatore.php
@@ -9,3 +9,64 @@ if ($user_class->jail > 0 || $user_class->hospital > 0) {
 }
 ?>
 <h1>Follow Salvatore</h1><hr />
+
+<canvas id="gameCanvas" width="800" height="600"></canvas>
+
+
+<script type="text/javascript">
+    const canvas = document.getElementById('gameCanvas');
+    const ctx = canvas.getContext('2d');
+
+    const player = {
+        x: 50,
+        y: 50,
+        width: 50,
+        height: 50,
+        color: 'blue',
+        speed: 5
+    };
+
+    const salvatore = {
+        x: 200,
+        y: 200,
+        width: 50,
+        height: 50,
+        color: 'red'
+    };
+
+    function drawCharacter(character) {
+        ctx.fillStyle = character.color;
+        ctx.fillRect(character.x, character.y, character.width, character.height);
+    }
+
+    function clearCanvas() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    function update() {
+        clearCanvas();
+        drawCharacter(player);
+        drawCharacter(salvatore);
+        requestAnimationFrame(update);
+    }
+
+    function movePlayer(event) {
+        switch (event.key) {
+            case 'ArrowUp':
+                player.y -= player.speed;
+                break;
+            case 'ArrowDown':
+                player.y += player.speed;
+                break;
+            case 'ArrowLeft':
+                player.x -= player.speed;
+                break;
+            case 'ArrowRight':
+                player.x += player.speed;
+                break;
+        }
+    }
+
+    document.addEventListener('keydown', movePlayer);
+    update();
+</script>
