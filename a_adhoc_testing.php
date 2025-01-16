794b224c543f35c1be2470e61cb7712329687025
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2025-01-16 15:17:19 +0000

    u

--- a_adhoc_testing.php
+++ a_adhoc_testing.php
@@ -1,22 +1,22 @@
 <?php
-
-include 'header.php';
-
-
-$db->query("SELECT * FROM grpgusers WHERE mission_count = 0 LIMIT 100");
-$rows = $db->fetch_row();
-
-foreach ($rows as $row) {
-    $missionsQ = mysql_query("SELECT COUNT(id) AS mission_count FROM missions WHERE userid = " . $row['id'] . " AND completed = 'successful'");
-    $missionsR = mysql_fetch_assoc($missionsQ);
-    $missionsCount = $missionsR['mission_count'];
-
-    if ($missionsCount < 1) {
-        $missionsCount = 1;
-    }
-
-    $db->query("UPDATE grpgusers SET mission_count = " . $missionsCount . " WHERE id = " . $row['id']);
-    $db->execute();
-
-    echo 'Updated ' . $row['id'] . ' to ' . $missionsCount . ' missions<br>';
-}
+//
+//include 'header.php';
+//
+//
+//$db->query("SELECT * FROM grpgusers WHERE mission_count = 0 LIMIT 100");
+//$rows = $db->fetch_row();
+//
+//foreach ($rows as $row) {
+//    $missionsQ = mysql_query("SELECT COUNT(id) AS mission_count FROM missions WHERE userid = " . $row['id'] . " AND completed = 'successful'");
+//    $missionsR = mysql_fetch_assoc($missionsQ);
+//    $missionsCount = $missionsR['mission_count'];
+//
+//    if ($missionsCount < 1) {
+//        $missionsCount = 1;
+//    }
+//
+//    $db->query("UPDATE grpgusers SET mission_count = " . $missionsCount . " WHERE id = " . $row['id']);
+//    $db->execute();
+//
+//    echo 'Updated ' . $row['id'] . ' to ' . $missionsCount . ' missions<br>';
+//}
