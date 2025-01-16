749e72e59aa77b2e9af4ef0a85b9a14dc9e3fbe3
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2025-01-16 15:13:19 +0000

    u

--- a_adhoc_testing.php
+++ a_adhoc_testing.php
@@ -2,70 +2,16 @@
 
 include 'header.php';
 
-$prizes = array();
-$prizes[1] = 3000;
-$prizes[2] = 1500;
-$prizes[3] = 500;
 
-$db->query("SELECT * FROM petladder WHERE attacks > 0 ORDER BY attacks DESC LIMIT 3");
+$db->query("SELECT * FROM grpgusers WHERE mission_count < 1 LIMIT 10");
 $rows = $db->fetch_row();
 
-$i = 1;
 foreach ($rows as $row) {
-    $pet = $db->query("SELECT * FROM pets WHERE id = " . $row['pet_id'] . " LIMIT 1");
-    $pet = $db->fetch_row(true);
+    $missionsQ = mysql_query("SELECT COUNT(id) AS mission_count FROM missions WHERE userid = " . $row['id'] . " AND completed = 'successful'");
+    $missionsR = mysql_fetch_assoc($missionsQ);
+    $missionsCount = $missionsR['mission_count'];
 
-    if (isset($prizes[$i])) {
-        $prize = $prizes[$i];
+    $db->query("UPDATE grpgusers SET mission_count = " . $missionsCount . " WHERE id = " . $row['id']);
 
-        $db->query("UPDATE grpgusers SET points = points + " . $prize . " WHERE id = " . $pet['userid']);
-        $db->execute();
-
-        Send_Event($pet['userid'], "You have won " . $prize . " points for being in spot " . $i . " of the pet ladder for attacks.");
-    }
-
-    $i++;
+    echo 'Updated ' . $row['id'] . ' to ' . $missionsCount . ' missions<br>';
 }
-
-$db->query("SELECT * FROM petladder WHERE gym > 0 ORDER BY gym DESC LIMIT 3");
-$rows = $db->fetch_row();
-
-$i = 1;
-foreach ($rows as $row) {
-    $pet = $db->query("SELECT * FROM pets WHERE id = " . $row['pet_id'] . " LIMIT 1");
-    $pet = $db->fetch_row(true);
-
-    if (isset($prizes[$i])) {
-        $prize = $prizes[$i];
-
-        $db->query("UPDATE grpgusers SET points = points + " . $prize . " WHERE id = " . $pet['userid']);
-        $db->execute();
-
-        Send_Event($pet['userid'], "You have won " . $prize . " points for being in spot " . $i . " of the pet ladder for Gym.");
-    }
-
-    $i++;
-}
-
-$db->query("SELECT * FROM petladder WHERE exp > 0 ORDER BY exp DESC LIMIT 3");
-$rows = $db->fetch_row();
-
-$i = 1;
-foreach ($rows as $row) {
-    $pet = $db->query("SELECT * FROM pets WHERE id = " . $row['pet_id'] . " LIMIT 1");
-    $pet = $db->fetch_row(true);
-
-    if (isset($prizes[$i])) {
-        $prize = $prizes[$i];
-
-        $db->query("UPDATE grpgusers SET points = points + " . $prize . " WHERE id = " . $pet['userid']);
-        $db->execute();
-
-        Send_Event($pet['userid'], "You have won " . $prize . " points for being in spot " . $i . " of the pet ladder for Crime EXP.");
-    }
-
-    $i++;
-}
-
-$db->query("UPDATE petladder SET exp = 0, gym = 0, attacks = 0");
-$db->execute();
