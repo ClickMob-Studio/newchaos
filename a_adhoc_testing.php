0e4f71a628bc3539f426595ea3b4d90534f86e8d
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2025-01-16 15:14:55 +0000

    u

--- a_adhoc_testing.php
+++ a_adhoc_testing.php
@@ -5,6 +5,7 @@ include 'header.php';
 
 $db->query("SELECT * FROM grpgusers WHERE mission_count = 0 LIMIT 10");
 $rows = $db->fetch_row();
+var_dump($rows);
 
 foreach ($rows as $row) {
     $missionsQ = mysql_query("SELECT COUNT(id) AS mission_count FROM missions WHERE userid = " . $row['id'] . " AND completed = 'successful'");
@@ -12,6 +13,7 @@ foreach ($rows as $row) {
     $missionsCount = $missionsR['mission_count'];
 
     $db->query("UPDATE grpgusers SET mission_count = " . $missionsCount . " WHERE id = " . $row['id']);
+    $db->execute();
 
     echo 'Updated ' . $row['id'] . ' to ' . $missionsCount . ' missions<br>';
 }
