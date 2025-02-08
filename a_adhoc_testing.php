72c7a34e07ee36738a186c2d0be9d610ac396e81
aaronnewton12 <63728574+aaronnewton12@users.noreply.github.com>
2025-02-08 13:31:16 +0000

    rel_comp_leaderboard population

--- a_adhoc_testing.php
+++ a_adhoc_testing.php
@@ -1,38 +1,18 @@
 <?php
-exit;
+
 include 'header.php';
 
 
-$db->query("SELECT * FROM gang_territory_zone_battle WHERE is_complete > 0");
+$db->query("SELECT * FROM gprgusers WHERE relationship > 0 AND relplayer > 0");
 $rows = $db->fetch_row();
 
+$usersComplete = array();
 foreach ($rows as $row) {
-    if ($row['winning_gang_id'] == $row['attacking_gang_id']) {
-        if ($row['strength_attacking_user_id'] > 0) {
-            $db->query("UPDATE grpgusers SET gtzb_count = gtzb_count + 1 WHERE id = " . $row['strength_attacking_user_id']);
-            $db->execute();
-        }
-        if ($row['speed_attacking_user_id'] > 0) {
-            $db->query("UPDATE grpgusers SET gtzb_count = gtzb_count + 1 WHERE id = " . $row['speed_attacking_user_id']);
-            $db->execute();
-        }
-        if ($row['defense_attacking_user_id'] > 0) {
-            $db->query("UPDATE grpgusers SET gtzb_count = gtzb_count + 1 WHERE id = " . $row['defense_attacking_user_id']);
-            $db->execute();
-        }
-    } else {
-        if ($row['strength_defending_user_id'] > 0) {
-            $db->query("UPDATE grpgusers SET gtzb_count = gtzb_count + 1 WHERE id = " . $row['strength_defending_user_id']);
-            $db->execute();
-        }
-        if ($row['speed_defending_user_id'] > 0) {
-            $db->query("UPDATE grpgusers SET gtzb_count = gtzb_count + 1 WHERE id = " . $row['speed_defending_user_id']);
-            $db->execute();
-        }
-        if ($row['defense_defending_user_id'] > 0) {
-            $db->query("UPDATE grpgusers SET gtzb_count = gtzb_count + 1 WHERE id = " . $row['defense_defending_user_id']);
-            $db->execute();
-        }
-    }
+    if (!in_array($row['id'], $usersComplete)) {
+        $usersComplete[] = $row['id'];
+        $usersComplete[] = $row['relplayer'];
 
+        $db->query("INSERT INTO rel_comp_leaderboard (user_id, two_user_id) VALUES (" . $row['id'] . ", " . $row['relplayer'] . ")");
+        $db->execute();
+    }
 }
