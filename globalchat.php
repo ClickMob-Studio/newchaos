87d010ad4fb8195f351eb14b36c872020bd6aad8
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-04-04 02:37:07 +0200

    fix: add img tag

--- globalchat.php
+++ globalchat.php
@@ -1,17 +1,17 @@
-    <?php
+<?php
 include 'header.php';
 ?>
 <div class='box_top'>Global chat</div>
-						<div class='box_middle'>
-							<div class='pad'>
-<?php
+<div class='box_middle'>
+    <div class='pad'>
+        <?php
 
-if ($user_class->fbitime > 0) {
-    diefun("You can't communicate if you're in FBI Jail!");
-}
+        if ($user_class->fbitime > 0) {
+            diefun("You can't communicate if you're in FBI Jail!");
+        }
 
 
-    echo "
+        echo "
     <script>
     function addBB(text) {
         var textarea = document.getElementById('reply');
@@ -50,54 +50,54 @@ picker.on('emoji', selection => {
 trigger.addEventListener('click', () => picker.togglePicker(trigger));
 </script>";
 
-if(isset($_GET['gcban']) && $_GET['conf'] == $_SESSION['security']){
-    if($user_class->admin || $user_class->gm || $user_class->cm){
-        $db->query("SELECT id FROM grpgusers WHERE id = ? AND admin = 0 AND gm = 0");
-        $db->execute(array(
-            $_GET['gcban']
-        ));
-        $id = $db->fetch_single();
-        if(empty($id) || $id <= 0)
-            diefun("Invalid id number. Ban failed.");
-        $db->query("INSERT INTO bans VALUES('', ?, ?, ?, ?)");
-        $db->execute(array(
-            $id,
-            $user_class->id,
-            'gc',
-            60
-        ));
-        diefun(formatName($id). " has been banned for 60 minutes. If the user needs a more severe punishment, use a mail ban.");
-    }
-}
-if(isset($_GET['delgc'])){
-    if($user_class->admin || $user_class->gm || $user_class->cm){
-        $db->query("DELETE FROM globalchat WHERE id = ?");
-        $db->execute(array(
-            $_GET['delgc']
-        ));
-    }
-}
+        if (isset($_GET['gcban']) && $_GET['conf'] == $_SESSION['security']) {
+            if ($user_class->admin || $user_class->gm || $user_class->cm) {
+                $db->query("SELECT id FROM grpgusers WHERE id = ? AND admin = 0 AND gm = 0");
+                $db->execute(array(
+                    $_GET['gcban']
+                ));
+                $id = $db->fetch_single();
+                if (empty($id) || $id <= 0)
+                    diefun("Invalid id number. Ban failed.");
+                $db->query("INSERT INTO bans VALUES('', ?, ?, ?, ?)");
+                $db->execute(array(
+                    $id,
+                    $user_class->id,
+                    'gc',
+                    60
+                ));
+                diefun(formatName($id) . " has been banned for 60 minutes. If the user needs a more severe punishment, use a mail ban.");
+            }
+        }
+        if (isset($_GET['delgc'])) {
+            if ($user_class->admin || $user_class->gm || $user_class->cm) {
+                $db->query("DELETE FROM globalchat WHERE id = ?");
+                $db->execute(array(
+                    $_GET['delgc']
+                ));
+            }
+        }
 
-$db->query("SELECT days FROM bans WHERE id = ? AND type = 'gc'");
-$db->execute(array(
-    $user_class->id
-));
-if($mins = $db->fetch_single())
-    diefun("You are banned from global chat for $mins minutes.");
-$_SESSION['security'] = rand(1000000000,2000000000);
-$db->query("SELECT id FROM globalchat ORDER BY id DESC");
-$db->execute();
-$lastid = $db->fetch_row(true);
-$db->query("SELECT * FROM bans WHERE `type` = 'mail' AND id = $user_class->id");
-$db->execute();
-if ($db->num_rows()) {
-    $r = $db->fetch_row(true);
-    diefun("&nbsp;You have been mail banned for " . prettynum($r['days']) . " days.");
-}
-if ($user_class->level < 2 && $user_class->prestige == 0)
-    diefun("You must be level 2 to use this feature.");
-$lastid = (!empty($lastid['id'])) ? $lastid['id'] : 0;
-print <<<TEXT
+        $db->query("SELECT days FROM bans WHERE id = ? AND type = 'gc'");
+        $db->execute(array(
+            $user_class->id
+        ));
+        if ($mins = $db->fetch_single())
+            diefun("You are banned from global chat for $mins minutes.");
+        $_SESSION['security'] = rand(1000000000, 2000000000);
+        $db->query("SELECT id FROM globalchat ORDER BY id DESC");
+        $db->execute();
+        $lastid = $db->fetch_row(true);
+        $db->query("SELECT * FROM bans WHERE `type` = 'mail' AND id = $user_class->id");
+        $db->execute();
+        if ($db->num_rows()) {
+            $r = $db->fetch_row(true);
+            diefun("&nbsp;You have been mail banned for " . prettynum($r['days']) . " days.");
+        }
+        if ($user_class->level < 2 && $user_class->prestige == 0)
+            diefun("You must be level 2 to use this feature.");
+        $lastid = (!empty($lastid['id'])) ? $lastid['id'] : 0;
+        print <<<TEXT
 <script>
 var lastGmailID = $lastid;
 syncGmail();
@@ -140,75 +140,81 @@ document.addEventListener('DOMContentLoaded', function () {
 </script>
 
 TEXT;
-echo'
+        echo '
     <div id="gccontainer" style="margin:0; padding:10px;">
         ' . gcTalking() . '
    </div>';
-?>
-<style>
-    .flexele{
-        flex: 1;
-        text-align:center;
-        padding:10px;
-    }
-    .flexcont{
-        text-align: center;
-    }
-    </style>
+        ?>
+        <style>
+            .flexele {
+                flex: 1;
+                text-align: center;
+                padding: 10px;
+            }
 
-<table style="margin-bottom:-10px;">
-<tr>
-    <td class="flexcont">
-        <span class="flexele forumhover" onclick="addBB('[b][/b]', 4);return false;">[b]</span>
-    </td>
-    <td class="flexcont">
-        <span class="flexele forumhover" onclick="addBB('[u][/u]', 4);return false;">[u]</span>
-    </td>
-    <td class="flexcont">
-        <span class="flexele forumhover" onclick="addBB('[i][/i]', 4);return false;">[i]</span>
-    </td>
-    <td class="flexcont">
-        <span class="flexele forumhover" onclick="addBB('[s][/s]', 4);return false;">[s]</span>
-    </td>
-    <td class="flexcont">
-        <span class="flexele forumhover" onclick="addBB('[url][/url]', 6);return false;">[url]</span>
-    </td>
-    <td class="flexcont">
-        <span class="flexele forumhover" onclick="v('[img][/img]', 6);return false;">[img]</span>
-    </td>
-    <td class="flexcont">
-        <span class="flexele forumhover" onclick="addBB('[tag][/tag]', 6);return false;">[tag]</span>
-    </td>
-    <td class="flexcont">
-        <span class="flexele forumhover" onclick="addBB('[youtube][/youtube]', 10);return false;">[youtube]</span>
-    </td>
-    <td class="flexcont">
-        <span id="semojis" class="forumhover" onclick="return showemojis();" style="display:<?php echo ($user_class->hideemojis) ? 'block' : 'none'; ?>;flex:2;">Show Emojis</span>
-    </td>
-    <td class="flexcont">
-        <span id="hemojis" class="forumhover" onclick="return hideemojis();" style="display:<?php echo ($user_class->hideemojis) ? 'none' : 'block'; ?>;flex:2;">Hide Emojis</span>
-    </td>
-</tr>
-</table>
-<?php
-    echo'<hr style="border:0;border-top:thin solid #333;" />';
-    echo'<table><form name="message">';
-        echo'<tr><td><textarea autofocus name="msgtext" id="reply" oninput="typing();" style="width:90%;height:125px;"></textarea><br /></td>';
-        echo'<td><input type="submit" name="submit" onclick="return sendGmail();" value="Post" /></td></tr>';
-    echo'</form></table>';
-?>
-    <?php
-    echo'<div id="emojis" style="display:' , ($user_class->hideemojis) ? 'none' : 'block' , ';">';
+            .flexcont {
+                text-align: center;
+            }
+        </style>
+
+        <table style="margin-bottom:-10px;">
+            <tr>
+                <td class="flexcont">
+                    <span class="flexele forumhover" onclick="addBB('[b][/b]', 4);return false;">[b]</span>
+                </td>
+                <td class="flexcont">
+                    <span class="flexele forumhover" onclick="addBB('[u][/u]', 4);return false;">[u]</span>
+                </td>
+                <td class="flexcont">
+                    <span class="flexele forumhover" onclick="addBB('[i][/i]', 4);return false;">[i]</span>
+                </td>
+                <td class="flexcont">
+                    <span class="flexele forumhover" onclick="addBB('[s][/s]', 4);return false;">[s]</span>
+                </td>
+                <td class="flexcont">
+                    <span class="flexele forumhover" onclick="addBB('[url][/url]', 6);return false;">[url]</span>
+                </td>
+                <td class="flexcont">
+                    <span class="flexele forumhover" onclick="addBB('[img][/img]', 6);return false;">[img]</span>
+                </td>
+                <td class="flexcont">
+                    <span class="flexele forumhover" onclick="addBB('[tag][/tag]', 6);return false;">[tag]</span>
+                </td>
+                <td class="flexcont">
+                    <span class="flexele forumhover"
+                        onclick="addBB('[youtube][/youtube]', 10);return false;">[youtube]</span>
+                </td>
+                <td class="flexcont">
+                    <span id="semojis" class="forumhover" onclick="return showemojis();"
+                        style="display:<?php echo ($user_class->hideemojis) ? 'block' : 'none'; ?>;flex:2;">Show
+                        Emojis</span>
+                </td>
+                <td class="flexcont">
+                    <span id="hemojis" class="forumhover" onclick="return hideemojis();"
+                        style="display:<?php echo ($user_class->hideemojis) ? 'none' : 'block'; ?>;flex:2;">Hide
+                        Emojis</span>
+                </td>
+            </tr>
+        </table>
+        <?php
+        echo '<hr style="border:0;border-top:thin solid #333;" />';
+        echo '<table><form name="message">';
+        echo '<tr><td><textarea autofocus name="msgtext" id="reply" oninput="typing();" style="width:90%;height:125px;"></textarea><br /></td>';
+        echo '<td><input type="submit" name="submit" onclick="return sendGmail();" value="Post" /></td></tr>';
+        echo '</form></table>';
+        ?>
+        <?php
+        echo '<div id="emojis" style="display:', ($user_class->hideemojis) ? 'none' : 'block', ';">';
         emotes();
-    echo'</div>';
-    echo'</table>';
+        echo '</div>';
+        echo '</table>';
 
-echo'<style>';
-echo'#chatdiv img{';
-    echo'height: auto;';
-    echo'max-width: 500px;';
-echo'}';
-echo '
+        echo '<style>';
+        echo '#chatdiv img{';
+        echo 'height: auto;';
+        echo 'max-width: 500px;';
+        echo '}';
+        echo '
     span.likes, span.dislikes {
         margin-right: 10px;
         padding: 0px 8px;
@@ -217,128 +223,129 @@ echo '
         transform: rotateY(180deg);
     }
     ';
-echo'</style>';
-echo'<div id="chat_block">';
+        echo '</style>';
+        echo '<div id="chat_block">';
 
-$ignoredPlayerIds = array();
-$db->query("SELECT blocked FROM ignorelist WHERE blocker = $user_class->id");
-$db->execute();
-$ignored = $db->fetch_row();
+        $ignoredPlayerIds = array();
+        $db->query("SELECT blocked FROM ignorelist WHERE blocker = $user_class->id");
+        $db->execute();
+        $ignored = $db->fetch_row();
 
-foreach ($ignored as $ignore) {
-    $ignoredPlayerIds[] = $ignore['blocked'];
-}
-
-$db->query("UPDATE grpgusers SET globalchat = 0 WHERE id = $user_class->id");
-$db->execute();
-if (count($ignoredPlayerIds)) {
-    $db->query("SELECT * FROM globalchat WHERE playerid NOT IN (" . implode(',', $ignoredPlayerIds) . ") ORDER BY timesent DESC LIMIT 80");
-} else {
-    $db->query("SELECT * FROM globalchat ORDER BY timesent DESC LIMIT 80");
-}
-//$db->query("SELECT * from globalchat ORDER BY timesent DESC LIMIT 80");
-$rows = $db->fetch_row();
-foreach ($rows as $row) {
-
-        $db->query("SELECT COUNT(*) FROM chat_rating WHERE post_id = ? AND rating_action = 'like'");
-        $db->execute(
-            array(
-                $row['id']
-            )
-        );
-        $likes = $db->fetch_single();
-
-        $db->query("SELECT COUNT(*) FROM chat_rating WHERE post_id = ? AND rating_action = 'dislike'");
-        $db->execute(
-            array(
-                $row['id']
-            )
-        );
-        $dislikes = $db->fetch_single();
-
-        $db->query("SELECT * FROM chat_rating WHERE user_id = $user_class->id AND post_id = ? AND rating_action='like'");
-        $db->execute(
-            array(
-                $row['id']
-            )
-        );
-        $userLiked = $db->fetch_single();
-
-        $db->query("SELECT * FROM chat_rating WHERE user_id = $user_class->id AND post_id = ? AND rating_action='dislike'");
-        $db->execute(
-            array(
-                $row['id']
-            )
-        );
-        $userDisliked = $db->fetch_single();
-
-    if(!$m->get('tavcache.'.$row['playerid'])){
-        $reply_class = new User($row['playerid']);
-        $array = array(
-            'name' => $reply_class->formattedname,
-            'avatar' => $reply_class->avatar,
-            'admin' => $reply_class->admin,
-            'gm' => $reply_class->gm
-        );
-        $m->set('tavcache.'.$row['playerid'], $array, 60);
-    } else
-        $array = $m->get('tavcache.'.$row['playerid']);
-    $avatar = ($array['avatar'] != "") ? $array['avatar'] : "/images/no-avatar.png";
-    $quotetext=str_replace(array('\'','"'),array('\\\'','&quot;'),$row['body']);
-    echo'<div class="floaty">';
-          echo'</div>';
-        echo '<hr style="border:0;border-top:thin solid #333;" />';
-        echo '<table class="flexcont" style="width:100%;">';
-        echo '<tr>';
-
-        // Left cell for avatar and username
-        echo '<td class="flexele" style="border-right:thin solid #333;text-align:center;width:150px;">';
-        echo '<img src="' . $avatar . '" height="150" width="150" style="border:1px solid #666666" />';
-        echo '<br />';
-        if($row['playerid'] > 0) {
-            echo $array['name'];
-        } else {
-            echo '<span style="color:red">System</span>';
+        foreach ($ignored as $ignore) {
+            $ignoredPlayerIds[] = $ignore['blocked'];
         }
-        echo '</td>';
 
-        // Right cell for the body content
-        echo '<td class="flexele" style="padding:10px;">';
-        echo BBCodeParse(stripslashes($row['body']));
-        echo '<br><br>';
-        echo howlongago($row['timesent']) . ' ago <br><br>';
-        echo (($user_class->admin || $user_class->gm || $user_class->cm) && (!$array['admin'] && !$array['gm'])) ? '<a href="?gcban=' . $row['playerid'] . '&conf=' . $_SESSION['security'] . '">Ban User</a>' : '';
-        echo ($user_class->admin || $user_class->gm || $user_class->cm) ? '<a href="?delgc=' . $row['id'] . '">Delete Post</a>' : '';
-        echo'<br><div class="flexele forumhover" onClick="addsmiley(\'[quote=' . $row['playerid'] . ']' . str_replace(array("\n","\r"),array('','\n'),$quotetext) . '[/quote]\\n\\n\');">';
-        echo 'Quote';
-    echo'</div>';
-        echo '</td>';
+        $db->query("UPDATE grpgusers SET globalchat = 0 WHERE id = $user_class->id");
+        $db->execute();
+        if (count($ignoredPlayerIds)) {
+            $db->query("SELECT * FROM globalchat WHERE playerid NOT IN (" . implode(',', $ignoredPlayerIds) . ") ORDER BY timesent DESC LIMIT 80");
+        } else {
+            $db->query("SELECT * FROM globalchat ORDER BY timesent DESC LIMIT 80");
+        }
+        //$db->query("SELECT * from globalchat ORDER BY timesent DESC LIMIT 80");
+        $rows = $db->fetch_row();
+        foreach ($rows as $row) {
 
-        echo '</tr>';
-        echo '</table>';
+            $db->query("SELECT COUNT(*) FROM chat_rating WHERE post_id = ? AND rating_action = 'like'");
+            $db->execute(
+                array(
+                    $row['id']
+                )
+            );
+            $likes = $db->fetch_single();
+
+            $db->query("SELECT COUNT(*) FROM chat_rating WHERE post_id = ? AND rating_action = 'dislike'");
+            $db->execute(
+                array(
+                    $row['id']
+                )
+            );
+            $dislikes = $db->fetch_single();
+
+            $db->query("SELECT * FROM chat_rating WHERE user_id = $user_class->id AND post_id = ? AND rating_action='like'");
+            $db->execute(
+                array(
+                    $row['id']
+                )
+            );
+            $userLiked = $db->fetch_single();
+
+            $db->query("SELECT * FROM chat_rating WHERE user_id = $user_class->id AND post_id = ? AND rating_action='dislike'");
+            $db->execute(
+                array(
+                    $row['id']
+                )
+            );
+            $userDisliked = $db->fetch_single();
+
+            if (!$m->get('tavcache.' . $row['playerid'])) {
+                $reply_class = new User($row['playerid']);
+                $array = array(
+                    'name' => $reply_class->formattedname,
+                    'avatar' => $reply_class->avatar,
+                    'admin' => $reply_class->admin,
+                    'gm' => $reply_class->gm
+                );
+                $m->set('tavcache.' . $row['playerid'], $array, 60);
+            } else
+                $array = $m->get('tavcache.' . $row['playerid']);
+            $avatar = ($array['avatar'] != "") ? $array['avatar'] : "/images/no-avatar.png";
+            $quotetext = str_replace(array('\'', '"'), array('\\\'', '&quot;'), $row['body']);
+            echo '<div class="floaty">';
+            echo '</div>';
+            echo '<hr style="border:0;border-top:thin solid #333;" />';
+            echo '<table class="flexcont" style="width:100%;">';
+            echo '<tr>';
+
+            // Left cell for avatar and username
+            echo '<td class="flexele" style="border-right:thin solid #333;text-align:center;width:150px;">';
+            echo '<img src="' . $avatar . '" height="150" width="150" style="border:1px solid #666666" />';
+            echo '<br />';
+            if ($row['playerid'] > 0) {
+                echo $array['name'];
+            } else {
+                echo '<span style="color:red">System</span>';
+            }
+            echo '</td>';
+
+            // Right cell for the body content
+            echo '<td class="flexele" style="padding:10px;">';
+            echo BBCodeParse(stripslashes($row['body']));
+            echo '<br><br>';
+            echo howlongago($row['timesent']) . ' ago <br><br>';
+            echo (($user_class->admin || $user_class->gm || $user_class->cm) && (!$array['admin'] && !$array['gm'])) ? '<a href="?gcban=' . $row['playerid'] . '&conf=' . $_SESSION['security'] . '">Ban User</a>' : '';
+            echo ($user_class->admin || $user_class->gm || $user_class->cm) ? '<a href="?delgc=' . $row['id'] . '">Delete Post</a>' : '';
+            echo '<br><div class="flexele forumhover" onClick="addsmiley(\'[quote=' . $row['playerid'] . ']' . str_replace(array("\n", "\r"), array('', '\n'), $quotetext) . '[/quote]\\n\\n\');">';
+            echo 'Quote';
+            echo '</div>';
+            echo '</td>';
+
+            echo '</tr>';
+            echo '</table>';
 
 
 
-}
+        }
 
 
-print"</div>";
+        print "</div>";
 
 
 
-include("footer.php");
-?>
+        include("footer.php");
+        ?>
 
-<style>
-    #newtables th {
+        <style>
+            #newtables th {
 
-padding: 10px;
-text-align: left;
-font-weight: normal;
-border-bottom: 1px solid #555;
-}
-#newtables td{
-    padding: 10px;
-    border-bottom: 1px solid #444;
-}
-</style>
+                padding: 10px;
+                text-align: left;
+                font-weight: normal;
+                border-bottom: 1px solid #555;
+            }
+
+            #newtables td {
+                padding: 10px;
+                border-bottom: 1px solid #444;
+            }
+        </style>
