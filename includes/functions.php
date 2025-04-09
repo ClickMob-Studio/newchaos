bcc8286c059112a2b5285debbe9234a14ec018e7
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-04-09 15:23:25 +0200

    chore: fix timeremaining

--- includes/functions.php
+++ includes/functions.php
@@ -1,4 +1,36 @@
 <?php
+
+function timeRemaining($futureTimestamp)
+{
+    $secondsLeft = $futureTimestamp - time();
+
+    if ($secondsLeft <= 0) {
+        return "now";
+    }
+
+    $units = [
+        'year' => 365 * 24 * 60 * 60,
+        'month' => 30 * 24 * 60 * 60,
+        'week' => 7 * 24 * 60 * 60,
+        'day' => 24 * 60 * 60,
+        'hour' => 60 * 60,
+        'minute' => 60,
+        'second' => 1
+    ];
+
+    $parts = [];
+
+    foreach ($units as $name => $duration) {
+        if ($secondsLeft >= $duration) {
+            $value = floor($secondsLeft / $duration);
+            $secondsLeft %= $duration;
+            $parts[] = "$value $name" . ($value > 1 ? "s" : "");
+        }
+    }
+
+    return implode(", ", array_slice($parts, 0, 4)); // Show up to 4 time units
+}
+
 function ordinal($number)
 {
     $ends = array('th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th');
