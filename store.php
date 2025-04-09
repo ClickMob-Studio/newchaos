17fafb0df42f21e5f057fdc1b9648e2a03787a88
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-04-09 14:14:33 +0200

    chore: move to easter limited section

--- store.php
+++ store.php
@@ -1283,6 +1283,29 @@ include 'header.php';
     </div>
     <br>
 </div>
+
+<?php if ($user_class->admin == 1): ?>
+    <br>
+    <div class="floaty" style="margin: 3px;">
+        <h4>EASTER LIMITEDS</h4>
+        <hr>
+        <div class="vip-packages"
+            style="display: flex; justify-content: space-around; align-items: stretch; flex-wrap: wrap;">
+
+            <!-- Rare Egg Basket -->
+            <div class="vip-package">
+                <h4 style="color: brown;">1 x <?= item_popup('Rare Egg Basket', 344, 'brown'); ?></h4>
+                <img src="/css/images/2025/rare_easter_basket.png" height="100" alt="Mission Pass">
+
+                <h4>Purchase now for only<br><a href="store.php?buy=easterbasket"><button class="gold-button">25 <img
+                                src="https://chaoscity.co.uk/goldbar.png" alt="Gold bar"></button></a></h4>
+            </div>
+
+        </div>
+        <br>
+    </div>
+<?php endif; ?>
+
 <br>
 <div class="floaty" style="margin: 3px;">
     <h4>VIP PACKAGES</h4>
@@ -1360,25 +1383,13 @@ include 'header.php';
     <br>
 </div>
 
-<br /><br />
+<br /><br /><br />
 
-<br>
 <div class="floaty" style="margin: 3px;">
     <h4>ITEMS</h4>
     <hr>
     <div class="items-upgrades"
         style="display: flex; justify-content: space-around; align-items: stretch; flex-wrap: wrap;">
-        <?php if ($user_class->admin == 1): ?>
-            <!-- Rare Egg Basket -->
-            <div class="vip-package">
-                <h4 style="color: brown;">1 x <?= item_popup('Rare Egg Basket', 344, 'brown'); ?></h4>
-                <img src="/css/images/2025/rare_easter_basket.png" height="100" alt="Mission Pass">
-
-                <h4>Purchase now for only<br><a href="store.php?buy=easterbasket"><button class="gold-button">25 <img
-                                src="https://chaoscity.co.uk/goldbar.png" alt="Gold bar"></button></a></h4>
-            </div>
-        <?php endif; ?>
-
         <!-- Mission Pass -->
         <div class="vip-package">
             <h4 style="color: brown;">1 x Mission Pass</h4>
