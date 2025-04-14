02bb855d40d845da700b30c5b576d494fe7a5ac8
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-04-14 11:18:38 +0200

    chore: gap

--- newcrimes2.php
+++ newcrimes2.php
@@ -33,7 +33,7 @@ $rows = $db->fetch_row();
     }
 </style>
 
-<div class="max-w-7xl mx-auto flex">
+<div class="max-w-7xl mx-auto flex gap-x-4">
       <div class="min-w-xs max-w-xs w-xs border border-white/10 bg-black/40 border-6 rounded-lg p-4"></div>
       <div class="w-full border border-white/10 bg-black/40 border-6 rounded-lg p-4"></div>
 </div>
