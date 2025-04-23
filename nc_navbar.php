98ec5398b2c9072a6862ed389613c8c07cc53d64
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-04-23 11:10:28 +0200

    chore: quick nav

--- nc_navbar.php
+++ nc_navbar.php
@@ -399,7 +399,7 @@ $navPage = str_replace('/', '', $_SERVER['REQUEST_URI']);
     <div class="px-4 py-2 bg-black/40 rounded-lg flex gap-x-4">
         <div class="flex gap-x-2 items-center cursor-pointer" x-on:click="qnOpen = !qnOpen">
             <span class="text-md text-white">QUICK NAV</span>
-            <img src="assets/images/icons/QuickNav Expanded.png" class="h-[14px] w-[14px]" :class="qnOpen ? 'transform-[rotate(180deg)]'
+            <img src="assets/images/icons/QuickNav Expanded.png" class="h-[14px] w-[14px] transition-all" :class="qnOpen ? 'transform-[rotate(180deg)]'
                 : 'transform-[rotate(0deg)]'" />
         </div>
         <div class=" flex gap-x-2" x-show="qnOpen" x-transition>
