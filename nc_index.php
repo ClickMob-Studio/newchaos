eca488aed25af438b36003012738270076dc63e6
Mathias Mogensen <mathias@appflowy.io>
2025-04-23 18:28:19 +0200

    chore: nc index

--- nc_index.php
+++ nc_index.php
@@ -1,7 +1,15 @@
 <?php
+
 session_start();
 
 include 'nc_header.php';
+
+if ($user_class->firstlogin1 == 0) {
+    $stmt = mysql_query("UPDATE grpgusers SET firstlogin1 = 1 WHERE id = " . $user_class->id);
+    Send_Event2($user_class->id, "Is the latest thug on the streets.", $user_class->id);
+    Send_Event($user_class->id, "<div class='text-white'>Welcome To Chaos City!<br>To get you started we are giving you:</div><div class='fw-bold text-white'>&bull; 3 VIP Days<br>&bull; $100,000 Cash<br>&bull; 1,250 Points</div>", $user_class->id);
+}
+
 ?>
 
 <!-- Carousel -->
@@ -169,194 +177,12 @@ include 'nc_header.php';
 </div>
 
 <div class="max-w-7xl mx-auto flex flex-col gap-y-4 px-2 md:px-6 lg:px-8">
+    <h1 class="text-xl">Message Board</h1>
+</div>
+
+<div class="max-w-7xl mx-auto flex flex-col gap-y-4 px-2 md:px-6 lg:px-8">
     <div class="w-full border border-white/10 bg-black/40 border-6 rounded-lg p-4">
 
-        <?php
-        if ($user_class->firstlogin1 == 0) {
-            $stmt = mysql_query("UPDATE grpgusers SET firstlogin1 = 1 WHERE id = " . $user_class->id);
-            Send_Event2($user_class->id, "Is the latest thug on the streets.", $user_class->id);
-            Send_Event($user_class->id, "<div class='text-white'>Welcome To Chaos City!<br>To get you started we are giving you:</div><div class='fw-bold text-white'>&bull; 3 VIP Days<br>&bull; $100,000 Cash<br>&bull; 1,250 Points</div>", $user_class->id);
-        }
-        if ($user_class->level >= 10) {
-            $line = mysql_fetch_array(mysql_query("SELECT * FROM referrals WHERE referred = " . $user_class->id . " AND credited = '0'"));
-            if (mysql_num_rows($line)) {
-                bloodbath('referrals', $line['referrer']);
-                mysql_query("UPDATE grpgusers SET credits = credits + 50, points = points + 100, referrals = referrals + 1, refcomp = refcomp + 1, refcount = refcount + 1 WHERE referred = " . $user_class->id);
-                mysql_query("UPDATE referrals SET credited = 1 WHERE referred =" . $user_class->id);
-                mysql_query("UPDATE referrals SET viewed = 1 WHERE referred = " . $user_class->id);
-                Send_Event($line['referrer'], "You have been credited 50 Credits & 100 Points for referring [-_USERID_-]. Keep up the good work!", $line['referred']);
-                Send_Event(1, 'USER ID: ' . $line['referred'] . ' referral for ' . $user_class->formattedname . ' payed out');
-                Send_Event(2, 'USER ID: ' . $line['referred'] . ' referral for ' . $user_class->formattedname . ' payed out');
-
-            }
-        }
-
-        ?>
-        <h1 class="text-lg text-white font-medium">General Information</h1>
-        <div class="table-container">
-            <table id="newtables" class="w-full text-white">
-                <tr>
-                    <th width="10%" class="bg-white/5 px-2">Name:</th>
-                    <td width="30%" class="bg-white/10 px-2">
-                        <a href="profiles.php?id=<?= $user_class->id ?>"><?= $user_class->formattedname ?></a>
-                    </td>
-                    <th width="10%" class="bg-white/5 px-2">HP:</th>
-                    <td width="30%" class="bg-white/10 px-2"><?= prettynum($user_class->formattedhp) ?></td>
-                </tr>
-                <tr>
-                    <th width="10%" class="bg-white/5 px-2">Level:</th>
-                    <td width="30%" class="bg-white/10 px-2"><?= $user_class->level ?></td>
-                    <th width="10%" class="bg-white/5 px-2">Energy:</th>
-                    <td width="30%" class="bg-white/10 px-2"><?= prettynum($user_class->formattedenergy) ?></td>
-                </tr>
-                <tr>
-                    <th width="10%" class="bg-white/5 px-2">Money:</th>
-                    <td width="30%" class="bg-white/10 px-2">$<?= prettynum($user_class->money) ?></td>
-                    <th width="10%" class="bg-white/5 px-2">Awake:</th>
-                    <td width="30%" class="bg-white/10 px-2"><?= prettynum($user_class->formattedawake) ?></td>
-                </tr>
-                <tr>
-                    <th width="10%" class="bg-white/5 px-2">Bank:</th>
-                    <td width="30%" class="bg-white/10 px-2">$<?= prettynum($user_class->bank) ?></td>
-                    <th width="10%" class="bg-white/5 px-2">Nerve:</th>
-                    <td width="30%" class="bg-white/10 px-2"><?= prettynum($user_class->formattednerve) ?></td>
-                </tr>
-                <tr>
-                    <th width="10%" class="bg-white/5 px-2">EXP:</th>
-                    <td width="30%" class="bg-white/10 px-2"><?= prettynum($user_class->formattedexp) ?></td>
-                    <th width="10%" class="bg-white/5 px-2">Work EXP:</th>
-                    <td width="30%" class="bg-white/10 px-2"><?= prettynum($user_class->workexp) ?></td>
-                </tr>
-                <tr>
-                    <th width="10%" class="bg-white/5 px-2">RM Days:</th>
-                    <td width="30%" class="bg-white/10 px-2"><?= prettynum($user_class->rmdays) ?></td>
-                    <th width="10%" class="bg-white/5 px-2">Activity Points:</th>
-                    <td width="30%" class="bg-white/10 px-2"><a href="spendactivity.php">Activity Points Store
-                            [<?= prettynum($user_class->apoints) ?> Activity Points]</td>
-                </tr>
-            </table>
-
-            <h1 class="text-lg text-white mt-2 font-medium">Stat Information</h1>
-            <table id="newtables" class="w-full text-white">
-                <tr>
-                    <th width="15%" class="bg-white/5 px-2">Strength:</th>
-                    <td class="bg-white/10 px-2"><?= prettynum($user_class->strength) ?></td>
-                    <td class="bg-white/10 px-2">[Ranked: <?= getRank("$user_class->id", "strength") ?>]</td>
-                    <th width="15%" class="bg-white/5 px-2">Defense:</th>
-                    <td class="bg-white/10 px-2"><?= prettynum($user_class->defense) ?></td>
-                    <td class="bg-white/10 px-2">[Ranked: <?= getRank("$user_class->id", "defense") ?>]</td>
-                </tr>
-                <tr>
-                    <th width="15%" class="bg-white/5 px-2">Speed:</th>
-                    <td class="bg-white/10 px-2"><?= prettynum($user_class->speed) ?></td>
-                    <td class="bg-white/10 px-2">[Ranked: <?= getRank("$user_class->id", "speed") ?>]</td>
-                    <th width="15%" class="bg-white/5 px-2">Agility:</th>
-                    <td class="bg-white/10 px-2"><?= prettynum($user_class->agility) ?></td>
-                    <td class="bg-white/10 px-2">[Ranked: <?= getRank("$user_class->id", "agility") ?>]</td>
-                </tr>
-                <tr>
-                    <th width="15%" class="bg-white/5 px-2">Total:</th>
-                    <td class="bg-white/10 px-2"><?= prettynum($user_class->totalattrib) ?></td>
-                    <td class="bg-white/10 px-2">[Ranked: <?= getRank("$user_class->id", "total") ?>]</td>
-                </tr>
-            </table>
-
-            <h1 class="text-lg text-white mt-2 font-medium">Modded Stats Information</h1>
-            <table id="newtables" class="w-full text-white">
-                <tr>
-                    <th width="15%" class="bg-white/5 px-2">Modded Strength:</th>
-                    <td width="25%" class="bg-white/10 px-2"><?= prettynum($user_class->moddedstrength) ?></td>
-                    <th width="15%" class="bg-white/5 px-2">Modded Defense:</th>
-                    <td width="25%" class="bg-white/10 px-2"><?= prettynum($user_class->moddeddefense) ?></td>
-                </tr>
-                <tr>
-                    <th width="15%" class="bg-white/5 px-2">Modded Speed:</th>
-                    <td width="25%" class="bg-white/10 px-2"><?= prettynum($user_class->moddedspeed) ?></td>
-                    <th width="15%" class="bg-white/5 px-2">Modded Agility:</th>
-                    <td width="25%" class="bg-white/10 px-2"><?= prettynum($user_class->moddedagility) ?></td>
-                </tr>
-                <th width="15%" class="bg-white/5 px-2">Modded Total:</th>
-                <td width="25%" class="bg-white/10 px-2"><?= prettynum($user_class->moddedtotalattrib) ?></td>
-                <th width="15%" class="bg-white/5 px-2"></th>
-                <td width="25%" class="bg-white/10 px-2"></td>
-            </table>
-
-            <h1 class="text-lg text-white mt-2 font-medium">Battle Statistics</h1>
-            <table id="newtables" class="w-full text-white">
-                <tr>
-                    <th width="10%" class="bg-white/5 px-2">Won:</th>
-                    <td width="30%" class="bg-white/10 px-2"><?= prettynum($user_class->battlewon) ?></td>
-                    <th width="10%" class="bg-white/5 px-2">Lost:</th>
-                    <td width="30%" class="bg-white/10 px-2"><?= prettynum($user_class->battlelost) ?></td>
-                </tr>
-                <tr>
-                    <th width="10%" class="bg-white/5 px-2">Total:</th>
-                    <td width="30%" class="bg-white/10 px-2"><?= prettynum($user_class->battletotal) ?></td>
-                    <th width="10%" class="bg-white/5 px-2">Money Gain:</th>
-                    <td width="30%" class="bg-white/10 px-2">$<?= prettynum($user_class->battlemoney) ?></td>
-                </tr>
-            </table>
-
-            <h1 class="text-lg text-white mt-2 font-medium">Crime Rankings</h1>
-            <table id="newtables" class="w-full text-white">
-                <tr>
-                    <th width="10%" class="bg-white/5 px-2">Succeeded:</th>
-                    <td width="30%" class="bg-white/10 px-2"><?= prettynum($user_class->crimesucceeded) ?></td>
-                    <th width="10%" class="bg-white/5 px-2">Failed:</th>
-                    <td width="30%" class="bg-white/10 px-2"><?= prettynum($user_class->crimefailed) ?></td>
-                </tr>
-                <tr>
-                    <th width="10%" class="bg-white/5 px-2">Total:</th>
-                    <td width="30%" class="bg-white/10 px-2"><?= prettynum($user_class->crimetotal) ?></td>
-                    <th width="10%" class="bg-white/5 px-2">Money Gain:</th>
-                    <td width="30%" class="bg-white/10 px-2">$<?= prettynum($user_class->crimemoney) ?></td>
-                </tr>
-            </table>
-
-            <h1 class="text-lg text-white mt-2 font-medium">Bonus Stats</h1>
-            <table id="newtables" class="w-full text-white">
-                <tr>
-                    <th width="10%" class="bg-white/5 px-2">Total Tax Paid:</th>
-                    <td width="30%" class="bg-white/10 px-2">$<?= prettynum($user_class->totaltax) ?></td>
-                    <th width="10%" class="bg-white/5 px-2">???:</th>
-                    <td width="30%" class="bg-white/10 px-2"><?= prettynum($user_class->crimefailed) ?></td>
-                </tr>
-            </table>
-
-            <div class="text-center mt-4">
-                <div class="d-flex flex-wrap justify-content-center">
-                    <a href="achievements.php"
-                        class="p-2 text-white/80 hover:text-white bg-secondary m-2">[Achievements]</a>
-                    <a href="translog.php" class="p-2 text-white/80 hover:text-white bg-secondary m-2">[Transfer
-                        Logs]</a>
-                    <a href="attackv2_logs.php" class="p-2 text-white/80 hover:text-white bg-secondary m-2">[Attack Log
-                        NEW]</a>
-                    <a href="attacklog.php" class="p-2 text-white/80 hover:text-white bg-secondary m-2">[Attack Log]</a>
-                    <a href="defenselog.php" class="p-2 text-white/80 hover:text-white bg-secondary m-2">[Defense
-                        Log]</a>
-                    <a href="muglog.php" class="p-2 text-white/80 hover:text-white bg-secondary m-2">[Mug Log]</a>
-                    <a href="spylog.php" class="p-2 text-white/80 hover:text-white bg-secondary m-2">[Spy Log]</a>
-                </div>
-            </div>
-        </div>
-
-    </div>
-
-    <div class="w-full border border-white/10 bg-black/40 border-6 rounded-lg p-4 text-white">
-        <h1 class="text-lg font-medium">EXP Calculator</h1>
-        <div class="d-flex">
-            <div class="flex flex-col p-2">
-                What level are you aiming for?
-                <input type="text" oninput="calcEXP();" id="levelcalc" size="8"
-                    class="bg-white/10 border min-w-md max-w-md border-gray rounded-lg mt-1" />
-            </div>
-            <div class="p-2">
-                <span id="levelrtn">
-                    You need <?= prettynum(experience($user_class->level + 1) - $user_class->exp); ?>
-                    EXP to get to level <?= prettynum($user_class->level + 1); ?>.
-                </span>
-            </div>
-        </div>
     </div>
 </div>
 
