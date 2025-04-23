9f9f029af7fa125b1dbcb8416f7fd802271e4f8b
Mathias Mogensen <mathias@appflowy.io>
2025-04-23 23:49:15 +0200

    chore: title

--- forums.php
+++ forums.php
@@ -37,8 +37,8 @@ $threads = getThreads($fid, $page);
 
 ?>
 
-<div class="max-w-7xl mx-auto flex gap-y-4 px-2 md:px-6 lg:px-8 items-center gap-x-2">
-    <h1 class="pl-4 pt-2 pb-4 text-white text-2xl"><?= $forum['name'] ?></h1>
+<div class="max-w-7xl mx-auto flex gap-y-4 px-2 md:px-6 lg:px-8 items-center gap-x-2 pt-2 pb-4">
+    <h1 class="pl-4 text-white text-2xl"><?= $forum['name'] ?></h1>
     <span class="text-lg text-gray-300"> <em>-</em> </span>
     <h3 class="text-lg text-gray-400"><?= $forum['description'] ?></h3>
 </div>
