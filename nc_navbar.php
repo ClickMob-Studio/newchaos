73eefc6ac5cdb1b5b5df4b5d6b7dc30b750c2381
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-04-23 11:01:25 +0200

    chore: quick nav

--- nc_navbar.php
+++ nc_navbar.php
@@ -397,21 +397,23 @@ $navPage = str_replace('/', '', $_SERVER['REQUEST_URI']);
 <!-- Quick Navigation -->
 <div class="max-w-7xl mx-auto mt-2 py-2 select-none hidden sm:flex px-2 md:px-6 lg:px-8">
     <div class="px-4 py-2 bg-black/40 rounded-lg flex gap-x-4">
-        <div class="flex gap-x-2 items-center">
+        <div class="flex gap-x-2 items-center" x-data="{ qnOpen: false }" x-on:click="qnOpen = !qnOpen">
             <span class="text-md text-white">QUICK NAV</span>
             <img src="assets/images/icons/QuickNav Expanded.png" class="h-[14px] w-[14px]" />
         </div>
-        <a href="#" class="text-white"> HOME </a>
-        <a href="#" class="text-gray-400 hover:text-gray-300"> STORE </a>
-        <a href="#" class="text-gray-400 hover:text-gray-300"> CRIMES </a>
-        <a href="#" class="text-gray-400 hover:text-gray-300"> JAIL </a>
-        <a href="#" class="text-gray-400 hover:text-gray-300"> HOSPITAL </a>
-        <a href="#" class="text-gray-400 hover:text-gray-300"> GYM </a>
-        <a href="#" class="text-gray-400 hover:text-gray-300"> BANK </a>
-        <a href="#" class="text-gray-400 hover:text-gray-300"> ESTATE </a>
-        <a href="#" class="text-gray-400 hover:text-gray-300"> DRUGS </a>
-        <a href="#" class="text-gray-400 hover:text-gray-300"> GANG </a>
-        <a href="#" class="text-gray-400 hover:text-gray-300"> CRAFTING </a>
-        <a href="#" class="text-gray-400 hover:text-gray-300"> QUESTS </a>
+        <div class="flex gap-x-2" x-show="qnOpen" x-transition>
+            <a href="#" class="text-white"> HOME </a>
+            <a href="#" class="text-gray-400 hover:text-gray-300"> STORE </a>
+            <a href="#" class="text-gray-400 hover:text-gray-300"> CRIMES </a>
+            <a href="#" class="text-gray-400 hover:text-gray-300"> JAIL </a>
+            <a href="#" class="text-gray-400 hover:text-gray-300"> HOSPITAL </a>
+            <a href="#" class="text-gray-400 hover:text-gray-300"> GYM </a>
+            <a href="#" class="text-gray-400 hover:text-gray-300"> BANK </a>
+            <a href="#" class="text-gray-400 hover:text-gray-300"> ESTATE </a>
+            <a href="#" class="text-gray-400 hover:text-gray-300"> DRUGS </a>
+            <a href="#" class="text-gray-400 hover:text-gray-300"> GANG </a>
+            <a href="#" class="text-gray-400 hover:text-gray-300"> CRAFTING </a>
+            <a href="#" class="text-gray-400 hover:text-gray-300"> QUESTS </a>
+        </div>
     </div>
 </div>
