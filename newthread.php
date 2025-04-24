ff0f4bfc2fad2f5c3a09270db561a76f7aae13fd
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-04-24 22:43:27 +0200

    chore: emoji mart

--- newthread.php
+++ newthread.php
@@ -266,7 +266,7 @@ if (!$canpostthreads) {
             'z-[1000]', 'overflow-y-auto',
             'duration-300', 'ease-in-out',
             'fixed', 'left-0', 'right-0', 'bottom-0', 'h-[60vh]',
-            'md:absolute', 'md:top-[2rem]', 'md:left-0', 'md:w-80', 'md:rounded-xl', 'md:h-60vh',
+            'md:absolute', 'md:top-[2rem]', 'md:left-0', 'md:w-80', 'md:rounded-xl', 'md:h-40vh',
             'transition-all',
             'opacity-0', 'pointer-events-none', 'hidden'
         );
