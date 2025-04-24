349198e31dfadfb8f64dd1a1f6c1ee00bbbed1f9
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-04-24 17:28:33 +0200

    chore: new thread

--- newthread.php
+++ newthread.php
@@ -56,12 +56,16 @@ if (!$canpostthreads) {
 </style>
 
 <form method="POST">
+    <input type="hidden" name="fid" value="<?= $fid ?>">
+
     <div class="max-w-7xl mx-auto flex flex-col gap-y-4 px-2 md:px-6 lg:px-8">
         <div class="w-full border border-white/10 bg-black/40 border-4 rounded-lg">
             <h2 class="text-white text-xl px-4 py-2">Post new thread in <?= $forum['name'] ?></h2>
 
-            <input type="text" id="subject" name="subject" placeholder="Subject"
-                class="text-white bg-black/40 border border-white/10 rounded-lg px-4 py-2 w-full" required>
+            <div class="px-4 py-2">
+                <input type="text" id="subject" name="subject" placeholder="Subject"
+                    class="text-white bg-black/40 border border-white/10 rounded-lg px-4 py-1 w-full" required>
+            </div>
 
             <div class="px-4 py-2 rounded-lg text-white">
                 <div id="editor"></div>
