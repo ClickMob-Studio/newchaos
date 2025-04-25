86ac4ecdfa72832690a8f70244a184feba0b07e1
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-04-25 11:11:04 +0200

    chore: forums

--- forums.php
+++ forums.php
@@ -93,7 +93,7 @@ if ($canonlyviewownthreads) {
                         <tr class="border-b border-gray-700 hover:bg-gray-600">
                             <!-- Fetch username by doing $author = new User($thread['uid']); -->
                             <?php $author = new User($thread['uid']); ?>
-                            <td class="px-6 py-4 flex">
+                            <td class="px-6 py-4 flex items-center gap-x-6">
                                 <img class="size-6 md:size-8 rounded-full" src="<?php echo $author->avatar ?>" alt="" />
                                 <?= $author->formattedname ?>
                             </td>
