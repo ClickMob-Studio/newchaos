74a06d37e8a147621a7530bb395efa7961ce3e57
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-07-15 21:42:03 +0200

    chore: use shell_exec

--- deployer.php
+++ deployer.php
@@ -57,7 +57,7 @@ $commands = array(
 // Run the commands for output
 $output = '';
 foreach ($commands as $command) {
-    $tmp = exec($command);
+    $tmp = shell_exec($command);
     $output .= "<span style=\"color: #6BE234;\">$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
     $output .= htmlentities(trim($tmp)) . "\n";
 }
