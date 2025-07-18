109f1ee3921e90544591907eb9f865cfeca3c065
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-07-18 22:22:53 +0200

    chore: fix content type

--- ajax_crimes2.php
+++ ajax_crimes2.php
@@ -5,7 +5,7 @@ ob_start();
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header('Content-type: application/json');
+header('Content-Type: application/json');
 session_start();
 
 function shorthandNumber($number)
