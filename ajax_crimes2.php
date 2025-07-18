f4f61e7cea64edeabd312812999ca6626b3655db
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-07-18 22:21:07 +0200

    chore: ob start

--- ajax_crimes2.php
+++ ajax_crimes2.php
@@ -1,5 +1,7 @@
 <?php
 
+ob_start();
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
