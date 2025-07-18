25fc278908fc0f4c7148bc1958a9de28c6fc1183
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-07-18 22:40:08 +0200

    chore: more debug

--- ajax_crimes2.php
+++ ajax_crimes2.php
@@ -140,6 +140,8 @@ if (isset($_POST['id']) || isset($input['id'])) {
             $questSeasonMissionUser = getQuestSeasonMissionUser($user_class->id, $currentQuestSeason['id']);
             $questSeasonMission = getQuestSeasonMission($user_class->id, $currentQuestSeason['id']);
 
+            log_error('DUMP: ' . print_r($questSeasonMissionUser, true));
+            log_error('DUMP: ' . print_r($questSeasonMissionUserUser, true));
             if (isset($questSeasonMission['requirements']->whitecollar_fraud) || (int) $questSeasonMissionUser['progress']->whitecollar_fraud < 10) {
                 $exp = ceil($user_class->maxexp / 4);
                 updateQuestSeasonMissionUserProgress($questSeasonMissionUser, 'whitecollar_fraud', 1);
