10ca11672fadba7459be3be714b6dd4e2d46c7d5
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-07-18 22:31:10 +0200

    chore: revert

--- ajax_crimes2.php
+++ ajax_crimes2.php
@@ -1,7 +1,5 @@
 <?php
 
-ob_start();
-
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
