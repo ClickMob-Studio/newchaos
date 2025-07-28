1f057f45f16e5e96fde4f5bde96a1ed52ef9488e
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-07-28 19:00:03 +0200

    fix: html of ratings.php

--- ratings.php
+++ ratings.php
@@ -37,32 +37,32 @@ include 'header.php';
             </tr>
         </div>
         <div class="contenthead floaty">
-            < h1>Bottom Rated</h1>
-                <table id="newtables" style="width:100%;">
+            <h1>Bottom Rated</h1>
+            <table id="newtables" style="width:100%;">
+                <tr>
+                    <td><b>Rank</b></td>
+                    <td><b>Mobster</b></td>
+                    <td><b>Rating</b></td>
+                </tr>
+                <?php
+                $db->query("SELECT * FROM `grpgusers` g WHERE `admin` = 0 AND NOT EXISTS(SELECT * FROM bans b WHERE b.id = g.id AND type IN ('perm','freeze')) ORDER BY `rating` ASC LIMIT 10");
+                $results = $db->fetch_row();
+                $rank = 0;
+                foreach ($results as $line) {
+                    $rank++;
+                    $user_hall = new User($line['id']);
+                    ?>
                     <tr>
-                        <td><b>Rank</b></td>
-                        <td><b>Mobster</b></td>
-                        <td><b>Rating</b></td>
+                        <td width="15%"><?php echo $rank; ?></td>
+                        <td width="40%"><?php echo $user_hall->formattedname; ?></td>
+                        <td width="20%"><?php echo prettynum($user_hall->rating); ?></td>
                     </tr>
                     <?php
-                    $db->query("SELECT * FROM `grpgusers` g WHERE `admin` = 0 AND NOT EXISTS(SELECT * FROM bans b WHERE b.id = g.id AND type IN ('perm','freeze')) ORDER BY `rating` ASC LIMIT 10");
-                    $results = $db->fetch_row();
-                    $rank = 0;
-                    foreach ($results as $line) {
-                        $rank++;
-                        $user_hall = new User($line['id']);
-                        ?>
-                        <tr>
-                            <td width="15%"><?php echo $rank; ?></td>
-                            <td width="40%"><?php echo $user_hall->formattedname; ?></td>
-                            <td width="20%"><?php echo prettynum($user_hall->rating); ?></td>
-                        </tr>
-                        <?php
-                    }
-                    ?>
-                    </td>
-                    </tr>
-                </table>
+                }
+                ?>
+                </td>
+                </tr>
+            </table>
         </div>
         <?php
         include 'footer.php';
