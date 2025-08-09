3959fddf46603be056b8ef8cc870cd15a14e1f42
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-08-10 01:15:01 +0200

    fix: howlongago profile ajax

--- profileajax.php
+++ profileajax.php
@@ -25,35 +25,3 @@ if (isset($_GET['user_id'])) {
     echo "Error: user_id parameter not received";
 }
 ?>
-
-<?php
-
-function howlongago($ts, $stop = 'none')
-{
-    $ts = time() - $ts;
-    if ($ts < 1)
-        return " NOW";
-    elseif ($ts == 1)
-        return $ts . "s";
-    elseif ($ts < 60)
-        return $ts . "s";
-    elseif ($ts < 120)
-        return "1m " . ($ts % 60) . "s";
-    elseif ($ts < 60 * 60)
-        return floor($ts / 60) . "m " . ($ts % 60) . "s";
-    elseif ($ts < 60 * 60 * 2)
-        return "1h " . floor(($ts / 60) % 60) . "m " . ($ts % 60) . "s";
-    elseif ($ts < 60 * 60 * 24)
-        return floor($ts / (60 * 60)) . "h " . floor(($ts / 60) % 60) . "m " . ($ts % 60) . "s";
-    elseif ($ts < 60 * 60 * 24 * 2)
-        return "1d " . floor($ts / (60 * 60) % 24) . "h " . floor(($ts / 60) % 60) . "m " . ($ts % 60) . "s";
-    elseif ($ts < (60 * 60 * 24 * 7) or $stop == 'days')
-        return floor($ts / (60 * 60 * 24)) . "d " . floor($ts / (60 * 60) % 24) . "h " . floor(($ts / 60) % 60) . "m " . ($ts % 60) . "s";
-    elseif ($ts < 60 * 60 * 24 * 30.5)
-        return floor($ts / (60 * 60 * 24 * 7)) . " weeks ago";
-    elseif ($ts < 60 * 60 * 24 * 365)
-        return floor($ts / (60 * 60 * 24 * 30.5)) . " months ago";
-    else
-        return floor($ts / (60 * 60 * 24 * 365)) . " years ago";
-}
-?>
