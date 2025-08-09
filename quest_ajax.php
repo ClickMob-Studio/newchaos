0152102fe6ba44ae206dedccf683e2324dfbe50c
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-08-09 23:22:41 +0200

    chore: remove logs

--- quest_ajax.php
+++ quest_ajax.php
@@ -2,17 +2,8 @@
 
 header('Content-Type: application/json');
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-ini_set('log_errors', 1);
-error_reporting(E_ALL);
-
-error_log("REACHED BEFORE REQUIRES!");
-
 require_once __DIR__ . '/classes.php';
 require_once __DIR__ . '/database/pdo_class.php';
-
-error_log("REACHED AFTER REQUIRES!");
 
 $id = null;
 if (isset($_GET['user_id'])) {
@@ -33,18 +24,12 @@ if (!$user_class) {
     exit;
 }
 
-error_log("REACHED UNO!");
 try {
     $currentQuestSeason = getCurrentQuestSeasonForUser($user_class->id);
-    error_log("REACHED DOS!");
     if (isset($currentQuestSeason['id'])) {
-        error_log("REACHED TRES!");
         $questSeasonUser = getQuestSeasonUser($user_class->id, $currentQuestSeason['id']);
-        error_log("REACHED QUATTRO!");
         $questSeasonMissionUser = getQuestSeasonMissionUser($user_class->id, $currentQuestSeason['id']);
-        error_log("REACHED SINGO!");
         $questSeasonMission = getQuestSeasonMission($user_class->id, $currentQuestSeason['id']);
-        error_log("REACHED SES!");
 
         $field = null;
         if (isset($_POST['field'])) {
@@ -62,22 +47,17 @@ try {
             $value = $_GET['value'];
         }
 
-        error_log("REACHED OCHO!");
         if (isset($questSeasonMission['requirements']->$field) && $field && $value) {
             updateQuestSeasonMissionUserProgress($questSeasonMissionUser, $field, $value);
-            error_log("REACHED NUEVE!");
 
             echo json_encode(array('success' => true));
             exit;
         }
     }
 
-    error_log("REACHED DIEZ!");
     echo json_encode(array('success' => false));
     exit;
 } catch (Throwable $e) {
-    error_log("REACHED ONCE!");
-
     http_response_code(500);
     echo json_encode(['success' => false, 'error' => 'server_error']);
     exit;
