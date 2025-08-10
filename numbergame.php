77403d0cb4f728508507b75bc49f6ad2c64b7f6e
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-08-10 11:56:58 +0200

    fix: numbergame

--- numbergame.php
+++ numbergame.php
@@ -19,32 +19,32 @@ include "header.php";
         $games = $db->fetch_row();
         $no2 = count($games);
 
-        if (isset($_GET['take'])) {
+        if (!isset($_GET['take'])) {
+            print "<b>Mini Competition! Current Prize: 1,000 Points<br />Guess a number from 1 to 25<br />You will recieve a event if you win.</b><br /><br /><table width='30%' cellspacing=1 class='myTable'><tr><td class='contenthead' align='center'>Number</td><td class='contenthead' align='center'>Player</td></tr>";
+            $db->query("SELECT * FROM numbergame ORDER BY number ASC");
+            $results = $db->fetch_row();
+            foreach ($results as $r) {
+                $usersid = ($r['userid']);
+                $db->query("SELECT * FROM grpgusers WHERE id = ?");
+                $db->execute([$usersid]);
+                $u = $db->fetch_row(true);
+                if ($r['userid'] == 0) {
+                    if ($no2 > 0) {
+                        $u['username'] = "-";
+                    } else {
+                        $u['username'] = "[<a href='numbergame.php?take={$r['number']}'>select</a>]";
+                    }
+                } else {
+                    $u['username'] = "<a href='profiles.php?id={$u['id']}'>{$u['username']}</a>";
+                }
+                print "\n<tr align=center><td class=contentcontent>{$r['number']}</td><td class=contentcontent>{$u['username']}</td></tr>";
+                $current_row = 1 - $current_row;
+            }
+
+        } else {
             $_GET['take'] = abs((int) $_GET['take']);
             $getid = $_GET['take'];
-            if (!$getid) {
-                print "<b>Mini Competition! Current Prize: 1,000 Points<br />Guess a number from 1 to 25<br />You will recieve a event if you win.</b><br /><br /><table width='30%' cellspacing=1 class='myTable'><tr><td class='contenthead' align='center'>Number</td><td class='contenthead' align='center'>Player</td></tr>";
-                $db->query("SELECT * FROM numbergame ORDER BY number ASC");
-                $results = $db->fetch_row();
-                foreach ($results as $r) {
-                    $usersid = ($r['userid']);
-                    $db->query("SELECT * FROM grpgusers WHERE id = ?");
-                    $db->execute([$usersid]);
-                    $u = $db->fetch_row(true);
-                    if ($r['userid'] == 0) {
-                        if ($no2 > 0) {
-                            $u['username'] = "-";
-                        } else {
-                            $u['username'] = "[<a href='numbergame.php?take={$r['number']}'>select</a>]";
-                        }
-                    } else {
-                        $u['username'] = "<a href='profiles.php?id={$u['id']}'>{$u['username']}</a>";
-                    }
-                    print "\n<tr align=center><td class=contentcontent>{$r['number']}</td><td class=contentcontent>{$u['username']}</td></tr>";
-                    $current_row = 1 - $current_row;
-                }
-            }
-        } else {
+
             $db->query("SELECT * FROM numbergame WHERE number = ?");
             $db->execute([$getid]);
             $nogame = $db->fetch_row(true);
