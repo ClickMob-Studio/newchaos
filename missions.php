f283a413540254b2495976f71c805a9e9ef3b799
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-08-18 20:59:51 +0200

    fix: add cancel mission button

--- missions.php
+++ missions.php
@@ -162,6 +162,8 @@ $currenttime = time();
         <td>{$busts}</td>
 
     </tr>
+
+    <div><a href='?cancel' style='font-weight:800;color:red'>Cancel Current Mission</a></div>
 </div>";
         } else {
             $currenttime = time();
