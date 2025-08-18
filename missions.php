aaa8bd767775afc91f5f1606d4777d3bbbcbb4d6
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-08-18 21:01:52 +0200

    chore: try to move button

--- missions.php
+++ missions.php
@@ -162,8 +162,9 @@ $currenttime = time();
         <td>{$busts}</td>
 
     </tr>
-</div>
-<div><a href='?cancel' style='font-weight:800;color:red'>Cancel Current Mission</a></div>";
+</div>";
+
+            echo "<div><a href='?cancel' style='font-weight:800;color:red'>Cancel Current Mission</a></div>";
         } else {
             $currenttime = time();
             foreach ($missions as $v) {
