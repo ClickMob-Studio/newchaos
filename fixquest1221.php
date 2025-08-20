7eb3278024205fc6a128040a68cb35e9611a632f
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-08-20 09:52:07 +0200

    fix: quest

--- fixquest1221.php
+++ fixquest1221.php
@@ -1,34 +1,34 @@
 <?php
 
-require "header.php";
+// require "header.php";
 
-if ($user_class->admin != 1) {
-    die("Unauthorized");
-}
+// if ($user_class->admin != 1) {
+//     die("Unauthorized");
+// }
 
-$db->query("SELECT * FROM `quest_season_mission` WHERE `id` = 17");
-$mission = $db->fetch_row(true);
-if (empty($mission)) {
-    die("Could not find mission with ID 17");
-}
+// $db->query("SELECT * FROM `quest_season_mission` WHERE `id` = 17");
+// $mission = $db->fetch_row(true);
+// if (empty($mission)) {
+//     die("Could not find mission with ID 17");
+// }
 
-$db->query("SELECT * FROM `quest_season_mission_user` WHERE `quest_season_mission_id` = 17 AND `is_paid_out` = 1");
-$missionUsers = $db->fetch_row();
-if (empty($missionUsers)) {
-    die("No users that haven't been paid out");
-}
+// $db->query("SELECT * FROM `quest_season_mission_user` WHERE `quest_season_mission_id` = 17 AND `is_paid_out` = 1");
+// $missionUsers = $db->fetch_row();
+// if (empty($missionUsers)) {
+//     die("No users that haven't been paid out");
+// }
 
-foreach ($missionUsers as $u) {
-    $payouts = json_decode($mission['payouts'], true);
-    $payoutsToDisplay = 'You have received the following payouts:<br />';
-    $payoutsToDisplay .= '<ul>';
-    foreach ($payouts as $field => $value) {
-        if ($field === 'items') {
-            $user_c = new User($u['user_id']);
-            foreach ($value as $key => $item) {
-                Give_Item($item['id'], $user_c->id, $item['quantity']);
-            }
-        }
-    }
-}
+// foreach ($missionUsers as $u) {
+//     $payouts = json_decode($mission['payouts'], true);
+//     $payoutsToDisplay = 'You have received the following payouts:<br />';
+//     $payoutsToDisplay .= '<ul>';
+//     foreach ($payouts as $field => $value) {
+//         if ($field === 'items') {
+//             $user_c = new User($u['user_id']);
+//             foreach ($value as $key => $item) {
+//                 Give_Item($item['id'], $user_c->id, $item['quantity']);
+//             }
+//         }
+//     }
+// }
 
