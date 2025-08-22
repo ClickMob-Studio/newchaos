650a963dca7bb9feeba3ce76e778d9b71b077a5c
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-08-22 15:35:10 +0200

    fix: editgang.php

--- editgang.php
+++ editgang.php
@@ -13,56 +13,56 @@ $validtax = array(
 include 'header.php';
 ?>
 <div class='box_top'>Edit Gang</div>
-						<div class='box_middle'>
-							<div class='pad'>
-<?php
-$gang_class = new Gang($user_class->gang);
-if ($user_class->gang == 0)
-    error("You aren't in a gang.");
-$user_rank = new GangRank($user_class->grank);
-if ($user_class->gangleader == 0)
-    error("You don't have permission to be here!");
-if (isset($_POST['submit'])) {
-    if (!in_array($_POST['tax'], $validtax))
-        error("Tax Fraud Detected!");
-	$tax = security($_POST['tax']);
-    $gangbanner = (getimagesize($_POST['banner']) !== false) ? $_POST['banner'] : "";
-    $error = (strlen($gangname) < 3) ? "<div>Your gang's name has to be at least 3 characters long.</div>" : "";
-    $error = (strlen($gangname) > 25) ? "<div>Your gang's name can only be a max of 25 characters long.</div>" : "";
-    $error = (strlen($gangtag) < 1) ? "<div>Your gang's tag has to be at least 1 character long.</div>" : "";
-    $error = (strlen($gangtag) > 3) ? "<div>Your gang's tag can only be a max of 3 characters long.</div>" : "";
-    $gangtag = strip_tags($_POST['tag']);
-    $gangname = strip_tags($_POST['name']);
-	$gangdesc = $_POST['desc'];
-	$gangdesc2 = $_POST['desc2'];
-	$db->query("SELECT name FROM gangs WHERE name LIKE ? AND id <> ?");
-	$db->execute(array(
-		$gangname,
-		$gang_class->id
-	));
-    $error = ($db->fetch_single()) ? "<div>The gang name you chose is already taken.</div>" : "";
-	$db->query("SELECT tag FROM gangs WHERE tag LIKE ? AND id <> ?");
-	$db->execute(array(
-		$gangtag,
-		$gang_class->id
-	));
-    $error = ($db->fetch_single()) ? "<div>The gang tag you chose is already taken.</div>" : "";
-    if (empty($error)) {
-		$db->query("UPDATE gangs SET tag = ?, name = ?, description = ?, publicpage = ?, banner = ?, tax = ? WHERE id = ?");
-		$db->execute(array(
-			$gangtag,
-			$gangname,
-			$gangdesc,
-			$gangdesc2,
-			$gangbanner,
-			$tax,
-			$gang_class->id
-		));
-        echo Message("Your gang has successfully been edited.");
-    } else
-        error($error);
-}
-print "
+<div class='box_middle'>
+    <div class='pad'>
+        <?php
+        $gang_class = new Gang($user_class->gang);
+        if ($user_class->gang == 0)
+            error("You aren't in a gang.");
+        $user_rank = new GangRank($user_class->grank);
+        if ($user_class->gangleader == 0)
+            error("You don't have permission to be here!");
+        if (isset($_POST['submit'])) {
+            if (!in_array($_POST['tax'], $validtax))
+                error("Tax Fraud Detected!");
+            $tax = security($_POST['tax']);
+            $gangbanner = (getimagesize($_POST['banner']) !== false) ? $_POST['banner'] : "";
+            $error = (strlen($gangname) < 3) ? "<div>Your gang's name has to be at least 3 characters long.</div>" : "";
+            $error = (strlen($gangname) > 25) ? "<div>Your gang's name can only be a max of 25 characters long.</div>" : "";
+            $error = (strlen($gangtag) < 1) ? "<div>Your gang's tag has to be at least 1 character long.</div>" : "";
+            $error = (strlen($gangtag) > 3) ? "<div>Your gang's tag can only be a max of 3 characters long.</div>" : "";
+            $gangtag = strip_tags($_POST['tag']);
+            $gangname = strip_tags($_POST['name']);
+            $gangdesc = $_POST['desc'];
+            $gangdesc2 = $_POST['desc2'];
+            $db->query("SELECT name FROM gangs WHERE name LIKE ? AND id <> ?");
+            $db->execute(array(
+                $gangname,
+                $gang_class->id
+            ));
+            $error = ($db->fetch_single()) ? "<div>The gang name you chose is already taken.</div>" : "";
+            $db->query("SELECT tag FROM gangs WHERE tag LIKE ? AND id <> ?");
+            $db->execute(array(
+                $gangtag,
+                $gang_class->id
+            ));
+            $error = ($db->fetch_single()) ? "<div>The gang tag you chose is already taken.</div>" : "";
+            if (empty($error)) {
+                $db->query("UPDATE gangs SET tag = ?, name = ?, description = ?, publicpage = ?, banner = ?, tax = ? WHERE id = ?");
+                $db->execute(array(
+                    $gangtag,
+                    $gangname,
+                    $gangdesc,
+                    $gangdesc2,
+                    $gangbanner,
+                    $tax,
+                    $gang_class->id
+                ));
+                echo Message("Your gang has successfully been edited.");
+            } else
+                error($error);
+        }
+        print "
 
 <tr><td class='contentcontent'>
     <form method='post'>
@@ -80,9 +80,9 @@ print "
                 <td width='25%'>
                     <select name='tax'>
 ";
-foreach ($validtax as $tax)
-    echo "<option value='$tax'", ($tax == $gang_class->tax) ? " selected" : "", ">$tax%</option>";
-echo "
+        foreach ($validtax as $tax)
+            echo "<option value='$tax'", ($tax == $gang_class->tax) ? " selected" : "", ">$tax%</option>";
+        echo "
                     </select>
                 </td>
             </tr>
@@ -92,11 +92,11 @@ echo "
             </tr>
             <tr>
                 <td width='25%'><b>Public Gang Page:</b><br />[<a href='bbcode.php'>BBCode</a>]</td>
-                <td width='75%'><textarea name='desc2' cols='53' rows='7'>" . str_replace(array('<','>'), array('&lt;','&gt;'), $gang_class->publicpage) . "</textarea></td>
+                <td width='75%'><textarea name='desc2' cols='53' rows='7'>" . str_replace(array('<', '>'), array('&lt;', '&gt;'), (isset($gang_class->publicpage) ? $gang_class->publicpage : "")) . "</textarea></td>
             </tr>
             <tr>
                 <td width='25%'><b>Private Gang Page:</b><br />[<a href='bbcode.php'>BBCode</a>]</td>
-                <td width='75%'><textarea name='desc' cols='53' rows='7'>" . str_replace(array('<','>'), array('&lt;','&gt;'), $gang_class->description) . "</textarea></td>
+                <td width='75%'><textarea name='desc' cols='53' rows='7'>" . str_replace(array('<', '>'), array('&lt;', '&gt;'), (isset($gang_class->description) ? $gang_class->description : "")) . "</textarea></td>
             </tr>
             <tr>
                 <td width='25%'></td>
@@ -106,11 +106,12 @@ echo "
     </form>
 </td></tr>
 ";
-include("gangheaders.php");
-include 'footer.php';
-function error($text) {
-    echo Message($text);
-    include 'footer.php';
-    die();
-}
-?>
+        include("gangheaders.php");
+        include 'footer.php';
+        function error($text)
+        {
+            echo Message($text);
+            include 'footer.php';
+            die();
+        }
+        ?>
