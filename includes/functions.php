0ac4ebe20e03b8f17ba83f4c1402e59aa01832d6
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-09-30 12:42:26 +0200

    fix: quest

--- includes/functions.php
+++ includes/functions.php
@@ -3202,7 +3202,6 @@ function getDisplayForQuestReq($req, $num, $progress)
         return 'Steal the books' . $status;
     } else if ($req === 'attack_player') {
         return 'Attack Player: ' . formatName($num) . $status;
-    } else if ($req === 'whitecollar_fraud') {
     } else if ($req === 'attacks') {
         return 'Attack: ' . number_format($progress[$req]) . '/' . $num . ' ' . $status;
     } else if ($req === 'whitecollar_fraud') {
