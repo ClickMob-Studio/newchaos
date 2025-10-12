08a3a8876eec20ae1b74ba61cca3eb06429c14dd
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-10-12 23:44:23 +0200

    fix: leave gang

--- leavegang.php
+++ leavegang.php
@@ -30,13 +30,21 @@ include 'header.php';
                 AddToArmory($user_class->eqweapon, $user_class->gang);
                 perform_query("UPDATE grpgusers SET eqweapon = 0, weploaned = 0 WHERE id = ?", [$user_class->id]);
             }
-            if ($user_class->armorloaned == 1) {
+            if ($user_class->armloaned == 1) {
                 AddToArmory($user_class->eqarmor, $user_class->gang);
                 perform_query("UPDATE grpgusers SET eqarmor = 0, armloaned = 0 WHERE id = ?", [$user_class->id]);
             }
             if ($user_class->shoesloaned == 1) {
                 AddToArmory($user_class->eqshoes, $user_class->gang);
                 perform_query("UPDATE grpgusers SET eqshoes = 0, shoeloaned = 0 WHERE id = ?", [$user_class->id]);
+            }
+            if ($user_class->glovesloaned == 1) {
+                AddToArmory($user_class->eqgloves, $user_class->gang);
+                perform_query("UPDATE grpgusers SET eqgloves = 0, glovesloaned = 0 WHERE id = ?", [$user_class->id]);
+            }
+            if ($user_class->offhandloaned == 1) {
+                AddToArmory($user_class->eqoffhand, $user_class->gang);
+                perform_query("UPDATE grpgusers SET eqoffhand = 0, offhandloaned = 0 WHERE id = ?", [$user_class->id]);
             }
 
             $db->query("SELECT * FROM gang_loans WHERE to = ?");
