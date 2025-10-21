a0d791dc7e15ba94bb87ae8c4f68e67bb80660c3
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-10-21 11:22:27 +0200

    feat: includes

--- includes/repositories/chaos_repository.php
+++ includes/repositories/chaos_repository.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once '/../../dbcon.php';
-require_once '/../../classes.php';
-require_once '/../../database/pdo_class.php';
+require_once './../../dbcon.php';
+require_once './../../classes.php';
+require_once './../../database/pdo_class.php';
 require_once '../cache.php';
 require_once '../functions.php';
 require_once '../dto/chaos_dto.php';
