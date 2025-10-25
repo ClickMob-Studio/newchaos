b4685eda457e93f61553dcabb835f0c684f7704a
Mathias Mogensen <mathiasrieckm@gmail.com>
2025-10-26 01:51:56 +0200

    fix: hall of fame

--- halloffame.php
+++ halloffame.php
@@ -1,189 +1,170 @@
 <?php
 include 'header.php';
 
-$db->query("SELECT * FROM cities");
+
+function h($s)
+{
+    return htmlspecialchars((string) $s, ENT_QUOTES, 'UTF-8');
+}
+
+$db->query("SELECT `id`, `name` FROM `cities` ORDER BY `name` ASC");
 $db->execute();
 $cities = $db->fetch_row();
 
-$view = (isset($view) && $view != "") ? $view : 'level';
-if (!in_array($view, ['level', 'strength', 'defense', 'speed', 'agility', 'money', 'points', 'total', 'rmdays', 'bank', 'crimes', 'battlewon', 'battlelost', 'muggedmoney', 'posts', 'backalleywins', 'relationshipdays'])) {
+$cityLookup = [];
+if (is_array($cities)) {
+    foreach ($cities as $c) {
+        $cityLookup[(int) $c['id']] = $c['name'];
+    }
+}
+
+$allowedMetrics = [
+    'level' => ['label' => 'Level', 'column' => 'level'],
+    'strength' => ['label' => 'Strength', 'column' => 'strength'],
+    'defense' => ['label' => 'Defense', 'column' => 'defense'],
+    'speed' => ['label' => 'Speed', 'column' => 'speed'],
+    'agility' => ['label' => 'Agility', 'column' => 'agility'],
+    'money' => ['label' => 'Money', 'column' => 'money'],
+    'points' => ['label' => 'Points', 'column' => 'points'],
+    'total' => ['label' => 'Total', 'column' => 'total'],
+    'rmdays' => ['label' => 'RM Days', 'column' => 'rmdays'],
+    'bank' => ['label' => 'Bank', 'column' => 'bank'],
+    'crimes' => ['label' => 'Crimes Done', 'column' => 'crimes'],          // will map to crimesucceeded
+    'battlewon' => ['label' => 'Kills', 'column' => 'battlewon'],
+    'battlelost' => ['label' => 'Deaths', 'column' => 'battlelost'],
+    'muggedmoney' => ['label' => 'Highest Muggers', 'column' => 'muggedmoney'],
+    'posts' => ['label' => 'Forum Posts', 'column' => 'posts'],
+    'backalleywins' => ['label' => 'Back Alley Wins', 'column' => 'backalleywins'],
+    'relationshipdays' => ['label' => 'Time Married', 'column' => 'relationshipdays'],
+];
+
+$view = isset($_GET['view']) ? (string) $_GET['view'] : 'level';
+if (!array_key_exists($view, $allowedMetrics)) {
     $view = 'level';
 }
-?>
 
+$orderByColumn = ($view === 'crimes') ? 'crimesucceeded' : $allowedMetrics[$view]['column'];
+
+$secondarySort = ($view === 'level') ? ", `exp` DESC" : "";
+
+$selectedCityId = isset($_GET['cityid']) ? (int) $_GET['cityid'] : 0;
+$selectedCityId = array_key_exists($selectedCityId, $cityLookup) ? $selectedCityId : 0;
+
+$sql = "SELECT *
+        FROM `grpgusers` gu
+        WHERE (SELECT COUNT(*) FROM bans b WHERE b.id = gu.id AND b.type IN ('perm','freeze')) = 0
+          AND gu.`admin` = '0'
+          AND gu.`ban/freeze` = '0'";
+
+if ($selectedCityId > 0) {
+    $sql .= " AND gu.`cityid` = :cityid";
+}
+
+$sql .= " ORDER BY `{$orderByColumn}` DESC{$secondarySort} LIMIT 50";
+$db->query($sql);
+if ($selectedCityId > 0) {
+    $db->bind(':cityid', $selectedCityId);
+}
+$db->execute();
+$rows = $db->fetch_row(); // array of users
+
+?>
 <div class='box_top'>Hall Of Fame</div>
 <div class='box_middle'>
     <div class='pad'>
 
         <div class="contenthead floaty">
+            <form method="get" class="hof-filters" style="margin-bottom:12px; text-align:center;">
+                <label for="cityid" style="margin-right:8px;"><strong>City:</strong></label>
+                <select name="cityid" id="cityid" onchange="this.form.submit()" style="min-width:200px;">
+                    <option value="0" <?php echo $selectedCityId === 0 ? ' selected' : ''; ?>>All cities</option>
+                    <?php if (is_array($cities)): ?>
+                        <?php foreach ($cities as $row): ?>
+                            <option value="<?php echo (int) $row['id']; ?>" <?php echo ($selectedCityId === (int) $row['id']) ? ' selected' : ''; ?>>
+                                <?php echo h($row['name']); ?>
+                            </option>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                </select>
 
-            <table id="newtables" style="width:100%;">
+                <label for="view" style="margin:0 8px 0 16px;"><strong>Metric:</strong></label>
+                <select name="view" id="view" onchange="this.form.submit()" style="min-width:200px;">
+                    <?php foreach ($allowedMetrics as $key => $cfg): ?>
+                        <option value="<?php echo h($key); ?>" <?php echo ($view === $key) ? ' selected' : ''; ?>>
+                            <?php echo h($cfg['label']); ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
+
+                <noscript>
+                    <button type="submit" style="margin-left:12px;">Apply</button>
+                </noscript>
+            </form>
+
+            <table width='100%' border="0" bordercolor="#444444" cellpadding="4" cellspacing="0" align="center"
+                class="myTable">
                 <tr>
-                    <td align="center">
-                        <form method="get">
-                            <input type="hidden" name="view" value="<?php
-                            if (isset($view)) {
-                                echo $view;
-                            }
-                            ?>" />
-                            <?php
-                            foreach ($cities as $row) {
-                                ?>
-                                <option value='<?php
-                                echo $row['id'];
-                                ?>' <?php
-                                if (isset($_GET['cityid']) && $_GET['cityid'] == $row['id']) {
-                                    echo ($_GET['cityid'] == $row['id']) ? " selected" : "";
-                                }
-                                ?>><?php
-                                echo $row['name'];
-                                ?></option>
-                                <?php
-                            }
-                            ?>
-                            </select>
-                        </form>
-                        <form method="get">
-                            <select name='view' onchange='this.form.submit()'>
-                                <option value='level'>Select...</option>
-                                <option value='level' <?php
-                                if (isset($view) && $view == "level") {
-                                    echo " selected";
-                                }
-                                ?>>Level</option>
-                                <option value='strength' <?php
-                                if (isset($view) && $view == "strength") {
-                                    echo " selected";
-                                }
-                                ?>>Strength</option>
-                                <option value='defense' <?php
-                                if (isset($view) && $view == "defense") {
-                                    echo " selected";
-                                }
-                                ?>>Defense</option>
-                                <option value='speed' <?php
-                                if (isset($view) && $view == "speed") {
-                                    echo " selected";
-                                }
-                                ?>>Speed</option>
-                                <option value='agility' <?php
-                                if (isset($view) && $view == "agility") {
-                                    echo " selected";
-                                }
-                                ?>>Agility</option>
-                                <option value='money' <?php
-                                if (isset($view) && $view == "money") {
-                                    echo " selected";
-                                }
-                                ?>>Money</option>
-                                <option value='points' <?php
-                                if (isset($view) && $view == "points") {
-                                    echo " selected";
-                                }
-                                ?>>Points</option>
-                                <option value='total' <?php
-                                if (isset($view) && $view == "total") {
-                                    echo " selected";
-                                }
-                                ?>>Total</option>
-                                <option value='rmdays' <?php
-                                if (isset($view) && $view == "rmdays") {
-                                    echo " selected";
-                                }
-                                ?>>RM Days</option>
-                                <option value='bank' <?php
-                                if (isset($view) && $view == "bank") {
-                                    echo " selected";
-                                }
-                                ?>>Bank</option>
-                                <option value='crimes' <?php if (isset($view) && $view == "crimes") {
-                                    echo " selected";
-                                }
-                                ?>>Crimes Done</option>
-
-                                <option value='battlewon' <?php if (isset($view) && $view == "battlewon") {
-                                    echo " selected";
-                                }
-                                ?>>Kills</option>
-                                <option value='battlelost' <?php if (isset($view) && $view == "battlelost") {
-                                    echo " selected";
-                                }
-                                ?>>Deaths</option>
-
-                                <option value='muggedmoney' <?php if (isset($view) && $view == "muggedmoney") {
-                                    echo " selected";
-                                }
-                                ?>>
-                                    Highest Muggers</option>
-
-
-                                <option value='posts' <?php if (isset($view) && $view == "posts") {
-                                    echo " selected";
-                                }
-                                ?>>Forum Posts</option>
-                                <option value='backalleywins' <?php if (isset($view) && $view == "backalleywins") {
-                                    echo " selected";
-                                }
-                                ?>>Back Alley Wins</option>
-                                <option value='relationshipdays' <?php if (isset($view) && $view == "relationshipdays") {
-                                    echo " selected";
-                                }
-                                ?>>Time Married</option>
-
-                            </select>
-                        </form>
-                    </td>
+                    <td><b>Rank</b></td>
+                    <td><b>Mobster</b></td>
+                    <td><b>Level</b></td>
+                    <td><b>Money</b></td>
+                    <td><b>Gang</b></td>
+                    <td><b>Online</b></td>
                 </tr>
-                <table width='100%' border="0" bordercolor="#444444" cellpadding="4" cellspacing="0" align="center"
-                    class="myTable">
-                    <tr>
-                        <td><b>Rank</b></td>
-                        <td><b>Mobster</b></td>
-                        <td><b>Level</b></td>
-                        <td><b>Money</b></td>
-                        <td><b>Gang</b></td>
-                        <td><b>Online</b></td>
-                    </tr>
 
-                    <?php
-                    $view = (isset($view) && $view != "") ? $view : 'level';
-                    if ($view == 'crimes') {
-                        $view = 'crimesucceeded';
-                    }
-                    $view2 = ($view == "level") ? ", `exp` DESC" : "";
+                <?php
+                $rank = 0;
 
-                    $db->query("SELECT * FROM `grpgusers` gu WHERE (SELECT count(*) FROM bans b WHERE b.id = gu.id AND type IN ('perm','freeze')) = 0 AND `admin` = '0' AND `ban/freeze` = '0' ORDER BY `" . $view . "` DESC" . $view2 . " LIMIT 0,50");
-                    $db->execute();
-                    $rows = $db->fetch_row();
-                    $rank = 0;
+                if (is_array($rows) && count($rows) > 0) {
                     foreach ($rows as $line) {
                         $rank++;
                         $user_hall = new User($line['id']);
-                        echo '<tr><td>';
-                        if ($rank == 1)
-                            echo "<font color=#FFD700><b>1st</b></font>";
-                        elseif ($rank == 2)
-                            echo "<font color=#C0C0C0><b>2nd</b></font>";
-                        elseif ($rank == 3)
-                            echo "<font color=#CD7F32><b>3rd</b></font>";
-                        else {
-                            echo ordinal($rank);
-                        }
 
-                        echo '</td>';
-                        echo "
-<td>$user_hall->formattedname</td>
-<td>$user_hall->level</td>
-<td>" . prettynum($user_hall->money, 1) . "</td>
-<td>$user_hall->formattedgang</td>
-<td>$user_hall->formattedonline</td>
-</tr>
-";
+                        echo "<tr><td>";
+                        if ($rank === 1) {
+                            echo "<span style='color:#FFD700; font-weight:bold;'>1st</span>";
+                        } elseif ($rank === 2) {
+                            echo "<span style='color:#C0C0C0; font-weight:bold;'>2nd</span>";
+                        } elseif ($rank === 3) {
+                            echo "<span style='color:#CD7F32; font-weight:bold;'>3rd</span>";
+                        } else {
+                            echo h(ordinal($rank));
+                        }
+                        echo "</td>";
+
+                        echo "<td>{$user_hall->formattedname}</td>";
+                        echo "<td>" . h($user_hall->level) . "</td>";
+                        echo "<td>" . h(prettynum($user_hall->money, 1)) . "</td>";
+                        echo "<td>{$user_hall->formattedgang}</td>";
+                        echo "<td>{$user_hall->formattedonline}</td>";
+                        echo "</tr>";
                     }
-                    ?>
-                </table>
-                </td>
-                </tr>
-                <?php
-                include 'footer.php';
+                } else {
+                    echo "<tr><td colspan='6' style='text-align:center; opacity:0.8;'>No results found.</td></tr>";
+                }
                 ?>
+            </table>
+        </div>
+    </div>
+</div>
+
+<style>
+    .hof-filters select {
+        padding: 4px 6px;
+    }
+
+    .myTable tr:nth-child(even) {
+        background: #111;
+    }
+
+    .myTable tr:nth-child(odd) {
+        background: #0c0c0c;
+    }
+
+    .myTable td {
+        border-bottom: 1px solid #222;
+    }
+</style>
+
+<?php
+include 'footer.php';
